a5c016da9165c68034c9bfb8d7136782de8c6520
Don Peterson <someonesdad1@gmail.com>
2021-06-08 22:25:50 +0100

    Updated

--- brace.py
+++ brace.py
@@ -3,19 +3,31 @@ Perform brace expansion similar to that done in modern shells.  Note that
 numerical sequences aren't part of the syntax.
 '''
 
+if 1:  # Copyright, license
+    # These "trigger strings" can be managed with trigger.py
+    #∞copyright∞# Copyright (C) 2021 Don Peterson #∞copyright∞#
+    #∞contact∞# gmail.com@someonesdad1 #∞contact∞#
+    #∞license∞#
+    #   Licensed under the GNU Free Documentation License 1.2
+    #∞license∞#
+    #∞what∞#
+    # Function to perform brace expansion similar to modern shells
+    #∞what∞#
+    #∞test∞# run #∞test∞#
+    pass
 def BraceExpansion(s):
     '''Return a list of the items indicated by the string s.  Examples:
-
+ 
     * BraceExpansion("{a,b}/*.{jpg,png}") returns
         ['a/*.jpg', 'a/*.png', ' b/*.jpg', ' b/*.png']
-
     * BraceExpansion("{,a}/{c,d}") returns
         ['/c', '/d', 'a/c', 'a/d']
-
     * BraceExpansion(r"{,,a}/{c,d}") returns
         ['/c', '/d', '/c', '/d', 'a/c', 'a/d']
     '''
     # From http://rosettacode.org/wiki/Brace_expansion
+    # The web page's content is available under the GNU Free
+    # Documentation license 1.2.
     def getitem(s, depth=0):
         out = [""]
         while s:
@@ -46,3 +58,25 @@ def BraceExpansion(s):
                 comma, s = True, s[1:]
         return None
     return getitem(s)[0]
+
+if __name__ == "__main__": 
+    from lwtest import run
+    from itertools import product
+    from pdb import set_trace as xx
+    def TestSimple():
+        s = ' '.join(BraceExpansion("a{d,c,b}e"))
+        t = "ade ace abe"
+        assert(s == t)
+    def TestCartesianProduct():
+        s = ' '.join(BraceExpansion("{a,b,c}{d,e,f}"))
+        t = ' '.join([i + j for i, j in product("abc", "def")])
+        assert(s == t)
+        s = str(BraceExpansion("{a,b}/*.{jpg,png}"))
+        t = "['a/*.jpg', 'a/*.png', 'b/*.jpg', 'b/*.png']"
+        assert(s == t)
+    def TestNested():
+        s = ' '.join(BraceExpansion("{,a}{b,{c,d},e}"))
+        t = "b c d e ab ac ad ae"
+        assert(s == t)
+    exit(run(globals())[0])
+
