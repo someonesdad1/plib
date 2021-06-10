9d2649086d277539b38e39898e5ce03124eab533
Don Peterson <someonesdad1@gmail.com>
2021-06-10 20:50:34 +0100

    Updated

--- geometry.py
+++ geometry.py
@@ -1,33 +1,31 @@
 '''
 TODO:
-
     * Quadrilateral must verify the shape is convex.
       https://en.wikipedia.org/wiki/Quadrilateral#Concave_quadrilaterals
       gives the definition that a concave quadrilateral has one interior
       angle > 180 deg and one of the two diagonals lies outside the
       quadrilateral.
-
-----------------------------------------------------------------------
-
-A collection of plane geometry formulas.  This module will not work under
-python 2.
-
 '''
-
-# Copyright (C) 2010 Don Peterson
-# Contact:  gmail.com@someonesdad1
-
-#
-# Licensed under the Open Software License version 3.0.
-# See http://opensource.org/licenses/OSL-3.0.
-#
-
-import collections
-from util import IsIterable, IsConvexPolygon
-from math import pi, atan, hypot, sqrt, asin
-from roundoff import RoundOff
-from pdb import set_trace as xx
-
+if 1:  # Copyright, license
+    # These "trigger strings" can be managed with trigger.py
+    #∞copyright∞# Copyright (C) 2010 Don Peterson #∞copyright∞#
+    #∞contact∞# gmail.com@someonesdad1 #∞contact∞#
+    #∞license∞#
+    #   Licensed under the Open Software License version 3.0.
+    #   See http://opensource.org/licenses/OSL-3.0.
+    #∞license∞#
+    #∞what∞#
+    # A collection of plane geometry formulas
+    #∞what∞#
+    #∞test∞# run #∞test∞#
+    pass
+if 1:   # Imports
+    import collections
+    from math import pi, atan, hypot, sqrt, asin
+    from pdb import set_trace as xx
+if 1:   # Custom imports
+    from util import IsIterable, IsConvexPolygon
+    from roundoff import RoundOff
 def ArePoints(*points, n=2):
     '''Return True if each of the parameters are n-tuples of numbers or
     objects that can be converted to numbers using float().
@@ -40,7 +38,6 @@ def ArePoints(*points, n=2):
         except Exception:
             return False
     return True
-
 def Det(*a):
     '''Determinant of a 3 dimensional matrix (a[0] is element (1, 1),
     a[1] is element (1, 2), etc.).
@@ -49,8 +46,6 @@ def Det(*a):
     return (a[0]*(a[4]*a[8] - a[7]*a[5])
             - a[1]*(a[3]*a[8] - a[6]*a[5])
             + a[2]*(a[3]*a[7] - a[4]*a[6]))
-
-
 def TriangleCircumscribedCircle(p1, p2, p3):
     '''Return the (radius, center) of the circumscribed circle for the
     triangle of the 3 given points.  The Cartesian coordinates of the
@@ -77,7 +72,6 @@ def TriangleCircumscribedCircle(p1, p2, p3):
     center = (RoundOff(Sx/a), RoundOff(Sy/a))
     radius = RoundOff(sqrt(b/a + (hypot(Sx, Sy)/a)**2))
     return radius, center
-
 def TriangleInscribedCircle(p1, p2, p3):
     '''Return the (radius, center) of the inscribed circle for the
     triangle of the 3 given points.  The Cartesian coordinates of the
@@ -101,7 +95,6 @@ def TriangleInscribedCircle(p1, p2, p3):
     cx = RoundOff((a*x1 + b*x2 + c*x3)/s)
     cy = RoundOff((a*y1 + b*y2 + c*y3)/s)
     return radius, (cx, cy)
-
 # For some quadrilateral formulas, see
 # http://www.geometryatlas.com/categories/Quadrilaterals-General
 def Quadrilateral(p1, p2, p3, p4):
@@ -154,7 +147,6 @@ def Quadrilateral(p1, p2, p3, p4):
     assert(abs(d["area"]/A - 1) < 1e-14)
 
     return results
-
 def QuadrilateralArea(p1, p2, p3, p4):
     '''The Cartesian coordinates of the vertices of the quadrilateral
     are the points p1, p2, p3, p4; each are 2-vectors.
@@ -166,7 +158,6 @@ def QuadrilateralArea(p1, p2, p3, p4):
     x0, x1, x2, x3 = p1[0], p2[0], p3[0], p4[0]
     y0, y1, y2, y3 = p1[1], p2[1], p3[1], p4[1]
     return abs(y0*(x1 - x3) + y1*(x2 - x0) + y2*(x3 - x1) + y3*(x0 - x2))/2
-
 def QuadrilateralPerimeter(p1, p2, p3, p4):
     '''The Cartesian coordinates of the vertices of the quadrilateral
     are in the points p1, p2, p3, p4; each are 2-vectors.
@@ -181,7 +172,6 @@ def QuadrilateralPerimeter(p1, p2, p3, p4):
             (x[2]*(-1))))**(2) + ((y[1] + (y[2]*(-1))))**(2)))**(1/2) +
             ((((x[2] + (x[3]*(-1))))**(2) + ((y[2] +
             (y[3]*(-1))))**(2)))**(1/2))
-
 def QuadrilateralAngles(p1, p2, p3, p4):
     '''Returns the four interior angles in radians of a quadrilateral.
     The Cartesian coordinates of the vertices of the quadrilateral are
@@ -216,7 +206,6 @@ def QuadrilateralAngles(p1, p2, p3, p4):
         c = pi/2
     d = 2*pi - (a + b + c)
     return (a, b, c, d)
-
 def QuadrilateralDiagonals(p1, p2, p3, p4):
     '''Returns the lengths of the two diagonals of a quadrilateral.
     The Cartesian coordinates of the vertices of the quadrilateral are
@@ -229,5 +218,58 @@ def QuadrilateralDiagonals(p1, p2, p3, p4):
     d1 = hypot(x[0] - x[2], y[0] - y[2])
     d2 = hypot(x[1] - x[3], y[1] - y[3])
     return (d1, d2)
-
-QuadrilateralDiagonals((0, 0), (1, 0), (0, 1), (1, 1))
+if __name__ == "__main__": 
+    from lwtest import run, assert_equal, raises, Assert
+    def TestDet():
+        a = (1, 0, 0, 0, 1, 0, 0, 0, 1)
+        assert_equal(Det(*a), 1)
+        assert_equal(Det(*range(1, 10)), 0)
+    def TestTriangleCircumscribedCircle():
+        '''Unit circle with center at origin.
+        '''
+        p  = ((-1, 0), (0, 1), (1, 0))
+        r, c = TriangleCircumscribedCircle(*p)
+        assert_equal(r, 1)
+        assert_equal(c[0], 0)
+        assert_equal(c[1], 0)
+        p  = ((0, 1), (1, 0), (-1, 0))
+        r, c = TriangleCircumscribedCircle(*p)
+        assert_equal(r, 1)
+        assert_equal(c[0], 0)
+        assert_equal(c[1], 0)
+        p = ((1, 0), (-1, 0), (0, 1))
+        r, c = TriangleCircumscribedCircle(*p)
+        assert_equal(r, 1)
+        assert_equal(c[0], 0)
+        assert_equal(c[1], 0)
+    def TestTriangleInscribedCircle():
+        '''From a graphical construction; numbers in mm.  This was a 45
+        degree isoceles right triangle with the right angle at the origin.
+        The right-most point was p3 and the highest point was p2.  The
+        measured incircle diameter was 28.6 mm with measured center at
+        (28.5, 28.5).
+        '''
+        a = 98.3
+        p1 = (0, 0)
+        p2 = (0, a)
+        p3 = (a, 0)
+        r0 = 28.79140340936
+        tol = 2e-12
+        for p in ((p1, p2, p3), (p3, p1, p2), (p2, p3, p1)):
+            r, c = TriangleInscribedCircle(*p)
+            assert_equal(r, r0, reltol=tol)
+            assert_equal(c[0], r0, reltol=tol)
+            assert_equal(c[1], r0, reltol=tol)
+    def TestQuadrilateralArea():
+        a = 2.345
+        p1 = (0, 0)
+        p2 = (0, a)
+        p3 = (a, a)
+        p4 = (a, 0)
+        # Test each permutation
+        for p in ((p1, p2, p3, p4), (p4, p1, p2, p3),
+            (p3, p4, p1, p2), (p2, p3, p4, p1)):
+            A = QuadrilateralArea(*p)
+            #print(p, A, a*a)
+            assert_equal(A, a*a)
+    exit(run(globals(), halt=0)[0])
