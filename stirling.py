e2d8a39c6d953b2b147aac4d4a49d6a1f1a01084
Don Peterson <someonesdad1@gmail.com>
2021-06-11 05:18:35 +0100

    Updated

--- stirling.py
+++ stirling.py
@@ -7,17 +7,21 @@ References:
     http://mathworld.wolfram.com/StirlingNumberoftheFirstKind.html
     http://mathworld.wolfram.com/StirlingNumberoftheSecondKind.html
 '''
-
-# Copyright (C) 2014 Don Peterson
-# Contact:  gmail.com@someonesdad1
-
-#
-# Licensed under the Open Software License version 3.0.
-# See http://opensource.org/licenses/OSL-3.0.
-#
-
-__all__ = ["Stirling1", "Stirling2"]
-
+if 1:  # Copyright, license
+    # These "trigger strings" can be managed with trigger.py
+    #∞copyright∞# Copyright (C) 2014 Don Peterson #∞copyright∞#
+    #∞contact∞# gmail.com@someonesdad1 #∞contact∞#
+    #∞license∞#
+    #   Licensed under the Open Software License version 3.0.
+    #   See http://opensource.org/licenses/OSL-3.0.
+    #∞license∞#
+    #∞what∞#
+    # Calculates Stirling numbers
+    #∞what∞#
+    #∞test∞# run #∞test∞#
+    pass
+if 1:   # Global variables
+    __all__ = ["Stirling1", "Stirling2"]
 def s(n, k):
     '''Returns the Stirling number of the first kind.
  
@@ -37,7 +41,6 @@ def s(n, k):
     elif k > n:
         return 0
     return s(n - 1, k - 1) - (n - 1)*s(n - 1, k)
-
 def S(n, k):
     '''Returns the Stirling number of the second kind.
  
@@ -52,7 +55,6 @@ def S(n, k):
     elif k > n or n <= 0:
         return 0
     return S(n - 1, k - 1) + k * S(n - 1, k)
-
 class Memoize:
     '''Class to cache return values for a function.
     '''
@@ -66,6 +68,81 @@ class Memoize:
             result = self.func(*params)
             self.cache[params] = result
             return result
-
 Stirling1 = Memoize(s)
 Stirling2 = Memoize(S)
+
+if __name__ == "__main__": 
+    from lwtest import run, raises, assert_equal, Assert
+    def Test_first_kind():
+        '''Numbers from https://oeis.org/A008275 (See the triangle down
+        next to EXAMPLE).
+        '''
+        n = 1
+        expected = (0, 1)
+        for k in range(1, n):   
+            Assert(s(n, k) == expected[k])
+        n = 2
+        expected = (0, -1, 1)
+        for k in range(1, n):   
+            Assert(s(n, k) == expected[k])
+        n = 3
+        expected = (0, 2, -3, 1)
+        for k in range(1, n):   
+            Assert(s(n, k) == expected[k])
+        n = 4
+        expected = (0, -6, 11, -6, 1)
+        for k in range(1, n):   
+            Assert(s(n, k) == expected[k])
+        n = 5
+        expected = (0, 24, -50, 35, -10, 1)
+        for k in range(1, n):   
+            Assert(s(n, k) == expected[k])
+        n = 6
+        expected = (0, -120, 274, -225, 85, -15, 1)
+        for k in range(1, n):   
+            Assert(s(n, k) == expected[k])
+        n = 7
+        expected = (0, 720, -1764, 1624, -735, 175, -21, 1)
+        for k in range(1, n):   
+            Assert(s(n, k) == expected[k])
+        n = 8
+        expected = (0, -5040, 13068, -13132, 6769, -1960, 322, -28,  1)
+        for k in range(1, n):   
+            Assert(s(n, k) == expected[k])
+    def Test_second_kind():
+        '''Numbers from https://oeis.org/A008277 (See the triangle down
+        next to EXAMPLE).
+        '''
+        n = 1
+        expected = (0, 1)
+        for k in range(1, n):   
+            Assert(S(n, k) == expected[k])
+        n = 2
+        expected = (0, 1, 1)
+        for k in range(1, n):   
+            Assert(S(n, k) == expected[k])
+        n = 3
+        expected = (0, 1, 3, 1)
+        for k in range(1, n):   
+            Assert(S(n, k) == expected[k])
+        n = 4
+        expected = (0, 1, 7, 6, 1)
+        for k in range(1, n):   
+            Assert(S(n, k) == expected[k])
+        n = 5
+        expected = (0, 1, 15, 25, 10, 1)
+        for k in range(1, n):   
+            Assert(S(n, k) == expected[k])
+        n = 6
+        expected = (0, 1, 31, 90, 65, 15, 1)
+        for k in range(1, n):   
+            Assert(S(n, k) == expected[k])
+        n = 7
+        expected = (0, 1, 63, 301, 350, 140, 21, 1)
+        for k in range(1, n):   
+            Assert(S(n, k) == expected[k])
+        n = 8
+        expected = (0, 1, 127, 966, 1701, 1050, 266, 28, 1)
+        for k in range(1, n):   
+            Assert(S(n, k) == expected[k])
+    exit(run(globals(), halt=1)[0])
