5f145ba8f347a1ce8d30b5bb7763ef3da6005d6b
Don Peterson <someonesdad1@gmail.com>
2021-07-25 21:26:22 +0100

    Updated

---
+++ pgm/relay.py
@@ -0,0 +1,10 @@
+print('''
+DIN car relay pin numbering
+  85    Coil ground
+  86    Coil hot
+  30    Load common
+  87    Load NO
+  87a   Load NC
+Typical coil current is 200 mA @ 12.5 V, which means about 60-70 ohms
+coil resistance.
+'''.strip())
