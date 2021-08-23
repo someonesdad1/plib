c2d68a4bdd584480a1801c648bd8b015926ab8b7
Don Peterson <someonesdad1@gmail.com>
2021-08-23 18:48:26 +0100

    Updated

--- asme.py
+++ asme.py
@@ -17,7 +17,8 @@ if 1:  # Copyright, license
     #∞what∞#
     #∞test∞# run #∞test∞#
     pass
-
+if 1:   # Custom imports
+    from f import flt
 class UnifiedThread:
     '''Initialize with basic diameter in inches, threads per inch,
     class, and length of engagement in units of the basic diameter.
@@ -38,47 +39,48 @@ class UnifiedThread:
             msg = "Length of engagement must be between 0.1 and 1.5"
             raise ValueError(msg)
         # Basic diameter
-        self.D = basic_diameter
+        self.D = flt(basic_diameter)
         # Pitch
-        self.P = 1/tpi
+        self.P = flt(1/tpi)
         # H = fundamental height of vee thread
-        self.H = 3**0.5/2*self.P
+        self.H = flt(3**0.5/2*self.P)
         self.Class = Class
-        self.length_of_engagement = length_of_engagement
+        self.length_of_engagement = flt(length_of_engagement)
     def Allowance(self):
         if self.Class == 3:
-            return 0
-        return 0.3*self.Class2PDtol()
+            return flt(0)
+        return flt(0.3*self.Class2PDtol())
     def Class2PDtol(self):
-        return 0.0015*(self.D**(1/3) +
+        return flt(0.0015*(self.D**(1/3) +
                        (self.length_of_engagement*self.D)**0.5 +
-                       10*self.P**(2/3))
+                       10*self.P**(2/3)))
     def Dmax(self):
         'External thread max major diameter'
-        return self.D - self.Allowance()
+        return flt(self.D - self.Allowance())
     def Dmin(self):
         'External thread min major diameter'
-        c = 0.09 if self.Class == 1 else 0.06
-        return self.Dmax() - c*self.P**(2/3)
+        c = flt(0.09) if self.Class == 1 else flt(0.06)
+        return flt(self.Dmax() - c*self.P**(2/3))
     def Emax(self):
         'External thread max pitch diameter'
-        return self.D - self.Allowance() - 3*3**0.5*self.P/8
+        return flt(self.D - self.Allowance() - 3*3**0.5*self.P/8)
     def Emin(self):
         'External thread min pitch diameter'
-        c = 3/2 if self.Class == 1 else 1 if self.Class == 2 else 3/4
-        return self.Emax() - c*self.Class2PDtol()
+        c = (flt(3/2) if self.Class == 1 else flt(1) if self.Class == 2
+             else flt(3/4))
+        return flt(self.Emax() - c*self.Class2PDtol())
     def dmax(self):
         'Internal thread max minor diameter'
         if self.Class in (1, 2):
             if self.D < 1/4:
                 tol = 0.05*self.P**(2/3) + 0.03*self.P/self.D - 0.002
                 tol = min(tol, 0.394*self.P)
-                tol = max(tol, self.P/4 - 2*self.P*self.P)/5
+                tol = flt(max(tol, self.P/4 - 2*self.P*self.P)/5)
             else:
                 if 1/self.P >= 4:
-                    tol = self.P/4 - 2*self.P*self.P/5
+                    tol = flt(self.P/4 - 2*self.P*self.P/5)
                 else:
-                    tol = 3*self.P/20
+                    tol = flt(3*self.P/20)
         else:
             tol = 0.05*self.P**(2/3) + 0.03*self.P/self.D - 0.002
             tol = min(tol, 0.394*self.P)
@@ -86,32 +88,32 @@ class UnifiedThread:
                 tol = max(tol, 0.23*self.P - 3*self.P*self.P/2)
             else:
                 tol = max(tol, 0.120*self.P)
-        return tol + self.dmin()
+        return flt(tol + self.dmin())
     def dmin(self):
         'Internal thread min minor diameter'
-        return self.D - 5*3**0.5*self.P/8
+        return flt(self.D - 5*3**0.5*self.P/8)
     def emax(self):
         'Internal thread max pitch diameter'
-        c = 1.95 if self.Class == 1 else 1.3 if self.Class == 2 else 0.975
-        return self.emin() + c*self.Class2PDtol()
+        c = (flt(1.95) if self.Class == 1 else flt(1.3) if self.Class == 2
+             else flt(0.975))
+        return flt(self.emin() + c*self.Class2PDtol())
     def emin(self):
         'Internal thread min pitch diameter'
-        return self.dmin() + 3**0.5/4*self.P
+        return flt(self.dmin() + 3**0.5/4*self.P)
     def dext(self):
         'External thread minor diameter'
-        return 2*(self.Dmax()/2 - 17/24*self.H)
+        return flt(2*(self.Dmax()/2 - 17/24*self.H))
     def Dint(self):
         'Internal thread major diameter'
-        return self.D
+        return flt(self.D)
     def __str__(self):
-        return ("UnifiedThread(D={0}, tpi={1})".format(str(self.D),
-                str(1/self.P)))
+        return (f"UnifiedThread(D={self.D}, tpi={1/self.P})")
     def TapDrill(self, percent_thread=75):
         if not (0 <= percent_thread <= 100):
             raise ValueError("percent_thread must be between 0 and 100.")
         # A 100% thread is one with height of 6/8 of H
         h = (6/8)*self.H*percent_thread/100
-        return round(self.D - 2*h, 4)
+        return flt(self.D - 2*h)
     def SellersRecommendedTPI(self):
         '''Returns the recommended threads per inch via a formula from
         Sellers (proposed in 1864).  In "Handbook of Small Tools", 1908, pg
@@ -125,20 +127,19 @@ class UnifiedThread:
         '''
         a = 0.23 if self.D < 1/4 else 0.24
         pitch = a*(self.D + 5/8)**0.5 - 0.175
-        return round(1/pitch, 2)
+        return flt(1/pitch)
     def DoubleDepth(self):
         'Returns the double depth of the unified thread in inches'
-        return round(3.0/2*self.H, 4)
+        return flt(3.0/2*self.H)
     def NumberSize(self, n):
         '''A convenience function to return the diameter in inches of a
         number-size thread.  abs(n) is used.
         '''
-        return 0.06 + 0.013*abs(n)
-
+        return flt(0.06 + 0.013*abs(n))
 if __name__ == "__main__": 
     from lwtest import run, raises, assert_equal
     def Test_asme():
-        eps = 0.0001
+        eps = flt(0.0001)
         # Check the formulas on a 1/4-20 thread
         u = UnifiedThread(1/4, 20, Class=1)
         assert(abs(u.Class2PDtol() - 0.00373) <= eps)
---
+++ ruler.py
@@ -0,0 +1,110 @@
+'''
+Return a ruler for ASCII printing
+
+Run as a script for a demo.
+'''
+
+# Copyright (C) 2009, 2021 Don Peterson
+# Contact:  gmail.com@someonesdad1
+
+#
+# Licensed under the Open Software License version 3.0.
+# See http://opensource.org/licenses/OSL-3.0.
+#
+
+import os
+
+class Ruler(object):
+    '''Return ruler strings.  Initialize with either an integer or
+    ones/tens strings.  Call the object to get a ruler string; if the
+    call has an argument, it is an integer representing the maximum
+    width of the ruler.  For no argument, the width is defined by the
+    COLUMNS environment variable or is 79 if COLUMNS is not defined.
+    '''
+    # You can define your own choices here
+    choices = (
+        "····+····|", # 0
+        "----+----|", # 1
+        "┰┰┰┰┴┰┰┰┰┋", # 2
+        "1234·67890", # 3
+        "         |", # 4
+        "    +    |", # 5
+        "    ·    |", # 6
+        "====+====█", # 7
+    )
+    def __init__(self, num=None, ones=None, tens=True, zb=False):
+        '''If num is not None, then it must be a number that indicates
+        which ruler you want.  You can also specify the ones string;
+        supplying ones overrides supplying num.  If tens is True, then
+        a tens line is also produced.  If zb is True, make the ruler
+        zero-based.
+        '''
+        self.tens = tens
+        self.zb = bool(zb)
+        if num is None and ones is None:
+            num = 0
+        if num is not None:
+            self.ones = Ruler.choices[num]
+        else:
+            self.ones = ones
+            if len(self.ones) != 10:
+                raise ValueError("ones must be a string of length 10")
+    def __call__(self, columns=None, choice=None, ones=None):
+        if ones is not None:
+            if len(ones) != 10:
+                raise ValueError("ones must be a string of length 10")
+        elif choice is not None:
+            ones = Ruler.choices[int(choice)]
+        else:
+            ones = self.ones
+        if columns is None:
+            columns = self._ncols()
+        else:
+            columns = abs(int(columns))
+        if not columns:
+            return ""
+        s = ""
+        n, remainder = divmod(columns, 10)
+        if self.tens:
+            s = ''.join(["%10d" % i for i in range(1, n + 1)]) + " "
+            if self.zb:
+                s = "0" + s[:-1]
+            if len(s) > columns:
+                s = s[:columns]
+            s += "\n"
+        if self.zb:
+            sepchar = ones[9]
+            s += sepchar + ones*n
+            if remainder:
+                s += ones[:remainder - 1]
+        else:
+            s += ones*n + ones[:remainder]
+        return s
+    def _ncols(self):
+        columns = 80
+        if "COLUMNS" in os.environ:
+            columns = int(os.environ["COLUMNS"])
+        # Reduce so we don't get a second line because of the newline
+        columns -= 1
+        return columns
+
+if __name__ == "__main__":
+    import sys
+    r = Ruler()
+    z = Ruler(zb=True)
+    if len(sys.argv) > 1:
+        # Show rulers that were chosen on the command line
+        for n in sys.argv[1:]:
+            print(r(choice=int(n)))
+        print()
+        for n in sys.argv[1:]:
+            print(z(choice=int(n)))
+    else:
+        # Show all rulers
+        for i in range(len(Ruler.choices)):
+            print("Ruler type = " + str(i))
+            print(r(choice=i))
+        print()
+        for i in range(len(Ruler.choices)):
+            print("Ruler type = " + str(i) + " (zero-based)")
+            print(z(choice=i))
