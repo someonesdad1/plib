09439353bde24f4f025aba8aa1dd4dd3702cdbf5
Don Peterson <someonesdad1@gmail.com>
2021-08-28 02:31:38 +0100

    Updated

--- dpdecimal.py
+++ dpdecimal.py
@@ -1,12 +1,12 @@
 '''
 Todo:
-
+ 
     * If x is a dec, then -x returns a Decimal.  Need to have all 
       such operations defined so that a dec is returned.
     * Similarly, all math operations need to be defined to allow infection
       to occur.  Maybe these can be defined as part of the script instead
       of having to write every one of them:
-
+ 
 __repr__           __bool__           quantize           copy_sign
 __hash__           __int__            remainder_near     same_quantum
 __str__            __float__          fma                logical_and
@@ -32,20 +32,20 @@ __rpow__           max_mag            number_class       real
 __neg__            min                to_eng_string      imag
 __pos__            min_mag            compare_total      __doc__
 __abs__            next_toward        compare_total_mag  __module__
-
+ 
 Signatures:
- *  __abs__(self, /)
- *  __add__(self, value, /)
+    __abs__(self, /)
+    __add__(self, value, /)
     __bool__(self, /)
     __ceil__(...)
     __complex__(...)
- *  __copy__(...)
- *  __deepcopy__(...)
+    __copy__(...)
+    __deepcopy__(...)
     __divmod__(self, value, /)
     __eq__(self, value, /)
     __float__(self, /)
     __floor__(...)
- *  __floordiv__(self, value, /)
+    __floordiv__(self, value, /)
     __format__(...)
     __ge__(self, value, /)
     __getattribute__(self, name, /)
@@ -54,28 +54,29 @@ Signatures:
     __int__(self, /)
     __le__(self, value, /)
     __lt__(self, value, /)
- *  __mod__(self, value, /)
- *  __mul__(self, value, /)
+    __mod__(self, value, /)
+    __mul__(self, value, /)
     __ne__(self, value, /)
- *  __neg__(self, /)
- *  __pos__(self, /)
- *  __pow__(self, value, mod=None, /)
- *  __radd__(self, value, /)
- *  __rdivmod__(self, value, /)
+    __neg__(self, /)
+    __pos__(self, /)
+    __pow__(self, value, mod=None, /)
+    __radd__(self, value, /)
+    __rdivmod__(self, value, /)
     __reduce__(...)
- *  __repr__(self, /)
- *  __rfloordiv__(self, value, /)
+    __repr__(self, /)
+    __rfloordiv__(self, value, /)
     __rmod__(self, value, /)
- *  __rmul__(self, value, /)
+    __rmul__(self, value, /)
     __round__(...)
- *  __rpow__(self, value, mod=None, /)
- *  __rsub__(self, value, /)
- *  __rtruediv__(self, value, /)
+    __rpow__(self, value, mod=None, /)
+    __rsub__(self, value, /)
+    __rtruediv__(self, value, /)
     __sizeof__(...)
- *  __str__(self, /)
- *  __sub__(self, value, /)
- *  __truediv__(self, value, /)
+    __str__(self, /)
+    __sub__(self, value, /)
+    __truediv__(self, value, /)
     __trunc__(...)
+
     compare(self, /, other, context=None)
     compare_signal(self, /, other, context=None)
     compare_total(self, /, other, context=None)
@@ -84,15 +85,15 @@ Signatures:
     copy_abs(self, /)
     copy_negate(self, /)
     copy_sign(self, /, other, context=None)
- *  exp(self, /, context=None)
+    exp(self, /, context=None)
     fma(self, /, other, third, context=None)
- *  ln(self, /, context=None)
- *  log10(self, /, context=None)
- *  logb(self, /, context=None)
- *  logical_and(self, /, other, context=None)
- *  logical_invert(self, /, context=None)
- *  logical_or(self, /, other, context=None)
- *  logical_xor(self, /, other, context=None)
+    ln(self, /, context=None)
+    log10(self, /, context=None)
+    logb(self, /, context=None)
+    logical_and(self, /, other, context=None)
+    logical_invert(self, /, context=None)
+    logical_or(self, /, other, context=None)
+    logical_xor(self, /, other, context=None)
     max(self, /, other, context=None)
     max_mag(self, /, other, context=None)
     min(self, /, other, context=None)
@@ -114,10 +115,10 @@ Signatures:
     to_integral(self, /, rounding=None, context=None)
     to_integral_exact(self, /, rounding=None, context=None)
     to_integral_value(self, /, rounding=None, context=None)
-
+ 
     Class methods:
         from_float(f, /) from builtins.type
-
+ 
 Provides Decimal objects with custom string interpolation
 '''
 if 1:  # Copyright, license
@@ -141,12 +142,26 @@ if 1:   # Imports
     import decimal
     import locale
     from collections import deque
+    from functools import partial
     from pdb import set_trace as xx 
 if 1:   # Custom imports
     from wrap import dedent
+    from columnize import Columnize
+    if 0:
+        from color import C
+    else:
+        C = None
+    if 1:
+        import debug
+        debug.SetDebugger()
 if 1:   # Global variables
     ii = isinstance
     D = decimal.Decimal
+    class g: pass
+    g.d = C.lgrn if C else ""
+    g.o = C.lyel if C else ""
+    g.n = C.norm if C else ""
+
 class dec(decimal.Decimal):
     '''Provides decimal.Decimal numbers with custom string interpolation.
     This class also "infects" calculations with integers, floats, and
@@ -216,7 +231,104 @@ class dec(decimal.Decimal):
         return dec(super().__neg__())
     def __add__(self, other):
         return dec(super().__add__(other))
-
+def Signatures():
+    'Print out the types of the return values of decimal.Decimal methods'
+    x, y = D("1.234"), D("3.456")
+    X, Y = D("11001"), D("11101")
+    show = True
+    seen = []
+    w = 25
+    t = "Error:  {n:{w}s}: {e}"
+    def Type(z):
+        s = str(type(z))[1:-1].replace("class", "").replace("'", "").strip()
+        # Highlight the functions that return a Decimal, as these are ones
+        # we need to implement
+        if s == "decimal.Decimal":
+            s = g.d + s + g.n
+        return s
+    def ZeroArgs(name, x, us=""):
+        n = f"x.{us}{name}{us}()"
+        seen.append(n)
+        try:
+            z = eval(n)
+            if show:
+                print(f"{n:{w}s}   {Type(z)}")
+        except Exception as e:
+            print(t.format(**locals()))
+    def OneArg(name, x, y, us=""):
+        n = f"x.{us}{name}{us}(y)"
+        seen.append(n)
+        try:
+            z = eval(n)
+            if show:
+                print(f"{n:{w}s}   {Type(z)}")
+        except Exception as e:
+            print(t.format(**locals()))
+    def TwoArgs(name, x, y, z, us=""):
+        n = f"x.{us}{name}{us}(y, z)"
+        seen.append(n)
+        try:
+            t = eval(n)
+            if show:
+                print(f"{n:{w}s}   {Type(z)}")
+        except Exception as e:
+            print(t.format(**locals()))
+    print("Missing stuff:  format getattribute")
+    if 1:   # Functions with double underscores
+        f = partial(ZeroArgs, us="__")
+        for name in '''abs bool ceil complex copy neg pos float floor 
+            hash int reduce repr round sizeof str trunc
+            neg pos
+            '''.split():
+            f(name, x)
+        #
+        f = partial(OneArg, us="__")
+        for name in '''add deepcopy divmod eq floordiv ge gt
+            le lt mod mul ne pow radd rdivmod rfloordiv rmod rmul rpow
+            rsub rtruediv sub truediv
+            '''.split():
+            f(name, x, y)
+        #
+        f = partial(TwoArgs, us="__")
+        for name in '''
+            '''.split():
+            f(name, x, y, 2)
+    if 1:   # Regular functions
+        f = ZeroArgs
+        for name in '''conjugate copy_abs copy_abs copy_negate exp ln log10
+            logb logical_invert next_minus next_plus normalize
+            number_class radix sqrt to_eng_string to_integral to_integral_exact
+            to_integral_value
+            '''.split():
+            if name in "logical_invert".split():
+                f(name, X)
+            else:
+                f(name, x)
+        #
+        f = OneArg
+        for name in '''compare compare_signal compare_total compare_total_mag
+            copy_sign logical_and logical_or logical_xor max max_mag min
+            min_mag next_toward quantize remainder_near rotate same_quantum
+            scaleb shift
+            '''.split():
+            if name in '''logical_and logical_or logical_xor rotate scaleb
+                          shift'''.split():
+                if name in "rotate scaleb shift".split():
+                    f(name, X, D(1))
+                else:
+                    f(name, X, Y)
+            else:
+                f(name, x, y)
+        #
+        f = TwoArgs
+        for name in '''fma
+            '''.split():
+            f(name, x, y, 2)
+    if 1:   # List the functions we've seen
+        print(f"\nList of {len(seen)} functions examined:")
+        seen = [i[2:] for i in sorted(seen)]
+        for i in Columnize(seen, indent=" "*2):
+            print(i)
 def AddMethods():
     '''Routine to add the necessary methods so that dec objects infect
     routine calculations with their type.
@@ -240,9 +352,15 @@ def AddMethods():
         Add(name)
     for name in '''add'''.split():
         Add(name, "other")
-AddMethods()
 
 if 1:
+    # Show the return types of decimal.Decimal's methods
+    Signatures()
+    exit()
+if 0:
+    AddMethods()
+    exit()
+if 0:
     from lwtest import run, raises, assert_equal, Assert
     d = dec("1.2345")
     e = -d
@@ -255,7 +373,6 @@ if 1:
     exit()
 if 0:
     d = decimal.Decimal.__dict__
-    from columnize import Columnize
     for i in Columnize(d.keys()):
         print(i)
     exit()
