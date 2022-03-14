303dd0d97c5b93c02cfa5c67a782c3cd1998d2e2
Don Peterson <someonesdad1@gmail.com>
2022-03-14 20:47:57 +0100

    Updated

--- pgm/7bit.py
+++ pgm/7bit.py
@@ -24,7 +24,9 @@ if 1:   # Imports
     from pdb import set_trace as xx
 if 1:   # Custom imports
     from wrap import dedent
-    from get import GetLines
+    if 0:
+        import debug
+        debug.SetDebugger()
 if 1:   # Global variables
     control_chars = {
         0:"nul", 1:"soh", 2:"stx", 3:"etx", 4:"eot", 5:"enq", 6:"ack", 7:"bel",
@@ -33,89 +35,103 @@ if 1:   # Global variables
         23:"etb", 24:"can", 25:"em", 26:"sub", 27:"esc", 28:"fs", 29:"gs",
         30:"rs", 31:"us",
     }
-def RemoveASCII(s):
-    '''Return True if the string s has a non-ASCII character in it.
-    '''
-    if not hasattr(RemoveASCII, "func"):
-        # Create a dictionary for str.translate to remove ASCII
-        # characters.
-        mapping = {}
-        for i in range(0x20, 0x80):
-            mapping[i] = None
-        # Also remove whitespace characters
-        for i in (9, 10, 11, 12, 13):
-            mapping[i] = None
-        RemoveASCII.func = lambda s: s.translate(mapping)
-    return bool(RemoveASCII.func(s))
+if 1:   # Utility
+    def Error(msg, status=1):
+        print(msg, file=sys.stderr)
+        exit(status)
+    def Usage(status=1):
+        name = sys.argv[0]
+        print(dedent(f'''
+        Usage:  {name} [options] file1 [file2 ...]
+            Show the non-ASCII characters in the files by character with the line
+            numbers that character occurs on.  Column and line numbers are 1-based.
+            Whitespace is ignored.
+        Options
+            -c  Show column numbers too with line_number:column_number format.
+            -l  Just print out the file name if a non-ASCII character is found.
+            -w  Wrap lines to make easier to read.
+        '''))
+        exit(status)
+    def ParseCommandLine(d):
+        d["-c"] = False     # Use linenum:col format
+        d["-l"] = False     # Only print the filename
+        d["-w"] = False     # Wrap lines
+        try:
+            optlist, args = getopt.getopt(sys.argv[1:], "chlw")
+        except getopt.GetoptError as str:
+            msg, option = str
+            Error(msg)
+        for o, a in optlist:
+            if o[1] in list("clw"):
+                d[o] = not d[o]
+            elif o == "-h":
+                Usage(0)
+        if not args:
+            Usage()
+        return args
+if 1:   # Core functionality
+    def Codepoint(c):
+        '''Return a hex string representing the codepoint.  The string will 
+        be 9 characters long.
+        '''
+        h = f"U+{c:05x}:" if c > 0xffff else f"U+{c:04x}:"
+        return f"{h:9s}"
+    def RemoveASCII(s):
+        'Return True if the string of bytes s has a non-ASCII character in it'
+        # Create a str.translate tool in RemoveASCII.func to remove the
+        # ASCII characters in a string.
+        if not hasattr(RemoveASCII, "func"):
+            delete = ""
+            for i in range(0x20, 0x80):
+                delete += f"{i:02x}"
+            # Also remove whitespace characters
+            for i in (9, 10, 11, 12, 13):
+                delete += f"{i:02x}"
+            delete = bytes.fromhex(delete)
+            RemoveASCII.func = lambda s: s.translate(None, delete)
+        return bool(RemoveASCII.func(s))
+    def Header(c):
 
-def Usage(status=1):
-    name = sys.argv[0]
-    print(dedent(f'''
-    Usage:  {name} [options] file1 [file2 ...]
-      Show the non-ASCII characters in the files by character with the line
-      numbers that character occurs on.  Column and line numbers are 1-based.
-      Whitespace is ignored.
-    
-    Options
-      -c  Show column numbers too with line_number:column_number format.
-      -l  Just print out the file name if a non-ASCII character is found.
-      -w  Wrap lines to make easier to read.
-    '''))
-    exit(status)
-def Error(msg, status=1):
-    print(msg, file=sys.stderr)
-    exit(status)
-def ParseCommandLine(d):
-    d["-c"] = False     # Use linenum:col format
-    d["-l"] = False     # Only print the filename
-    d["-w"] = False     # Wrap lines
-    try:
-        optlist, args = getopt.getopt(sys.argv[1:], "clw")
-    except getopt.GetoptError as str:
-        msg, option = str
-        Error(msg)
-    for o, a in optlist:
-        if o[1] in list("clw"):
-            d[o] = not d[o]
-    if not args:
-        Usage()
-    return args
-def Codepoint(c):
-    '''Return a hex string representing the codepoint.  The string will 
-    be 9 characters long.
-    '''
-    if c > 0xffff:
-        h = f"U+{c:05x}:"
-    else:
-        h = f"U+{c:04x}:"
-    return f"{h:9s}"
-def ProcessLine(line, file, linenum, nonascii, d):
-    for i, c in enumerate(line):
-        if not (0x20 <= ord(c) < 0x80):
-            ln = f"{linenum}:{i + 1}" if d["-c"] else f"{linenum}"
-            nonascii[ord(c)].append(ln)
-def Header(c):
-    '''Return a header string of the form e.g.
-        ' ∞  U+221e:'
-         --- ------
-          a    b
-    where a is the 3-character string representing the non-ASCII
-    character (either the Unicode character or e.g. 'esc' for the escape
-    character) and b is the Unicode codepoint form.
-    '''
-    char = control_chars[c] if c in control_chars else chr(c)
-    cp = Codepoint(c)
-    return f"  {char:^3s} {cp}"
-def ProcessFile(file, d):
-    # Dictionary to keep track of non-ASCII characters found
-    nonascii = defaultdict(list)
-    try:
+        '''c is an integer from 0 to 255.  Return a header string of the
+        form e.g.
+            ' ∞  U+221e:'
+            --- ------
+            a    b
+        where a is the 3-character string representing the non-ASCII
+        character (either the Unicode character or e.g. 'esc' for the escape
+        character) and b is the Unicode codepoint form.
+        '''
+        char = control_chars[c] if c in control_chars else chr(c)
+        cp = Codepoint(c)
+        return f"  {char:^3s} {cp}"
+    def GetLines(file):
+        '''Read the data in from the file as binary.  Split on newlines and
+        return the list of byte strings representing each line.
+        '''
+        return open(file, "rb").read().split(b'\n')
+    def ProcessLine(line, file, linenum, nonascii):
+
+        '''For the line of the indicated file (line is a bytes object),
+        find any bytes that are not 7-bit ASCII characters and put them
+        into the dict nonascii.  nonascii is keyed by the non-7-bit
+        character and each value is a list of the file and line number
+        where the offending byte was found.
+
+        '''
+        for i, c in enumerate(line):
+            if not (0x20 <= c < 0x80):
+                # d["-c"] means to show column numbers too
+                value = f"{linenum}:{i + 1}" if d["-c"] else f"{linenum}"
+                nonascii[c].append(value)
+    def ProcessFile(file, d):
+        # Dictionary to keep track of non-ASCII bytes found
+        nonascii = defaultdict(list)
         for linenum, line in enumerate(GetLines(file)):
             if RemoveASCII(line):
                 if d["-l"]: 
                     print(file)
                     return
-                ProcessLine(line, file, linenum + 1, nonascii, d)
+                ProcessLine(line, file, linenum + 1, nonascii)
         if nonascii:
             print(file)
             for c in sorted(nonascii):
@@ -135,8 +151,6 @@ def ProcessFile(file, d):
                             print(line)
                 else:
                     print(f"{Header(c)}{linenumbers}")
-    except Exception as e:
-        print(f"{file}:  {str(e)}", file=sys.stderr)
 if __name__ == "__main__":
     d = {}
     files = ParseCommandLine(d)
---
+++ pgm/mpg.py
@@ -0,0 +1,114 @@
+'''
+Print cost of gas
+'''
+ 
+if 1:  # Copyright, license
+    # These "trigger strings" can be managed with trigger.py
+    #∞copyright∞# Copyright (C) 2022 Don Peterson #∞copyright∞#
+    #∞contact∞# gmail.com@someonesdad1 #∞contact∞#
+    #∞license∞#
+    #   Licensed under the Open Software License version 3.0.
+    #   See http://opensource.org/licenses/OSL-3.0.
+    #∞license∞#
+    #∞what∞#
+    # Print cost of gas
+    #∞what∞#
+    #∞test∞# #∞test∞#
+    pass
+if 1:   # Standard imports
+    import getopt
+    import os
+    import pathlib
+    import sys
+    from pdb import set_trace as xx
+if 1:   # Custom imports
+    from wrap import wrap, dedent
+    from f import flt
+    from clr import Clr
+if 1:   # Global variables
+    P = pathlib.Path
+    ii = isinstance
+    c = Clr()
+    class g:  pass
+    g.mpg = flt(18)     # mpg of your default car
+    g.tank = flt(26)    # Tank volume in gallons of your car
+    g.name = "2011 Suburban"  # Name of default car
+    g.mpg.rtz = g.mpg.rtdp = True
+if 1:   # Utility
+    def Error(*msg, status=1):
+        print(*msg, file=sys.stderr)
+        exit(status)
+    def Usage(status=1):
+        print(dedent(f'''
+        Usage:  {sys.argv[0]} [options] dpg [mpg]
+          Print a table of the cost in $ to drive a distance in miles given
+          mpg (miles per gallon) and dpg ($ per gallon).  The default mpg
+          is {g.mpg}.
+        '''))
+        exit(status)
+    def ParseCommandLine(d):
+        d["-e"] = 2000      # Ending number of miles driven
+        d["-n"] = g.name    # Name of vehicle
+        d["-t"] = g.tank    # Tank volume in gallons
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], "e:hn:t:", "help")
+        except getopt.GetoptError as e:
+            print(str(e))
+            exit(1)
+        for o, a in opts:
+            if o[1] in list(""):
+                d[o] = not d[o]
+            elif o == "-e":
+                d["-e"] = abs(int(a))
+            elif o in ("-h", "--help"):
+                Usage(status=0)
+            elif o == "-n":
+                d["-n"] = a
+            elif o == "-t":
+                d["-t"] = abs(flt(a))
+        if len(args) not in (1, 2):
+            Usage()
+        return args
+if 1:   # Core functionality
+    def Table(mpg, dpg):
+        'Table for miles per gallon and dollars per gallon'
+        # This is meant to print in at least an 80 column window
+        print(dedent(f'''
+        Car is {d["-n"]}
+            Car mileage                 {mpg} miles/gallon
+            Cost of fuel                {dpg} $/gallon
+            Tank volume in gallons      {d["-t"]}
+
+        Cost in $ vs. miles driven
+        '''))
+        w = 7   # Width of first column
+        n = 4   # First column number width
+        W = 6   # Width of table's columns
+        # Column header
+        print(f"{'Miles':^79s}")
+        print(f"{'':{w}s}", end="")
+        for m in range(0, 100, 10):
+            print(f"{m:^{W}d}", end=" ")
+        print()
+        # Hyphens for column markers
+        print(f"{'':{w}s}", end="")
+        for m in range(0, 100, 10):
+            print(f"{'-'*W:^{W}s}", end=" ")
+        print()
+        # Table detail
+        for M in range(0, d["-e"] + 1, 100):
+            s = f"{M:{n}d}" + (" "*(w - n))
+            print(f"{s}", end="")
+            for m in range(0, 100, 10):
+                miles = M + m
+                gallons = flt(miles/mpg)
+                cost = flt(gallons*dpg)
+                print(f"{cost!s:^{W}s}", end=" ")
+            print()
+
+if __name__ == "__main__":
+    d = {}      # Options dictionary
+    args = ParseCommandLine(d)
+    dpg = flt(args[0])
+    mpg = flt(args[1]) if len(args) == 2 else g.mpg
+    Table(mpg, dpg)
