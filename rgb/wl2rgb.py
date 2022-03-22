cbce71f6f7bb972aef8b61aa122448bdd794e53d
Don Peterson <someonesdad1@gmail.com>
2022-03-22 18:56:21 +0100

    Updated

--- rgb/wl2rgb.py
+++ rgb/wl2rgb.py
@@ -21,7 +21,10 @@ Observations
 import sys
 from rgb import ColorNum
 from util import Distribute
+from interpolate import LinearInterpFunction
 from pdb import set_trace as xx 
+from f import flt
+ii = isinstance
 if 1:
     import debug
     debug.SetDebugger()
@@ -59,10 +62,81 @@ def wl2rgb(nm, gamma=0.8):
     # Scale the RGB components by i and raise to the gamma power if gamma
     # is nonzero.
     if gamma:
-        b = [(i*j)**gamma for j in a]
+        b = [float((i*j)**gamma) for j in a]
+    else:
+        b = [float(i) for i in a]
     # Make sure the numbers are on [0, 1]
     assert(all([0 <= i <=1 for i in b]))
     return ColorNum(b)
+
+def rgb2wl(colornum):
+    'Convert the indicated color to a wavelength in nm'  
+    '''
+
+    The algorithm is
+
+        - Get the integer value of the hue on [0, 255]
+        - If hue > 212 return 645 nm
+        - Otherwise hue is in [0, 211] and use a dictionary lookup
+
+    '''
+    if not ii(colornum, ColorNum):
+        raise TypeError("colornum must be a ColorNum instance")
+    if not hasattr(rgb2wl, "dict"):
+        # Cache a dictionary to convert integer hue on [0, 211] to integer
+        # wavelength in nm.
+        dict = {}
+        if 0:
+            for nm in range(380, 645):
+                cn = wl2rgb(nm, gamma=0)
+                hue = cn.HLS[0]
+                dict[hue] = nm
+            rgb2wl.dict = dict
+            # Fix missing values
+            for i in set(range(0, 212)) - set(dict):
+                dict[i] = dict[i - 1]
+        else:
+            for nm in range(380, 781):
+                cn = wl2rgb(nm, gamma=0)
+                hue = cn.HLS[0]
+                if hue > 212:
+                    dict[hue] = 645
+                else:
+                    dict[hue] = nm
+            # Fill in missing values
+            for hue in range(256):
+                i = hue
+                while hue not in dict:
+                    i -= 1
+                    try:
+                        dict[hue] = dict[i]
+                    except KeyError:
+                        if i < 0:
+                            raise Exception("Bad algorithm")
+            rgb2wl.dict = dict
+
+
+    hue = colornum.HLS[0]
+    assert(ii(hue, int) and 0 <= hue <= 255)
+    if hue > 212:
+        return 645
+    return rgb2wl.dict[hue]
+
+if 0:
+    # Print hue vs nm for every nm
+    print("Hue  nm")
+    for nm in range(380, 645):
+        cn = wl2rgb(nm, gamma=0)
+        hue = cn.HLS[0]
+        print(f"{hue:3d} {nm:3d}")
+
+    '''
+    This gives the algorithm:
+        - For hue in range 0 to 212, map per the dict from above printout.
+        - For hue from 213 to 255, return wl of 645 nm.
+        
+    '''
+    exit()
 
 if __name__ == "__main__": 
     from clr import Clr
@@ -71,7 +145,8 @@ if __name__ == "__main__":
     from frange import frange
     import colorsys
     c = Clr(override=True)
-    f = lambda x: f"({x[0]:3d}, {x[1]:3d}, {x[2]:3d})"
+    fi = lambda x: f"({x[0]:3d}, {x[1]:3d}, {x[2]:3d})"
+    square = "■"
     def Introduction():
         print(dedent(f'''
  
@@ -108,10 +183,9 @@ if __name__ == "__main__":
         (in 1 nm wavelength steps):
  
         '''))
-        s = "■"
         for nm in range(380, 781):
             a = wl2rgb(nm)
-            print(f"{c(a.rgbhex)}{s}{c.n}", end="")
+            print(f"{c(a.rgbhex)}{square}{c.n}", end="")
         print()
     def SteppedWavelengths(step, compact=False):
         gamma = 0.8
@@ -131,7 +205,7 @@ if __name__ == "__main__":
             if compact:
                 out.append(f"{c(s)}{nm}{c.n}")
             else:
-                out.append(f"{c(s)}{nm} {s!s:7s}   {f(t)}   {f(u)}{c.n}")
+                out.append(f"{c(s)}{nm} {s!s:7s}   {fi(t)}   {fi(u)}{c.n}")
             count += 1
         if compact:
             o = Columnize(out, indent=" "*2)
@@ -352,7 +426,7 @@ if __name__ == "__main__":
         values show that they use similar but not identical colors.  My
         preference is for the HLS set of colors, so that's the method I'd
         choose.
-
+ 
         '''))
     colorbands = '''v380 v396 v412 b425 b439 b453 c465 c479 c490 
                     g500 g520 g540 y550 y565 y590 o600 o614 o628
@@ -364,7 +438,7 @@ if __name__ == "__main__":
         saturation.  The following will print out each color band color and
         its gradations.  This will make a candidate for a color set.  The
         hex number shown is HLS and the L value of 0xff is not used.
-
+ 
         '''))
         R = Distribute(0, 255, n + 1)   # The 1 is added to ignore zero
         if R[0] == 0:
@@ -390,11 +464,11 @@ if __name__ == "__main__":
                     print(f"{c(m.rgbhex)}{m.hlshex}{c.n} ", end="")
             print()
         print(dedent(f'''
-
+ 
         Missing from the set are the usual saturated colors of red, green,
         blue, yellow, cyan, magenta, bright white, and the shades of gray.
         Black also needs to be on the list.  Here are these:
-
+ 
         '''))
         d = {
             "blk": ColorNum((0, 0, 0)).hlshex,
@@ -413,10 +487,10 @@ if __name__ == "__main__":
         print()
         ngray = 12
         print(dedent(f'''
-
+ 
         Remember '$' means an HLS value.  Here are {ngray} shades of gray
         in RGB hex notation:
-
+ 
         '''))
         count = 0
         for i in Distribute(0, 255, ngray):
@@ -428,26 +502,26 @@ if __name__ == "__main__":
         print()
         n = 12*21 + ngray
         print(dedent(f'''
-
+ 
         These 12 grays could be added to a 'blk' color that's got the label
         of 'k000'.
-
+ 
         A fundamental feature of this naming scheme is that the names are
         all 7 characters long (the same as the #xxyyzz hex notation) and
         you can calculate the RGB values of the color from the name.
-
+ 
         Conclusions:  this isn't a bad set of colors.  The lblu, lyel,
         lcyn, and lmag colors aren't really needed, as there are ones in
         the table that are close enough.  The grays are needed and the
         brightest one can double as lwht.  There are {n} colors in this
         set.  
-
+ 
         '''))
     def SomeNames():
         print(dedent(f'''
         Invariably, someone will want to start giving names to colors.
         Here are some of my desires for such naming:
-
+ 
             - The color should be able to be computed from the name.
             - The name should be adequately close to the perceived color.
             - I'd want the names to be short.
@@ -456,18 +530,17 @@ if __name__ == "__main__":
               computed color since they are stripped out.
             - Adjectives as words or abbreviations can be moved around in
               the name and it's still the same name.
-
+ 
         The three groups of four columns each can be labeled "dark",
         "medium", and "light".  The "medium" colors are really the 0x80
         level of lightness and these are probably colors that would get
         used pretty heavily, so the "medium" could be left off.  Then 'lt'
         and 'dk' could be used for light and dark to keep things short.
-
+ 
         Here's an attempt to come up with a set of short names:
-
+ 
         '''))
         d = {
-
             "v380": "  uv    ultvio     purple               ",
             "v396": "  vv    viovio     wine                 ",
             "v412": "  bv    bluvio     plum                 ",
@@ -489,7 +562,6 @@ if __name__ == "__main__":
             "r632": "  or    ornred     tomato               ",
             "r682": "  rr    redred     red                  ",
             "r732": "  ir    infred     cherry               ",
-
         }
         indent = " "*4
         print(f"{indent}Band   Short  Longer     Named")
@@ -498,13 +570,13 @@ if __name__ == "__main__":
             s = colornum.rgbhex
             print(f"{indent}{c(s)}{name}  {d[name]}{c.n} ")
         print(dedent(f'''
-
+ 
         One of the features of these names is that they are all six or less
         characters, consistent with on of the goals.  Part of the
         motivation for this was the 6 character hex specifications, which
         include a '@', '#', or '$' leader character identifying what the
         numbers represent (HSV, RGB, or HLS).
-
+ 
         These names are also pretty easy to make matches with a few
         characters.  For r732 I first used 'blood', which worked well but
         confused searches with the 'blu' elements.  I then chose 'cherry'
@@ -513,7 +585,7 @@ if __name__ == "__main__":
         wine, jam, jelly, orchid, indigo, mauve, iris, etc.  Wine won over
         lilac because it was shorter and a typical red wine is darker than
         our lilac plants' blooms.
-
+ 
         Because these names are both subjective and preloaded in many
         minds, many folks will argue with my picks.  Yet remember the goal
         was a six letter or less name that would describe the color
@@ -526,9 +598,105 @@ if __name__ == "__main__":
         the naming scheme was regularlized.  This is nice because I am
         often using a variety of color specifiers, such as names, hex
         strings, RGB, HSV, and HLS.  
+ 
+        '''))
+    def HueVersusFrequency():
+        print(dedent(f'''
+        It's also convenient to have an inverse to the wl2rgb function by
+        Bruton.  Here's a printout of the "frequency" and hue of
+        wavelengths in nm in 10 nm steps:
+ 
+        '''))
+        c = Clr(override=True)
+        flt(0).rtdp = True
+        i = " "*4
+        use_color = True
+        print(f"{i}Hue  'Hz'    nm")
+        for nm in range(380, 781, 10):
+            cn = wl2rgb(nm, 0)
+            h, l, s = cn.HLS
+            f = int(1e5/nm)
+            if use_color:   # Print in color
+                cl = c(cn.rgbhex)
+                print(f"{i}{cl}{h:3d}{c.n}   {f:3d}   {nm:3d}")
+            else:
+                print(f"{i}{h:3d}   {f:3d}   {nm:3d}")
+        print(dedent(f'''
+ 
+        Frequency is int(1e5/nm) for wavelength nm in nm.  Note the hue
+        ranges over only 0-212 for return values of wl2rgb().  Higher
+        numbered hues are problematic since hues wrap around to red as they
+        approach 255 from below.  This is a discontinuity we need to deal
+        with.  Here are these higher hue numbers converted to their
+        equivalent RGB values with lightness = 128 and saturation of 255:
+ 
+        '''))
+        for hue in range(200, 256, 5):
+            hls = [i/255 for i in (hue, 128, 255)]
+            rgb = colorsys.hls_to_rgb(*hls)
+            cn = ColorNum(rgb)
+            cl = c(cn.rgbhex)
+            print(f"{i}{c(cn.rgbhex)}{hue:3d}{c.n}")
+        print(dedent(f'''
+ 
+        A plot of "frequency" versus hue is nonlinear, so I decided the
+        best way to get an inverse is to create a dictionary of hue versus
+        wavelength and use that for lookups.  As soon as the equivalent
+        hue is above 212, the color automatically becomes red.  Not ideal,
+        but it's a limitation of the mapping in wl2rgb().  Ultimately, we
+        should judge wl2rgb() by how well it seems to reproduce the visible
+        spectrum to our eyes.  Here's a terminal-based approximation that
+        fits on one line:
 
         '''))
-    if 1 or len(sys.argv) > 1:
+        for wl in range(380, 781, 6):
+            cn = wl2rgb(wl)
+            print(f"{c(cn.rgbhex)}{square}", end="")
+        print(f"{c.n}")
+ 
+        print(dedent(f'''
+
+        Here's the relationship between color and wavelength.  The first
+        column is wavelength in nm.  The second column is the hue of the
+        color calculated by the wl2rgb() function.  The third column is the
+        hue of the wavelength in nm calculated by rgb2wl(); the hue is
+        shown in the color from wl2rgb(), so the two shown colors should be
+        identical if we did a good job of generating an inverse.  The Diff
+        columns show the nonzero numerical differences in hue and
+        wavelengths.  The hue differences should be zero, but the
+        wavelengths will differ by 0 or 1 for a good fit (the differences
+        of 1 come about because not all wavelengths map to an integer hue)
+        over all except the reds.
+ 
+        '''))
+        print(f"{i}           Inv    Hue    nm")
+        print(f"{i}nm     Hue Hue    Diff  Diff")
+        for nm in range(380, 781, 10):
+            print(f"{i}{nm:3d}    ", end="")
+            cn = wl2rgb(nm, gamma=0)
+            x = cn.rgbhex
+            hue = cn.HLS[0]
+            print(f"{c(x)}{hue:03d}{c.n} ", end="")
+            # Convert wl2rgb() color's hue back to wavelength
+            nm_new = rgb2wl(cn)
+            cn_new = wl2rgb(nm_new, gamma=0)
+            hue_new = cn_new.HLS[0]
+            print(f"{c(cn_new.rgbhex)}{hue_new:03d}{c.n}   ", end="")
+            hue_diff = hue_new - hue
+            hue_diff = str(hue_diff) if hue_diff else ""
+            nm_diff = nm_new - nm
+            nm_diff = str(nm_diff) if nm_diff else ""
+            print(f"{hue_diff:4s}   ", end="")
+            print(f"{nm_diff:4s}")
+        print(dedent(f'''
+ 
+        Not suprisingly, the inverse for wavelength is poor for the reds
+        approaching the infrared.  Note the lack of dimming because gamma
+        was set to zero in wl2rgb().
+ 
+        '''))
+
+    if len(sys.argv) > 1:
         Introduction()
         SteppedWavelengths(25)
         BasicColorNames()
@@ -536,4 +704,5 @@ if __name__ == "__main__":
         HueIntervals()
         HueGradations()
         FirstChoice(4)
-    SomeNames()
+        SomeNames()
+    HueVersusFrequency()
