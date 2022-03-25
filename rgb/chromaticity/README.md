8216365cce78692564540abcf9e6d7ba000de07b
Don Peterson <someonesdad1@gmail.com>
2022-03-25 02:42:36 +0100

    Updated

--- rgb/chromaticity/README.md
+++ rgb/chromaticity/README.md
@@ -15,3 +15,10 @@ tool.
 
 http://ultra.sdk.free.fr/docs/Image-Processing/Colors/Format/Chromaticity%20Diagrams%20Lab%20Report.htm
 provides some computed chromaticity diagrams
+
+Color matching functions (CMF)
+https://en.wikipedia.org/wiki/CIE_1931_color_space#Color_matching_functions
+These are three functions of light wavelength wl:  xbar(wl), ybar(wl), and
+zbar(wl).  These weighting functions that produce the CIE tristimulus
+values X, Y, Z when integrated with the spectral radiance over the typical
+visible range of 380 to 780 nm.
--- rgb/chromaticity/cie_data.py
+++ rgb/chromaticity/cie_data.py
@@ -1,9 +1,24 @@
 '''
+
+ToDo
+    - Write an access function that uses the data to linearly interpolate
+      to the user's wavelength.
+    - Compare the approximation to the interpolated table values.  Print
+      out % deviations.
+    - In the cie_cmf_analytical_code.zip file,
+      xyzViewer/data/intraObserverVariance_20nm.h gives data on the
+      variance at various wavelengths.  Use interpolation for these numbers
+      to estimate the standard deviation of the diffs and display them as
+      nominal normal deviates.
+
+
 From 204.xls downloaded from
 https://web.archive.org/web/20170131100357/http://files.cie.co.at/204.xls
 Thu 24 Mar 2022
 '''
 from pdb import set_trace as xx 
+from wrap import dedent
+from math import exp
 
 if 1:
     import debug
@@ -190,38 +205,122 @@ data2 = '''
 780	0.000033	0.000013	0.000000
 !Sum:23.329353	23.332036	23.334153
 '''[1:-1]
-tab = "\t"
-def GetData(data):
-    f = lambda x: float(x)
-    out = []
-    for line in data.split("\n"):
-        if line[0] == "#":
-            continue
-        if line[0] == "!":
-            sums = [f(i) for i in line[5:].split(tab)]
-            continue
-        s = line.split(tab)
-        nm = int(s[0])
-        r = [f(i) for i in s[1:]]
-        out.append((nm, r))
-    # Check sums
-    x, y, z = 0, 0, 0
-    for nm, r in out:
-        x += r[0]
-        y += r[1]
-        z += r[2]
-    g = lambda x: round(x, 6)
-    if g(x) != sums[0]:
-        print("Error:  bad x sum")
-        print(x, sums[0])
-        exit(1)
-    if g(y) != sums[1]:
-        print("Error:  bad y sum")
-        print(y, sums[1])
-        exit(1)
-    if g(z) != sums[2]:
-        print("Error:  bad z sum")
-        print(z, sums[2])
-        exit(1)
-GetData(data2)
-GetData(data1)
+indent = " "*4
+def CIE_Data(dbg=False):
+    tab = "\t"
+    def GetData(data):
+        f = lambda x: float(x)
+        out = []
+        for line in data.split("\n"):
+            if line[0] == "#":
+                continue
+            if line[0] == "!":
+                sums = [f(i) for i in line[5:].split(tab)]
+                continue
+            s = line.split(tab)
+            nm = int(s[0])
+            r = [f(i) for i in s[1:]]
+            out.append((nm, r))
+        # Check sums
+        x, y, z = 0, 0, 0
+        for nm, r in out:
+            x += r[0]
+            y += r[1]
+            z += r[2]
+        g = lambda x: round(x, 6)
+        if g(x) != sums[0]:
+            print("Error:  bad x sum")
+            print(x, sums[0])
+            exit(1)
+        if g(y) != sums[1]:
+            print("Error:  bad y sum")
+            print(y, sums[1])
+            exit(1)
+        if g(z) != sums[2]:
+            print("Error:  bad z sum")
+            print(z, sums[2])
+            exit(1)
+        return out
+    def Header():
+        print(dedent(f'''
+        # Raw data data from
+        # https://web.archive.org/web/20170131100357/http://files.cie.co.at/204.xls
+        # Downloaded 24 Mar 2022
+ 
+        '''))
+    def Output(out, suppl=False):
+        if suppl:
+            print(dedent(f'''
+            # CIE 1964 supplementary standard colorimetric observer			
+            # Structure:  (wavelength_nm, (xbar10, ybar10, zbar10))
+            # Columns: 
+            #     wavelength, nm
+            #     xbar10(wl_nm), dimensionless
+            #     ybar10(wl_nm), dimensionless
+            #     zbar10(wl_nm), dimensionless
+    
+            CIE1964_suppl_CMF = (
+            '''))
+        else:
+            print(dedent(f'''
+            # CIE 1931 standard colorimetric observer			
+            # Structure:  (wavelength_nm, (xbar, ybar, zbar))
+            # Columns: 
+            #     wavelength, nm
+            #     xbar(wl_nm), dimensionless
+            #     ybar(wl_nm), dimensionless
+            #     zbar(wl_nm), dimensionless
+    
+            CIE1931_CMF = (
+            '''))
+        i = indent
+        if dbg:
+            out = out[:10]
+        for wl, xyz in out:
+            x, y, z = xyz
+            print(f"{i}({wl:3d}, ({x:8.6f}, {y:8.6f}, {z:8.6f})),")
+        print(f")")
+        if suppl:
+            print()
+    Header()
+    out = GetData(data2)
+    Output(out, suppl=True)
+    out = GetData(data1)
+    Output(out)
+def CIE_CMF_Approx(nm):
+    '''Produce the CIE 1931 CMFs with piecewise-linear approximations.  From
+    C. Wyman, et. al., "Simple Analytic Approximations to the CIE XYZ Color
+    Matching Functions", Journal of Computer Graphics Techniques, vol. 2,
+    no. 2, 2013.  https://jcgt.org/published/0002/02/01/
+    '''
+    if not (380 <= nm <= 780):
+        raise ValueError(f"'{nm}' must be between 380 and 780 nm")
+    T = lambda boolean, a, b: a if boolean else b
+    t1 = (nm - 442)*T(nm < 442, 0.0624, 0.0374)
+    t2 = (nm - 599.8)*T(nm < 599.8, 0.0264, 0.0323)
+    t3 = (nm - 501.1)*T(nm < 501.1, 0.049, 0.0382)
+    x = 0.362*exp(-t1**2/2) + 1.056*exp(-t2**2/2) - 0.065*exp(-t3**2/2)
+    t1 = (nm - 568.8)*T(nm < 568.8, 0.0213, 0.0247)
+    t2 = (nm - 530.9)*T(nm < 530.9, 0.0613, 0.0322)
+    y = 0.821*exp(-t1**2/2) + 0.286*exp(-t2**2/2)
+    t1 = (nm - 437)*T(nm < 437, 0.0845, 0.0278)
+    t2 = (nm - 459)*T(nm < 459, 0.0385, 0.0725)
+    z = 1.217*exp(-t1**2/2) + 0.681*exp(-t2**2/2)
+    return x, y, z
+def CIE_CMF_Approximation(dbg=False, nm_step=5):
+    i = indent
+    if dbg:
+        nm_step = 30
+    print(dedent(f'''
+    # Analytical approximation to CIE 1931 CMF\n
+    CIE1931_CMF = (
+    '''))
+    for nm in range(380, 781, nm_step):
+        x, y, z = CIE_CMF_Approx(nm)
+        print(f"{i}({nm:3d}, ({x:8.6f}, {y:8.6f}, {z:8.6f})),")
+
+if __name__ == "__main__": 
+    dbg = 0
+    if 1:
+        CIE_Data(dbg=dbg)
+    CIE_CMF_Approximation(dbg=dbg)
