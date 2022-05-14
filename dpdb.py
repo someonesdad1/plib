ece1d36787cd4ad34b6e61252d84940a66e2c0bb
Don Peterson <someonesdad1@gmail.com>
2022-05-14 04:24:21 +0100

    Updated

---
+++ dpdb.py
@@ -0,0 +1,90 @@
+'''
+
+This module is what I use to colorize parts of the python debugger output
+using the color.py module.
+
+    - The 'clr' command is used to turn colorizing on and off; it's
+      argument is a boolean that, if true, turns colorizing on.
+
+    - The three things I wanted were:
+        - Colorize the current line in a list command
+        - Colorize the current line, highlighting the file name, line
+          number and function name
+        - Make debugger error messages stand out
+
+    My universal use of the debugger has always been to set the variable 
+    xx to pdb.set_trace.  When inserted in a chunk of code as xx(), the
+    code stops in the debugger at that point.  Later python 3 versions used
+    the reserved word 'breakpoint' for this.
+
+    In any code I want to debug, I insert the line
+        from dpdb import set_trace as xx 
+    and get the requisite behavior.
+'''
+from pdb import Pdb
+import re
+import sys
+from wrap import dedent
+from color import TRM as t
+from pathlib import Path
+if 1:   # Colorizing strings
+    t.current_line = t("cynl")
+    t.directory = t("brnl")
+    t.filename = t("trq")
+    t.linenum = t("yell")
+    t.function = t("lavl")
+    t.error = t("ornl")
+if 1:   # Regular expressions
+    # Identify current line in list command
+    t.list = re.compile(r"^\d+\s->\s.*$")
+    # Identify current line when stepping
+    t.curr = re.compile(r'''
+        ^>\s        # Beginning of line is '> '
+        ([^(]*?)    # Match up to parentheses for dir/filename
+        \((.*?)\)   # Get text in parentheses for line number
+        ([^\n]*)    # Get text up to newline for function name
+        (\n.*)$     # String to end of line for current line
+        ''', re.M|re.X)
+
+class DPdb(Pdb):
+    def current_stopped_line(self, file, linenum, func, remainder):
+        print("> ", end="")
+        # Only colorize the file name portion
+        p = Path(file)
+        print(f"{t.directory}{p.parent}/", end="")
+        print(f"{t.filename}{p.name}{t.n} ", end="")
+        print(f"{t.linenum}{linenum}{t.n} ", end="")
+        print(f"{t.function}{func}{t.n}", end="")
+        print(f"{remainder}")
+    def message(self, msg):
+        # Look for a current line being printed with the list command.
+        # This will be a line number followed by '->'.
+        dec = ""
+        mo = t.list.match(msg)
+        if mo:
+            dec = f"{t.current_line}"
+        # Look for the current stopped line
+        mo = t.curr.match(msg)
+        if mo:
+            file, linenum, func, remainder = mo.groups()
+            self.current_stopped_line(file, linenum, func, remainder)
+        else:
+            t.print(f"{dec}{msg}")
+    def error(self, msg):
+        print(f"{t.error}", end="")
+        t.print('***', msg, file=self.stdout)
+    def do_clr(self, var):
+        'Toggle colorizing'
+        a = bool(var)
+        t.current_line = t("cynl") if a else ""
+        t.directory = t("brnl") if a else ""
+        t.filename = t("trq") if a else ""
+        t.linenum = t("yell") if a else ""
+        t.function = t("lavl") if a else ""
+        t.error = t("ornl") if a else ""
+
+def set_trace(*, header=None):
+    pdb = DPdb()
+    if header is not None:
+        pdb.message(header)
+    pdb.set_trace(sys._getframe().f_back)
