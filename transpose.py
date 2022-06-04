2848ecf3c20916cb6bb148a4bb1e5cf41dad6f0f
Don Peterson <someonesdad1@gmail.com>
2022-06-04 19:45:44 +0100

    Updated

--- transpose.py
+++ transpose.py
@@ -1,51 +1,10 @@
 '''
-- Add --test option and make it a command line too for text files.
-- Default splitting of lines for fields is str.split()
-- Options
-    - -c specifies the regex that marks a line as a comment, which
-      is ignored.  Default is '^\s*#'.
-    - --csv Parse as a CSV file
-    - -s Specify string to split on
-    - -r Specify regexp to split on
-
+- Need to write a test for IsRectangular() that includes strings and
+  objects in each row whose Len is zero.  I suspect it will break under
+  such conditions.
+- Write & test --csv option.  Needs to write transpose in CSV too.
+ 
 Generate the transpose of a nested list (matrix or vector) of objects
-    
-    The primary use case is when dealing with a text file of numbers.
-    Often, we have data representing vectors of numbers on a single line of
-    text, but we want to input such vectors to a program like the old
-    XWindows' xplot program that needed these row vectors in column-vector
-    form.  Running this file as a script on such a text file will produce
-    the needed form.  Example:
-
-    Input file of row vectors
-        1 2 3 
-        4 5 6
-    Output of column vectors:
-        1 4
-        2 5
-        3 6
-
-    A basic data structures course will teach you that the easiest way to
-    get the transpose of a two-dimensional array stored sequentially in
-    memory is to store the array with row-major ordering, then read it out
-    with column-major ordering.  If the matrix element is a[i][j], then the
-    matrix's transposed element is a[j][i], just like you learned in basic
-    matrix algebra.
-
-    This is easy in languages like C/C++ where arrays are sequential chunks
-    of memory and you use the right-size pointer strides, but it's not as
-    trivial in python because you also need to handle row vectors, column
-    vectors, matrices, and empty matrices, all of which can be either
-    homogeneous or heterogeneous in their element types.  A further
-    complication is that you can't blindly use python's len() function, as
-    some matrix elements could be strings when you're assuming the contents
-    of a matrix are nested lists.  Thus, the Len() function needs to be
-    substituted.  It's not quite as simple a task as it first appears and
-    most of the things found in a web search turn up naive solutions, at
-    least in the context of what I wanted this function to do (naive
-    solutions assume a nested list of numbers and don't handle the corner
-    cases).  
-
 '''
 if 1:   # Header
     # Copyright, license
@@ -68,8 +27,7 @@ if 1:   # Header
         import sys
     # Custom imports
         from wrap import wrap, dedent
-        from util import IsIterable
-        from lwtest import run, raises, assert_equal, Assert
+        from lwtest import run, raises, Assert, ToDoMessage
         from get import GetLines
     # Global variables
         ii = isinstance
@@ -78,6 +36,68 @@ if 1:   # Header
         g.type = None
         g.homogenous = False
 if 1:   # Core functionality
+    def IsHomogenous(x):
+        if not g.homogenous:
+            return
+        if type(x) != g.type:
+            raise TypeError(f"{x!r} is not of type {g.type}")
+    def IsIterable(s):
+        '''Return True if s is an acceptable iterable.  Strings and dicts 
+        are iterables, but not acceptable here.
+        '''
+        if ii(s, (dict, str)):
+            return False
+        try:
+            iter(s)
+            return True
+        except TypeError:
+            return False
+    def Size(m):
+        '''Return (rows, columns) for m, which must be an iterable.  Raise
+        a TypeError if each row doesn't have the same number of columns.  If
+        m is not an Iterable, return None.
+        '''
+        if not IsIterable(m):   # m is not a sequence
+            return None
+        if not m: # Corner case:  empty sequence
+            return (0, 0)
+        rows = Len(m)
+        # Get the number of columns
+        columns = Len(m[0]) if IsIterable(m) else 0
+        if not columns:
+            # First element is a string or object, so the sequence must be
+            # a row vector with remaining items also having a Len of zero.
+            try:
+                n = len(m)
+            except TypeError:
+                # Probably an unsubscriptable object
+                breakpoint() #xx
+                n = 0
+            if n == 1:
+                return (1, 1)   # Corner case:  single element
+            else:
+                # Count the number of columns
+                for item in m:
+                    if Len(item):
+                        raise TypeError(f"Element {item!r} doesn't have Len of zero")
+                    columns += 1
+                return (1, columns)
+        else:
+            # Each row in m must have the Len of the first row
+            first_row_Len = Len(m[0])
+            for row in m:
+                if Len(row) != first_row_Len:
+                    raise TypeError(f"row {row!r} doesn't have Len of {first_row_Len}")
+            return (rows, first_row_Len)
+        raise RuntimeError("Coding error:  should never reach this point")
+    def Len(item):
+        '''Return the length of the item.  If it's an iterable that's not a
+        string or dict, the normal len() is returned; otherwise, 0 is returned.
+        '''
+        try:
+            return len(item) if IsIterable(item) else 0
+        except Exception:
+            raise TypeError(f"Can't get length of {item!r}")
     def Transpose(seq, homogenous=False):
         '''seq must be 
             1) a list or tuple of objects (i.e., a vector) 
@@ -87,7 +107,9 @@ if 1:   # Core functionality
         '''
         if not ii(seq, (list, tuple)):
             raise TypeError("seq must be a list or tuple")
+        # Number of rows and columns in original matrix
         nrows, ncols = Size(seq)
+        # Number of rows and columns in transpose
         trows, tcols = ncols, nrows
         if not nrows or not ncols:
             return []
@@ -101,94 +123,50 @@ if 1:   # Core functionality
                 item0 = item0[0]
             g.type = type(item0)
         # Set up the return matrix
-        out, row_vector, col_vector = [], 0, 0
+        output, row_vector, col_vector = [], 0, 0
         if nrows == 1 and ncols != 1:       # seq was row vector
             # Result is column vector
             col_vector = ncols
             for i in range(trows):
-                out.append([0])
+                output.append([0])
         elif nrows != 1 and ncols == 1:     # seq was column vector
             # Result is row vector
             row_vector = nrows
-            out = [0]*tcols
+            output = [0]*tcols
         else:
             # seq was a matrix
-            if not IsRectangular(seq):
-                raise ValueError("seq is not a rectangular matrix")
             for i in range(trows):
-                out.append([0])
+                output.append([0])
             for i in range(trows):
-                out[i] = [0]*tcols
+                output[i] = [0]*tcols
         # Replace the elements
         if col_vector:
             for i in range(col_vector):
                 j = seq[i][0] if IsIterable(seq[i]) else seq[i]
                 IsHomogenous(j)
-                out[i][0] = j
+                output[i][0] = j
         elif row_vector:
             for i in range(row_vector):
                 j = seq[i][0] if IsIterable(seq[i]) else seq[i]
                 IsHomogenous(j)
-                out[i] = j
+                output[i] = j
         else:
             for i in range(nrows):
                 for j in range(ncols):
                     IsHomogenous(seq[i][j])
-                    out[j][i] = seq[i][j]
+                    output[j][i] = seq[i][j]
         if 0:
             # Show the details
             print(f"{msg}")
             print(f"  seq = {seq}")
             print(f"  Size = ({nrows}, {ncols}])")
-            print("  out sequence:")
-            pp(out)
+            print("  output sequence:")
+            pp(output)
+        # Reset the global variable
         g.homogenous = False
-        return out
-    def IsRectangular(m):
-        ''' Return True if each row has the same number of columns as
-        determined by Len().
-        '''
-        rows = Len(m)
-        columns = Len(m[0])
-        return all(Len(row) == columns for row in m)
-    def IsHomogenous(x):
-        if not g.homogenous:
-            return
-        if type(x) != g.type:
-            raise TypeError(f"{x!r} is not of type {g.type}")
-    def Size(m):
-        'Return (rows, columns) for m'
-        rows = Len(m)
-        if not rows:
-            # Corner case:  empty sequence
-            return (0, 0)
-        # Count the number of columns
-        columns = 0
-        n = Len(m[0])   # Number of items in first element
-        if not n:
-            # First element is a string or object, so the sequence must be
-            # a row vector of analogous objects.  All other items must have
-            # Len of zero.
-            if len(m) == 1:
-                return (1, 1)   # Corner case:  single element
-            else:
-                for item in m:
-                    if Len(item):
-                        raise TypeError(f"Element {item!r} doesn't have Len of zero")
-                    columns += 1
-                return (1, columns)
-        else:
-            # Each item in m must have the Len of n
-            for item in m:
-                if Len(item) != n:
-                    raise TypeError(f"Non-rectangular matrix")
-            return (rows, n)
-        raise RuntimeError("Coding error:  should never reach this point")
-    def Len(item):
-        '''Return the length of the item.  If it's an iterable that's not a
-        string, the normal len() is returned; otherwise, 0 is returned.
-        '''
-        return len(item) if IsIterable(item, ignore_strings=True) else 0
+        return output
+if 0:
+    exit()
 
 if __name__ == "__main__":
     if 1:   # Test code
@@ -212,8 +190,14 @@ if __name__ == "__main__":
             m = [["abc", 1], [4.8, "Another string"]]
             expected = [["abc", 4.8], [1, "Another string"]]
             Assert(Transpose(m) == expected)
+            # Improper size
+            raises(TypeError, Transpose, [[1, 2], 3])
             # 
             m = [[1, 2, 3, 4], [5, 6, 7, 8]]
+            expected = [[1, 5], [2, 6], [3, 7], [4, 8]]
+            Assert(Transpose(m) == expected)
+            m = ((1, 2, 3, 4), (5, 6, 7, 8))
+            Assert(Transpose(m) == expected)
         def TestHomogeneity():
             v = [1, 2]
             expected = [[1], [2]]
@@ -221,16 +205,19 @@ if __name__ == "__main__":
             v = [1, "2"]
             with raises(TypeError):
                 Transpose(v, homogenous=True)
-        def TestInversion():
-            # Use lists so that they'll compare equal
+        def TestInvariant():
+            'Invariant is Transpose(Transpose(x)) == x'
+            # Note:  we use only lists because things like
+            # [1, 2, [3]] != [1, 2, tuple([3])] even though they are equal 
+            # from an element by element value comparison.
             import time
             import math
             from f import flt
             from decimal import Decimal
+            inv = lambda x: Transpose(Transpose(x)) == x
             # Vector
             v = ["a", 1, 2, 3-3j, Decimal(math.pi)]
-            w = Transpose(v)
-            Assert(Transpose(w) == v)
+            Assert(inv(v))
             # Matrix
             start = time.time()
             class g:
@@ -240,14 +227,85 @@ if __name__ == "__main__":
                     return f"g({self.x})"
                 def __repr__(self):
                     return repr(str(self))
-            m = [["a", 1, 2., g()], [1-1j, g(), g(), "glorp"]]
-            w = Transpose(m)
-            Assert(Transpose(w) == m)
+            m = [
+                ["a", 1, 2., g()],
+                [1-1j, g(), g(), "glorp"]
+            ]
+            Assert(inv(m))
+        def TestSize():
+            for m in ([], (), range(0)):
+                Assert(Size(m) == (0, 0))
+            for m in (
+                [1], tuple([1]), range(1),
+                [[1]], tuple(tuple([1])), [range(1)],
+                    ):
+                Assert(Size(m) == (1, 1))
+            Assert(not Size(""))
+            # Must get an exception if array not rectangular
+            m = [[1, tuple([1]), 3], 1]
+            raises(TypeError, Size, m)
+            m[1] = (4, 5, 6)
+            Assert(Size(m) == (2, 3))
+            # Transpose of a row vector is a column vector
+            Assert(Transpose(Size(m)) == [[2], [3]])
+        def TestIsIterable():
+            II = IsIterable
+            # Things that are not iterables
+            class c: pass
+            for i in (None, {}, "", c()):
+                Assert(not II(i))
+            # Things that are iterables
+            for i in (tuple(), [], range(1)):
+                Assert(II(i))
+        def TestIsRectangular():
+            raises(TypeError, Transpose, [[1, 2], 3])
         def RunSelfTests():
             exit(run(globals(), halt=True)[0])
     if 1:   # Script
         def Manpage():
-            breakpoint() #xx
+            print(dedent('''
+
+            This module supplies the Transpose() function which takes an argument that's a
+            nested list or nested list.  The return value will be the transpose of the
+            argument expressed as a nested list.  You can also run this file as a script
+            to take the transpose of rectangular matrixes of strings from a file.
+
+            The primary use case is when dealing with a text file of numbers.  Often, we
+            have data representing vectors of numbers on a single line of text, but we
+            want to input such vectors to a program (e.g., like the old X11 xplot program)
+            that needs these row vectors in column-vector form.  Running this file as a
+            script on such a text file will produce the needed form.  Example:
+
+            Input file of row vectors
+                1 2 3 
+                4 5 6
+            Output of column vectors:
+                1 4
+                2 5
+                3 6
+
+            Design
+            ------
+
+            A basic data structures course will teach you that the easiest way to get the
+            transpose of a two-dimensional array stored sequentially in memory is to store
+            the array with row-major ordering, then read it out with column-major
+            ordering.  If the matrix element's address is a[i][j], then the matrix's
+            transposed element is a[j][i], just like you learned in basic matrix algebra.
+
+            This is easy in languages like C/C++ where arrays are sequential chunks of
+            memory and you use the right-size pointer strides, but it's not as trivial in
+            python because you also need to handle row vectors, column vectors, matrices,
+            and empty matrices, all of which can be either homogeneous or heterogeneous in
+            their element types.  A further complication is that you can't blindly use
+            python's len() function, as some matrix elements could be strings when you're
+            assuming the contents of a matrix are nested lists.  Thus, the Len() function
+            needs to be substituted.  It's not quite as simple a task as it first appears
+            and most of the things found in a web search turn up naive solutions, at least
+            in the context of what I wanted this function to do (naive solutions assume a
+            nested list of numbers and don't handle the corner cases nor strings or
+            objects).  
+            '''))
             exit(0)
         def Error(*msg, status=1):
             print(*msg, file=sys.stderr)
@@ -268,6 +326,7 @@ if __name__ == "__main__":
               -R r    Specify regexp to split lines, case independent
               -r r    Specify regexp to split lines
               -s s    Specify string to split lines [whitespace]
+              -t      Run self tests
               -v      Verbose output:  show input and output
             '''))
             exit(status)
@@ -280,11 +339,12 @@ if __name__ == "__main__":
             d["-R"] = None      # Regexp to split lines, ignore case
             d["-r"] = None      # Regexp to split lines
             d["-s"] = None      # String to split lines
+            d["-t"] = False     # Run self-tests
             d["-v"] = False     # Verbose output
             if len(sys.argv) < 2:
                 Usage()
             try:
-                opts, args = getopt.getopt(sys.argv[1:], "c:hi:j:R:r:s:v", 
+                opts, args = getopt.getopt(sys.argv[1:], "c:hi:j:R:r:s:tv", 
                         ["help", "test", "csv"])
             except getopt.GetoptError as e:
                 print(str(e))
@@ -302,7 +362,7 @@ if __name__ == "__main__":
                     d[o] = a
                 elif o == "--csv":
                     d["--csv"] = not d["--csv"]
-                elif o == "--test":
+                elif o in ("-t", "--test"):
                     RunSelfTests()
                 elif o == "-R":
                     d[o] = re.compile(a, re.I)
@@ -346,7 +406,6 @@ if __name__ == "__main__":
             if d["-v"]:
                 print("Output:")
             PrintLines(output)
-
     d = {}      # Options dictionary
     files = ParseCommandLine(d)
     while True:
@@ -357,3 +416,4 @@ if __name__ == "__main__":
             print()
         else:
             break
+# vim: tw=90
