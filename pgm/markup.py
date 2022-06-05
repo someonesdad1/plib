4b3f2a6a75126c3b74eb04982269abe788782440
Don Peterson <someonesdad1@gmail.com>
2022-06-06 00:42:00 +0100

    Updated

--- pgm/markup.py
+++ pgm/markup.py
@@ -4,76 +4,76 @@ Interactive utility to calculate profit and markup
     - The basic use case is to estimate the order of magnitude of the
       profitability of a project.
 
-Problem variables (number in [] is default)
-    c = cost = L + F
-        L = labor cost = H*h            [0]
-            H = $/hr of labor           [20]
-            h = hours of labor          [0]
-        F = cost of parts & supplies    [0]
-    s = selling price
-    p = profit as a fraction of s
-    m = markup as a fraction of c
-    u = multiplier = s/c
+    Problem variables (number in [] is default)
+        c = cost = L + PS
+            L = labor cost = dph*hr         [0]
+                dph = $/hr of labor         [20]
+                hr = hours of labor         [0]
+            PS = cost of parts & supplies   [0]
+        s = selling price
+        p = profit as a fraction of s
+        m = markup as a fraction of c
+        u = multiplier = s/c
 
-Commands
-    - ?             Help
-    - ??            Detailed help
-    - q or quit     Quit, writing registers to disk
-    - QUIT          Force quit without rewriting to disk
-    - reset         Set to default state
-    - > 'name'      Save state to register 'name'
-    - < 'name'      Restore state to register name
-    - del name      Delete a variable
-    - del 'name'    Delete a register
-    - ls            Show listing of register names
-    - k             Toggle colorizing
-    - #             Enter editor for project's comments
-    = ! cmd         Execute a python command
+    Commands
+        - ?             Help
+        - ??            Detailed help
+        - q or quit     Quit, writing registers to disk
+        - QUIT          Force quit without rewriting to disk
+        - reset         Set to default state
+        - > 'name'      Save state to register 'name'
+        - < 'name'      Restore state to register name
+        - del name      Delete a variable
+        - del 'name'    Delete a register
+        - ls            Show listing of register names
+        - k             Toggle colorizing
+        - #             Enter editor for project's comments
+        = ! cmd         Execute a python command
 
-    At exit (but not if QUIT was used), the current state of the script is always saved in register
-    'LAST' and this state will be restored at the next invocation.
+        At exit (but not if QUIT was used), the current state of the script is always saved in register
+        'LAST' and this state will be restored at the next invocation.
 
-Local variables
+    Local variables
 
-    Any line input with '=' in it sets a variable.  Any variable that is
-    not a problem variable is considered a local variable.
-    
-Registers
-    
-    All registers are stored in an internal dictionary that is written to
-    disk at exit.  A lockfile prevents another process from writing to this
-    file if another process has it open.
+        Any line input with '=' in it sets a variable.  Any variable that is
+        not a problem variable is considered a local variable.
+        
+    Registers
+        
+        All registers are stored in an internal dictionary that is written to
+        disk at exit.  A lockfile prevents another process from writing to this
+        file if another process has it open.
 
-Equations
-    c = s*(1 - p) = L + F
-    s = c*(1 + m)
-    p = m/(1 + m) = 1 - 1/u
-    m = s/c = p/(1 - p)
-    u = 1/(1 - p) = multiplier
-    m = p*u
+    Equations
+        c = s*(1 - p) = L + F
+        s = c*(1 + m)
+        p = m/(1 + m) = 1 - 1/u
+        m = s/c = p/(1 - p)
+        u = 1/(1 - p) = multiplier
+        m = p*u
 
-The above variables are printed out when you start the program.  If they
-are not defined, the value is None.  Then you enter commands like 'c = 10'.
-When there are enough variables, all None variables are calculated and you
-can continue entering changes.  Expressions are allowed.
+    The above variables are printed out when you start the program.  If they
+    are not defined, the value is None.  Then you enter commands like 'c = 10'.
+    When there are enough variables, all None variables are calculated and you
+    can continue entering changes.  Expressions are allowed.
 
-This should also include the ability to let you enter '.' when
-you are prompted for cost.  You're then prompted for $/hr for labor (enter
-'.' to use a default value), number of hours labor, and parts cost.  This
-gives you a total cost and lets you put a cost on your time.
+    This should also include the ability to let you enter '.' when
+    you are prompted for cost.  You're then prompted for $/hr for labor (enter
+    '.' to use a default value), number of hours labor, and parts cost.  This
+    gives you a total cost and lets you put a cost on your time.
 
 
-Desired interface
-    - Message prompts for 3 of c, s, p, m, or u
-        - Need to solve given csp csm csu cpm cpu cmu spm spu smu pmu
-    - Once got requisite set, it prints report in color
-    - Then prompts you to enter changes to the variables such as
-      'c *= .9'.  These are expressions that are evaluated with the
-      above local variables.  You can also enter any other variables
-      you want.  
-    - Entering 'ns = "name"' defines a namespace and this problem's
-      variables are saved under this name.  You can't overwrite an existing
-      namespace without using 'del "name"'.
+    Desired interface
+        - Message prompts for 3 of c, s, p, m, or u
+            - Need to solve given csp csm csu cpm cpu cmu spm spu smu pmu
+        - Once got requisite set, it prints report in color
+        - Then prompts you to enter changes to the variables such as
+        'c *= .9'.  These are expressions that are evaluated with the
+        above local variables.  You can also enter any other variables
+        you want.  
+        - Entering 'ns = "name"' defines a namespace and this problem's
+        variables are saved under this name.  You can't overwrite an existing
+        namespace without using 'del "name"'.
 
 '''
 if 1:   # Header
@@ -111,6 +111,25 @@ if 1:   # Header
         t.dbg = t("ornl")
         t.err = t("redl")
         prompt = f"{t.prompt}>> {t.n}"
+        #
+        class g: pass
+        g.dbg = True
+        g.vars = {}     # Dictionary for variables
+        g.names = {     # Core variable names
+            "c": "Cost = L + PS",
+            "L": "Labor = dph*hr",
+            "dph": "Labor cost $/hour",
+            "hr": "Hours of labor", 
+            "PS": "Cost of parts & supplies",
+            "s": "Selling price", 
+            "p": "Profit as a fraction of s",
+            "m": "Markup as a fraction of c",
+            "u": "Multiplier = s/c",
+        }
+        g.core = {}
+        for i in g.names:
+            g.core[i] = None
+        g.cmds = set("? ?? q quit QUIT reset > < del ls k # !".split())
 if 1:   # Utility
     def Error(*msg, status=1):
         print(*msg, file=sys.stderr)
@@ -160,6 +179,11 @@ if 1:   # Utility
     def Err(e):
         t.print(f"{t.err}Error:  {e}")
 if 1:   # Core functionality
+    def SetUpVariables():
+        # Set up core variables
+        for i in g.core_variables:
+            g.vars[i] = None
+        g.vars["dph"] = 20  # $/hr equivalent for labor
     def Variable(s):
         name, value = [i.strip() for i in s.split("=", 1)]
         try:
@@ -168,19 +192,44 @@ if 1:   # Core functionality
             Dbg(f"name = {name!r}, value = {val}")
         except Exception as e:
             Err(e)
+if 1:   # Command dispatch
+    def Help(*p, **kw):
+        pass
+    def HELP(*p, **kw):
+        pass
+    def Quit(*p, **kw):
+        pass
+    def Reset(*p, **kw):
+        pass
+    def StoreRegister(*p, **kw):
+        pass
+    def RecallRegister(*p, **kw):
+        pass
+    def Delete(*p, **kw):
+        pass
+    def ShowState(*p, **kw):
+        print(g.vars)
+    def ToggleColorizing(*p, **kw):
+        pass
+    def EditNote(*p, **kw):
+        pass
+    def ToggleDbg(*p, **kw):
+        g.dbg = not g.dbg
+    def ExecutePythonCommand(*p, **kw):
+        cmd = p[0]
+        try:
+            exec(cmd, globals(), g.vars)
+        except Exception as e:
+            Err(e)
+if 1:   # Command loop
     def Command(s):
-        if s == "du":
-            print(g.vars)
+        if s == "ls":
+            ShowState()
         elif s == "dbg":
-            g.dbg = not g.dbg
+            ToggleDbg()
         elif s.startswith("!"):
             cmd = s[1:].strip()
-            try:
-                exec(cmd, globals(), g.vars)
-            except Exception as e:
-                Err(e)
-                
-if 1:   # Command loop
+            ExecutePythonCommand(cmd)
     def CommandLoop():
         quit = False
         while not quit:
@@ -197,11 +246,24 @@ if 1:   # Command loop
                 print(f"{t('cyn')}vars:")
                 pp(g.vars, compact=1)
                 print(f"{t.n}", end="")
-
 if __name__ == "__main__":
-    class g: pass
-    g.dbg = True
-    g.vars = {}     # Dictionary for variables
+    # This variable has to be after the dispatch functions are defined
+    g.dispatch = {
+        "?": Help,
+        "??": HELP,
+        "q": Quit,
+        "quit": Quit,
+        "QUIT": Quit,
+        "reset": Reset,
+        "<": StoreRegister,
+        ">": RecallRegister,
+        "del": Delete,
+        "ls": ShowState,
+        "k": ToggleColorizing,
+        "#": EditNote,
+        "!": ExecutePythonCommand,
+        "dbg": ToggleDbg,
+    }
     d = {}          # Options dictionary
     args = ParseCommandLine(d)
     CommandLoop()
