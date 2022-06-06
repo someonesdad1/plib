72f8882f35b4bda444e8aeb927dc902cfa09c3cb
Don Peterson <someonesdad1@gmail.com>
2022-06-06 19:42:20 +0100

    Updated

--- pgm/markup.py
+++ pgm/markup.py
@@ -1,80 +1,54 @@
 '''
 
-Interactive utility to calculate profit and markup
-    - The basic use case is to estimate the order of magnitude of the
-      profitability of a project.
+To Do
+    - Each variable will be None, an expression string, int, or flt
+    - Once basics are working
 
-    Problem variables (number in [] is default)
-        c = cost = L + PS
-            L = labor cost = dph*hr         [0]
-                dph = $/hr of labor         [20]
-                hr = hours of labor         [0]
-            PS = cost of parts & supplies   [0]
+Interactive utility to calculate the profit of a project
+
+    Problem variables
+        c = cost 
         s = selling price
         p = profit as a fraction of s
         m = markup as a fraction of c
         u = multiplier = s/c
 
     Commands
-        - ?             Help
-        - ??            Detailed help
-        - q or quit     Quit, writing registers to disk
-        - QUIT          Force quit without rewriting to disk
-        - reset         Set to default state
-        - > 'name'      Save state to register 'name'
-        - < 'name'      Restore state to register name
-        - del name      Delete a variable
-        - del 'name'    Delete a register
-        - ls            Show listing of register names
-        - k             Toggle colorizing
-        - #             Enter editor for project's comments
-        = ! cmd         Execute a python command
-
-        At exit (but not if QUIT was used), the current state of the script is always saved in register
-        'LAST' and this state will be restored at the next invocation.
-
-    Local variables
-
-        Any line input with '=' in it sets a variable.  Any variable that is
-        not a problem variable is considered a local variable.
-        
-    Registers
-        
-        All registers are stored in an internal dictionary that is written to
-        disk at exit.  A lockfile prevents another process from writing to this
-        file if another process has it open.
-
+        x expr      Enter a variable where x is a variable name
+        v = expr    Define a local variable
+        q           Quit
+        .           Show defined variables
+        ! expr      Evaluate expression
+        clr         Reset to starting state
+ 
     Equations
-        c = s*(1 - p) = L + F
-        s = c*(1 + m)
-        p = m/(1 + m) = 1 - 1/u
-        m = s/c = p/(1 - p)
-        u = 1/(1 - p) = multiplier
-        m = p*u
+        e1      s = c*(1 + m)
+        e2      c = s*(1 - p)
+        e3      m = p/(1 - p)
+        e4      p = m/(1 + m)
+        e5      u = s/c
+ 
+    Basic operation
+        - Program starts with all variables set to None
+        - As soon as you define one of the 5 variables, the others are
+          shown in terms of that variable.  You'll get a numerical solution
+          when either c or s are defined numerically.  Otherwise, you'll
+          get two lines, one in terms of c and one in terms of s.
 
-    The above variables are printed out when you start the program.  If they
-    are not defined, the value is None.  Then you enter commands like 'c = 10'.
-    When there are enough variables, all None variables are calculated and you
-    can continue entering changes.  Expressions are allowed.
+    Solutions
+        To solve the problem, you must have either c or s and one of any of
+        the other variables.
 
-    This should also include the ability to let you enter '.' when
-    you are prompted for cost.  You're then prompted for $/hr for labor (enter
-    '.' to use a default value), number of hours labor, and parts cost.  This
-    gives you a total cost and lets you put a cost on your time.
-
-
-    Desired interface
-        - Message prompts for 3 of c, s, p, m, or u
-            - Need to solve given csp csm csu cpm cpu cmu spm spu smu pmu
-        - Once got requisite set, it prints report in color
-        - Then prompts you to enter changes to the variables such as
-        'c *= .9'.  These are expressions that are evaluated with the
-        above local variables.  You can also enter any other variables
-        you want.  
-        - Entering 'ns = "name"' defines a namespace and this problem's
-        variables are saved under this name.  You can't overwrite an existing
-        namespace without using 'del "name"'.
-
+        Have        Solution
+        ----        --------
+        c, s        m from e3, p from e4
+        c, m        s from e1
+        c, p        m from e3, s from e1
+        c, u        s from e5
+        s, m        p from e4, c from e2
+        s, p        m from e3, c from e2
+        s, u        c from e5
+ 
 '''
 if 1:   # Header
     # Copyright, license
@@ -103,58 +77,39 @@ if 1:   # Header
         from f import *
     # Global variables
         ii = isinstance
-        W = int(os.environ.get("COLUMNS", "80")) - 1
-        L = int(os.environ.get("LINES", "50"))
-        # Colors
-        t.prompt = t("magl")
-        t.cmdline = t("cynl")
-        t.dbg = t("ornl")
-        t.err = t("redl")
-        prompt = f"{t.prompt}>> {t.n}"
-        #
         class g: pass
-        g.dbg = True
-        g.vars = {}     # Dictionary for variables
-        g.names = {     # Core variable names
-            "c": "Cost = L + PS",
-            "L": "Labor = dph*hr",
-            "dph": "Labor cost $/hour",
-            "hr": "Hours of labor", 
-            "PS": "Cost of parts & supplies",
-            "s": "Selling price", 
-            "p": "Profit as a fraction of s",
-            "m": "Markup as a fraction of c",
-            "u": "Multiplier = s/c",
-        }
-        g.core = {}
-        for i in g.names:
-            g.core[i] = None
-        g.cmds = set("? ?? q quit QUIT reset > < del ls k # !".split())
+        g.dbg = False
+        g.w = int(os.environ.get("COLUMNS", "80")) - 1
+        g.last_changed = None
+        prompt = ">> "
+        # Problem's variables
+        c, s, p, m, u = [0]*5
+        # User's local variables
+        vars = {}
+        # Hold solution strings
+        class sol: pass
 if 1:   # Utility
     def Error(*msg, status=1):
         print(*msg, file=sys.stderr)
         exit(status)
     def Usage(status=1):
         print(dedent(f'''
-        Usage:  {sys.argv[0]} [options] etc.
-          Explanations...
-        Options:
-            -h      Print a manpage
+        Need to write Usage statement
         '''))
         exit(status)
     def ParseCommandLine(d):
-        d["-a"] = False
-        d["-d"] = 3         # Number of significant digits
+        d["-d"] = False
+        d["-n"] = 3         # Number of significant digits
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "ad:h", 
+            opts, args = getopt.getopt(sys.argv[1:], "dn:h", 
                     ["help", "debug"])
         except getopt.GetoptError as e:
             print(str(e))
             exit(1)
         for o, a in opts:
-            if o[1] in list("a"):
+            if o[1] in list("d"):
                 d[o] = not d[o]
-            elif o in ("-d",):
+            elif o in ("-n",):
                 try:
                     d["-d"] = int(a)
                     if not (1 <= d["-d"] <= 15):
@@ -170,6 +125,11 @@ if 1:   # Utility
                 # unhandled exception
                 import debug
                 debug.SetDebugger()
+        global dbg
+        dbg = d["-d"]
+        x = flt(0)
+        x.n = d["-n"]
+        x.rtz = x.rtdp = True
         return args
     def Dbg(*p, **kw):
         if g.dbg:
@@ -178,50 +138,81 @@ if 1:   # Utility
             print(f"{t.n}", end="")
     def Err(e):
         t.print(f"{t.err}Error:  {e}")
-if 1:   # Core functionality
-    def SetUpVariables():
-        # Set up core variables
-        for i in g.core_variables:
-            g.vars[i] = None
-        g.vars["dph"] = 20  # $/hr equivalent for labor
-    def Variable(s):
-        name, value = [i.strip() for i in s.split("=", 1)]
-        try:
-            val = eval(value, globals(), g.vars)
-            g.vars[name] = val
-            Dbg(f"name = {name!r}, value = {val}")
-        except Exception as e:
-            Err(e)
-if 1:   # Command dispatch
-    def Help(*p, **kw):
-        pass
-    def HELP(*p, **kw):
-        pass
-    def Quit(*p, **kw):
-        pass
-    def Reset(*p, **kw):
-        pass
-    def StoreRegister(*p, **kw):
-        pass
-    def RecallRegister(*p, **kw):
-        pass
-    def Delete(*p, **kw):
-        pass
-    def ShowState(*p, **kw):
-        print(g.vars)
-    def ToggleColorizing(*p, **kw):
-        pass
-    def EditNote(*p, **kw):
-        pass
-    def ToggleDbg(*p, **kw):
-        g.dbg = not g.dbg
-    def ExecutePythonCommand(*p, **kw):
-        cmd = p[0]
-        try:
-            exec(cmd, globals(), g.vars)
-        except Exception as e:
-            Err(e)
 if 1:   # Command loop
+    def PrintSolution():
+        notyet = "-"
+        def NotYet():
+            sol.c = str(c) if c else notyet
+            sol.s = str(s) if s else notyet
+            sol.m = f"{100*m}%" if m else notyet
+            sol.p = f"{100*p}%" if p else notyet
+            sol.u = str(u) if u else notyet
+        def e1(): return c*(1 + m)
+        def e2(): return s*(1 - p)
+        def e3(): return p/(1 - p)
+        def e4(): return m/(1 + m)
+        def e5(): return s/c
+            
+        # Get the solution strings
+        if (not c and not s) or g.last_changed is None:
+            NotYet()
+        elif g.last_changed == "c":
+            if s:
+                pass
+            elif p:
+                m, s = e3(), e1()
+            elif m:
+            elif u:
+            else:
+                NotYet()
+        elif g.last_changed == "s":
+        elif g.last_changed == "m":
+        elif g.last_changed == "p":
+        elif g.last_changed == "u":
+        else:
+            raise Exception(f"{g.last_changed!r} is bad value for g.last_changed")
+
+        # Print the results
+        ncols = 5
+        w = g.w//ncols
+        out, ln = [], []
+        # Title line
+        for i in "Sell Cost Profit Markup Mult".split():
+            ln.append(f"{i:^{w}s}")
+        out.append(''.join(ln))
+
+        print(dedent(f'''
+        Cost    SellingPrice   Profit    Markup     Multiplier
+        cost = {c}    selling price = {s}
+        profit {pr}    markup {mr}       multiplier {u}
+        '''))
+    def Variable(name, value):
+        global c, s, m, p, u
+        class Oops(Exception): pass
+        try:
+            if name == "c":
+                c = flt(eval(value, globals(), vars))
+            elif name == "s":
+                s = flt(eval(value, globals(), vars))
+            elif name == "m":
+                m = flt(eval(value, globals(), vars))
+            elif name == "p":
+                p = flt(eval(value, globals(), vars))
+            elif name == "u":
+                u = flt(eval(value, globals(), vars))
+            else:
+                raise Oops("Programming bug")
+        except Oops:
+            raise
+        except Exception as e:
+            print(f"{e}")
+        g.last_changed = name
+        if dbg:
+            print("    c =", c)
+            print("    s =", s)
+            print("    m =", m)
+            print("    p =", p)
+            print("    u =", u)
     def Command(s):
         if s == "ls":
             ShowState()
@@ -231,39 +222,29 @@ if 1:   # Command loop
             cmd = s[1:].strip()
             ExecutePythonCommand(cmd)
     def CommandLoop():
-        quit = False
-        while not quit:
-            print(prompt, end="")
-            s = input().strip()
-            t.out()     # Turn off prompt colorizing
-            if s in "q quit".split():
-                quit = True
-            elif "=" in s:
-                Variable(s)
-            elif s:
-                Command(s)
-            if 1:
-                print(f"{t('cyn')}vars:")
-                pp(g.vars, compact=1)
-                print(f"{t.n}", end="")
+        while True:
+            s = input(prompt).strip()
+            if s:
+                first_character = s[0]
+                remainder = s[1:] if len(s) > 1 else ""
+                if first_character == "q":
+                    break
+                elif first_character in "cspmu":
+                    if not remainder:
+                        print("Must include a value")
+                        continue
+                    Variable(first_character, remainder)
+                else:
+                    Command(s)
+                PrintSolution()
+if 1:
+    dbg = 1
+    c = 1 
+    Variable("s", "sqrt(2)**2")
+    PrintSolution()
+    exit()
+
 if __name__ == "__main__":
-    # This variable has to be after the dispatch functions are defined
-    g.dispatch = {
-        "?": Help,
-        "??": HELP,
-        "q": Quit,
-        "quit": Quit,
-        "QUIT": Quit,
-        "reset": Reset,
-        "<": StoreRegister,
-        ">": RecallRegister,
-        "del": Delete,
-        "ls": ShowState,
-        "k": ToggleColorizing,
-        "#": EditNote,
-        "!": ExecutePythonCommand,
-        "dbg": ToggleDbg,
-    }
     d = {}          # Options dictionary
     args = ParseCommandLine(d)
     CommandLoop()
