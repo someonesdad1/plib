21caac8ab03e88856885a0208f5383b04e035a12
Don Peterson <someonesdad1@gmail.com>
2023-04-29 01:37:30 +0100

    Updated

--- si.py
+++ si.py
@@ -10,7 +10,7 @@ SI prefixes
 # See http://opensource.org/licenses/OSL-3.0.
 #
 import sys
-from math import log10 as _log10
+import math
 from fmt import fmt
 
 have_mpmath = False
@@ -19,9 +19,12 @@ try:
     have_mpmath = True
 except ImportError:
     pass
+if 1:
+    import debug
+    debug.SetDebugger()
 ii = isinstance
 
-class _si(dict):
+class SI(dict):
     '''Class to present a bidict behavior for both SI prefix strings
     and exponents.  Index as a dictionary with an SI prefix string and
     you'll have the corresponding base 10 logarithm returned.  Call as
@@ -67,6 +70,7 @@ class _si(dict):
             -18: "a",
             -21: "z",
             -24: "y",
+            0: "",
             1: "da",
             2: "h",
             3: "k",
@@ -79,10 +83,20 @@ class _si(dict):
             24: "Y",
         }
         return d[x]
-
-si = _si()
-
-def SI(x, eng=False):
+# Convenience instance 
+si = SI()
+ 
+def GetSIExponent(e: int):
+    '''Return None if not (-24 <= e <= 26).  Otherwise, return the nearest
+    integer that is a suitable exponent division by 3.
+    '''
+    if not (-24 <= e <= 26):
+        return None
+    e = 24 if e > 24 else e
+    while abs(e) % 3 != 0:
+        e -= 1
+    return e
+def GetSI(x, eng=False):
     '''
     - Call with a single character string that is an SI prefix and
       you'll have the corresponding power of 10 returned.  An
@@ -95,9 +109,9 @@ def SI(x, eng=False):
         p is the appropriate SI prefix
       
       If x is 0, then (x, 0, "") is returned.
-
+ 
       If x is in (1/1000, 1000), then (x, x, "") is returned.
-
+ 
       If x is not within a factor of 1000 of
       the largest or smallest SI prefix, then None is returned for t
       and p.  The exception is when x is 0, in which case (x, 0, "")
@@ -120,22 +134,80 @@ def SI(x, eng=False):
             raise ValueError("'%s' is not a recognized SI prefix" % x)
     elif ii(x, (int, float)) or (have_mpmath and ii(x, M.mpf)):
         sgn = -1 if x < 0 else 1
+        no_match = (x, None, None)
         if not x:
             return (x, 0, "")
         if ii(x, (int, float)):
-            e = int(log10(x))
+            e = int(math.floor(math.log10(x)))
         else:
-            e = int(M.log10(x))
+            e = int(M.floor(M.log10(x)))
         # See if exponent is in range
-        if not(-24 <= e <= 27):
-            return (x, None, None)
-
+        e1 = GetSIExponent(e)
+        if e1 is None:
+            return no_match
+        assert(e1 <= e)
+        correction = e - e1
+        assert(correction in (0, 1, 2))
+        # Get significand
         t = fmt.significand(abs(x))
+        t = M.mpf(t) if have_mpmath and ii(x, M.mpf) else float(t)
+        # Adjust exponent to be a power of 3
+        p = si(e1)
+        t *= 10**correction
         return (x, t, p)
     else:
         raise ValueError("x must be string, float (mpmath.mpf OK too), or integer")
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
+    from lwtest import Assert
+    def Testing():
+        # float
+        a = 6.2
+        for e in range(-25, 28):
+            b = a*10**e
+            e1 = GetSIExponent(e)
+            expected_p = None
+            if e1 in si.values():
+                expected_p = si(e1)  # Yep, an incestuous test
+                correction = e - e1
+            if expected_p is not None:
+                x, t, p = GetSI(b)
+                Assert(p == expected_p)
+                Assert(correction in (0, 1, 2))
+                x1 = round(float(t), 2)
+                b1 = 10**correction*round(float(fmt.significand(t)), 2)
+                Assert(str(x1) == str(b1))
+            else:
+                if e < -24 or e > 25:
+                    Assert(GetSI(b) == (b, None, None))
+                elif 0 <= e < 3:
+                    Assert(GetSI(b) == (b, b, ""))
+                else:
+                    raise Exception("Bug")
+        exit() #xx
+        # mpmath
+        a = M.mpf(6.2)
+        for e in range(-25, 28):
+            b = a*10**e
+            e1 = GetSIExponent(e)
+            expected_p = None
+            if e1 in si.values():
+                expected_p = si(e1)
+            if expected_p is not None:
+                x, t, p = GetSI(b)
+                Assert(p == expected_p)
+                Assert(e - e1 in (0, 1, 2))
+                x1 = round(M.mpf(t), 4)
+                Assert(str(x1) == str(b))
+            else:
+                if b:
+                    Assert(GetSI(b) == (b, None, None))
+                else:
+                    Assert(GetSI(b) == (b, 0, ""))
+        for e in range(-25, 28):
+            x = eval(f"M.mpf(6.2e{e})")
+            print(GetSI(x))
+    Testing()
     names = {
         -24: "yocto",
         -21: "zepto",
