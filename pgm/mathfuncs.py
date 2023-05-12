cc4713b446f9d6898a6e101d3f8575957a06e569
Don Peterson <someonesdad1@gmail.com>
2023-05-12 17:30:17 +0100

    Updated

--- pgm/mathfuncs.py
+++ pgm/mathfuncs.py
@@ -1,5 +1,7 @@
 '''
 Identify the functions in math/cmath and print out their return types
+
+Done for python 3.9.10
 '''
 if 1:   # Header
     if 1:   # Copyright, license
@@ -26,10 +28,72 @@ if 1:   # Header
         from wrap import wrap, dedent
         from color import Color, TRM as t
         from columnize import Columnize
+        if 1:
+            import debug
+            debug.SetDebugger()
     if 1:   # Global variables
         ii = isinstance
         W = int(os.environ.get("COLUMNS", "80")) - 1
         L = int(os.environ.get("LINES", "50"))
+        # name [args] [returns]
+        mathfuncs = '''
+            acos        [float]; [float]
+            acosh       [float]; [float]
+            asin        [float]; [float]
+            asinh       [float]; [float]
+            atan        [float]; [float]
+            atan2       [float, float]; [float]
+            atanh       [float]; [float]
+            ceil        [float]; [int]
+            comb        [int, int]; [int]
+            copysign    [float, float]; [float]
+            cos         [float]; [float]
+            cosh        [float]; [float]
+            degrees     [float]; [float]
+            dist        [floatseq, floatseq]; [float]
+            erf         [float]; [float]
+            erfc        [float]; [float]
+            exp         [float]; [float]
+            expm1       [float]; [float]
+            fabs        [float]; [float]
+            factorial   [int]; [int]
+            floor       [float]; [int]
+            fmod        [float, float]; [float]
+            frexp       [float]; [float, int]
+            fsum        [iter]; [float] 
+            gamma       [float]; [float]
+            gcd         [int_seq]; [int]
+            hypot       [seq_coord]; [float]    
+            isclose     [float, float]; [bool]
+            isfinite    [float]; [bool] 
+            isinf       [float]; [bool]
+            isnan       [float]; [bool]
+            isqrt       [float]; [int] 
+            lcm         [int_seq]; [int]
+            ldexp       [float, int]; [float]
+            lgamma      [float]; [float]
+            log         [float, float=]; [float]
+            log10       [float]; [float]
+            log1p       [float]; [float]
+            log2        [float]; [float]
+            modf        [float]; [float, float] 
+            nextafter   [float, float]; [float]
+            perm        [int, =int]; [int] 
+            pow         [float, float]; [float]
+            prod        [iter, =float]; [float]
+            radians     [float]; [float]
+            remainder   [float, float]; [float]
+            sin         [float]; [float] 
+            sinh        [float]; [float] 
+            sqrt        [float]; [float] 
+            tan         [float]; [float] 
+            tanh        [float]; [float] 
+            trunc       [float]; [int] 
+            ulp         [float]; [float] 
+        '''
+
+        cmathfuncs = '''
+        '''
 if 1:   # Utility
     def Error(*msg, status=1):
         print(*msg, file=sys.stderr)
@@ -74,7 +138,7 @@ if 1:   # Utility
                 debug.SetDebugger()
         return args
 if 1:   # Core functionality
-    def Math():
+    def GetMathFuncs(show=False):
         funcs = []
         for i in dir(math):
             if i.startswith("__"):
@@ -84,10 +148,12 @@ if 1:   # Core functionality
             if "float" in str(t):
                 continue
             funcs.append(i)
-        print("Math functions:")
-        for i in Columnize(funcs, indent=" "*2):
-            print(i)
-    def CMath():
+        if show:
+            print("Math functions:")
+            for i in Columnize(funcs, indent=" "*2):
+                print(i)
+        return funcs
+    def GetCMathFuncs(show=False):
         funcs = []
         print()
         for i in dir(cmath):
@@ -98,12 +164,29 @@ if 1:   # Core functionality
             if "float" in str(t):
                 continue
             funcs.append(i)
-        print("Cmath functions:")
-        for i in Columnize(funcs, indent=" "*2):
-            print(i)
+        if show:
+            print("Cmath functions:")
+            for i in Columnize(funcs, indent=" "*2):
+                print(i)
+        return funcs
+    def CheckMathFuncs():
+        for line in mathfuncs.strip().split("\n"):
+            line = line.strip()
+            func = line[:12].strip()
+            input, output = [i.strip() for i in line[12:].split(";")]
+            #print(func, input, output)
+            if input == "[float]" and output == "[float]":
+                x = 0.5
+                try:
+                    y = eval(f"math.{func}(x)")
+                except ValueError:
+                    y = eval(f"math.{func}(3*x)")
+                if not ii(y, float):
+                    raise Exception(f"{func}:  bad type")
 
 if __name__ == "__main__":
     d = {}      # Options dictionary
     args = ParseCommandLine(d)
-    Math()
-    CMath()
+    #mathfuncs = GetMathFuncs()
+    #cmathfuncs = GetCMathFuncs()
+    CheckMathFuncs()
