f587b57cb16182f0ecbc959b51f77119855b927e
Don Peterson <someonesdad1@gmail.com>
2024-01-03 23:16:01 +0000

    Updated

--- pgm/chain.py
+++ pgm/chain.py
@@ -23,6 +23,7 @@ if 1:   # Custom imports
     from wrap import dedent
     from get import GetNumber
     from f import flt, pi, degrees, sin
+    from u import u
 if 1:   # Global variables
     default_unit = "inches"
     ii = isinstance
@@ -30,7 +31,7 @@ def Error(msg, status=1):
     print(msg, file=sys.stderr)
     exit(status)
 def ParseCommandLine(d):
-    d["-d"] = 3         # Number of significant digits
+    d["-d"] = 4         # Number of significant digits
     try:
         opts, args = getopt.getopt(sys.argv[1:], "d:")
     except getopt.GetoptError as e:
@@ -45,38 +46,38 @@ def ParseCommandLine(d):
             except ValueError:
                 msg = "-d argument must be an integer between 1 and 15"
                 Error(msg)
-    #sig.digits = d["-d"]
-    #sig.rtz = True
-    flt(0).n = d["-d"]
+    x = flt(0)
+    x.N = d["-d"]
+    x.rtz = x.rtdp = True
 def GetUserData(d):
     desired = d["desired"]  # It will be a lowercase letter
     # Make it 'hole' or 'disk'
     desired = {"h": "hole", "d": "disk"}[desired[0]]
-    key = f"{desired}_diameter" % desired
+    key = f"{desired}_diameter"
     # Diameter
     pm = f"What is the {desired} diameter you want? "
     value, unit = GetNumber(pm, low=0, low_open=True, allow_quit=True,
                             vars=globals(), use_unit=True)
     unit = unit if unit else default_unit
-    d[key] = flt(value, unit=unit)
+    d[key] = flt(value)*u(unit)
     # Drill diameter to use
     pm = "What drill diameter will be used? "
     value, unit = GetNumber(pm, low=0, low_open=True, allow_quit=True,
                             vars=globals(), use_unit=True)
     unit = unit if unit else default_unit
-    d["chain_drill"] = flt(value, unit=unit)
+    d["chain_drill"] = flt(value)*u(unit)
     # Distance between drilled hole edges
     pm = "What is the distance between the hole edges? "
     value, unit = GetNumber(pm, low=0, low_open=True, allow_quit=True,
                             vars=globals(), use_unit=True)
     unit = unit if unit else default_unit
-    d["dist_betw_holes"] = flt(value, unit=unit)
+    d["dist_betw_holes"] = flt(value)*u(unit)
     # Allowance
     pm = "What is the allowance between the final diameter and chain holes? "
     value, unit = GetNumber(pm, low=0, low_open=True, allow_quit=True,
                             vars=globals(), use_unit=True)
     unit = unit if unit else default_unit
-    d["allowance"] = flt(value, unit=unit)
+    d["allowance"] = flt(value)*u(unit)
 def GetInfo(d):
     digits = d["-d"]
     unit = default_unit
@@ -89,9 +90,9 @@ def GetInfo(d):
     '''))
     opt = {"d": "disk", "h": "hole"}
     d["desired"] = "hole"
+    print()
     while True:
-        print(dedent('''
-        Do you want to chain-drill a hole (h) or a disk (d)? ''', end=""))
+        print("Do you want to chain-drill a hole (h) or a disk (d)? ", end="")
         s = input("[%s] " % d["desired"][0])
         s = s.lower().strip()
         if s in opt:
@@ -106,60 +107,6 @@ def GetInfo(d):
             print("Unrecognized response\n")
     GetUserData(d)
     print()
-def PrintResultsOrig(d):
-    corr = 2*d["allowance"] + d["chain_drill"]
-    desired = d["desired"]  # Will be 'hole' or 'disk'
-    key = "%s_diameter" % desired
-    D = d[key] - corr if d["desired"] == "hole" else d[key] + corr
-    dbh = d["dist_betw_holes"]
-    n = int((pi*D - dbh)/(d["chain_drill"] + dbh))
-    if n <= 1:
-        Error("Problem has no reasonable solution")
-    dia = d[key]
-    circ = D
-    angle_radians = 2*pi/n
-    theta_deg = angle_radians*180/pi
-    chord = D*sin(angle_radians/2)
-    chain = d["chain_drill"]
-    matl_betw = chord - d["chain_drill"]
-    allow = d["allowance"]
-    chord = chord
-    betw_hole = d["dist_betw_holes"]
-    # Check for reasonableness
-    if n < 1:
-        print("No acceptable solution.  Try again.")
-        exit(1)
-    # Set up report variables in inches and mm
-    dia_in = sig(dia/u("inches"))
-    dia_mm = sig(dia/u("mm"))
-    chain_in = sig(chain/u("inches"))
-    chain_mm = sig(chain/u("mm"))
-    allow_in = sig(allow/u("inches"))
-    allow_mm = sig(allow/u("mm"))
-    betw_hole_in = sig(betw_hole/u("inches"))
-    betw_hole_mm = sig(betw_hole/u("mm"))
-    matl_betw_in = sig(matl_betw/u("inches"))
-    matl_betw_mm = sig(matl_betw/u("mm"))
-    circ_in = sig(circ/u("inches"))
-    circ_mm = sig(circ/u("mm"))
-    chord_in = sig(chord/u("inches"))
-    chord_mm = sig(chord/u("mm"))
-    theta = sig(theta_deg)
-    # Print report
-    print(dedent(f'''
-    Chain-drilled {desired} input data:
-        Desired {desired} diameter               = {dia_in} inches ({dia_mm} mm)
-        Drill diameter                      = {chain_in} inches ({chain_mm} mm)
-        Distance between drilled hole edges = {betw_hole_in} inches ({betw_hole_mm} mm)
-        Allowance                           = {allow_in} inches ({allow_mm} mm)
-      Results:
-        Number of holes to drill            = {n}
-        Angle between holes                 = {theta} deg
-        Minimum material between holes      = {matl_betw_in} inches ({matl_betw_mm} mm)
-      Information to lay out chain holes:
-        Circle for chain hole centers       = {circ_in} inches ({circ_mm} mm)
-        Divider setting for hole layout     = {chord_in} inches ({chord_mm} mm)
-    '''))
 def PrintResults(d):
     corr = 2*d["allowance"] + d["chain_drill"]
     desired = d["desired"]  # Will be 'hole' or 'disk'
@@ -169,70 +116,65 @@ def PrintResults(d):
     n = int((pi*D - dbh)/(d["chain_drill"] + dbh))
     if n <= 1:
         Error("Problem has no reasonable solution")
-    dia = d[key]
-    circ = D
-    angle_radians = 2*pi/n
-    theta_deg = degrees(angle_radians)
-    chord = D*sin(angle_radians/2)
-    chain = d["chain_drill"]
+    dia = flt(d[key])
+    circ = flt(D)
+    angle_radians = flt(2*pi/n)
+    theta_deg = flt(degrees(angle_radians))
+    chord = flt(D*sin(angle_radians/2))
+    chain = flt(d["chain_drill"])
     matl_betw = chord - d["chain_drill"]
-    allow = d["allowance"]
-    chord = chord
-    betw_hole = d["dist_betw_holes"]
+    allow = flt(d["allowance"])
+    betw_hole = flt(d["dist_betw_holes"])
     # Check for reasonableness
     if n < 1:
         print("No acceptable solution.  Try again.")
         exit(1)
     # Set up report variables in inches and mm
-    dia_in = str(dia.to("inches"))
-    dia_mm = str(dia.to("mm"))
-    chain_in = str(chain.to("inches"))
-    chain_mm = str(chain.to("mm"))
-    allow_in = str(allow.to("inches"))
-    allow_mm = str(allow.to("mm"))
-    betw_hole_in = str(betw_hole.to("inches"))
-    betw_hole_mm = str(betw_hole.to("mm"))
-    matl_betw_in = str(matl_betw.to("inches"))
-    matl_betw_mm = str(matl_betw.to("mm"))
-    circ_in = str(circ.to("inches"))
-    circ_mm = str(circ.to("mm"))
-    chord_in = str(chord.to("inches"))
-    chord_mm = str(chord.to("mm"))
-    theta = str(theta_deg)
+    dia_in = dia/u("inches")
+    dia_mm = dia/u("mm")
+    chain_in = chain/u("inches")
+    chain_mm = chain/u("mm")
+    allow_in = allow/u("inches")
+    allow_mm = allow/u("mm")
+    betw_hole_in = betw_hole/u("inches")
+    betw_hole_mm = betw_hole/u("mm")
+    matl_betw_in = matl_betw/u("inches")
+    matl_betw_mm = matl_betw/u("mm")
+    circ_in = circ/u("inches")
+    circ_mm = circ/u("mm")
+    chord_in = chord/u("inches")
+    chord_mm = chord/u("mm")
+    theta = theta_deg
     # Print report
     print(dedent(f'''
     Chain-drilled {desired} input data:
-        Desired {desired} diameter               = {dia_in} ({dia_mm})
-        Drill diameter                      = {chain_in} ({chain_mm})
-        Distance between drilled hole edges = {betw_hole_in} ({betw_hole_mm})
-        Allowance                           = {allow_in} ({allow_mm})
+        Desired {desired} diameter               = {dia_in} in ({dia_mm} mm)
+        Drill diameter                      = {chain_in} in ({chain_mm} mm)
+        Distance between drilled hole edges = {betw_hole_in} in ({betw_hole_mm} mm)
+        Allowance                           = {allow_in} in ({allow_mm} mm)
+        Number of digits in results         = {d["-d"]}
+        Remove trailing zeros               = {dia.rtz}
       Results:
         Number of holes to drill            = {n}
-        Angle between holes                 = {theta} deg
-        Minimum material between holes      = {matl_betw_in} ({matl_betw_mm})
+        Angle between holes                 = {theta}°
+        Minimum material between holes      = {matl_betw_in} in ({matl_betw_mm} mm)
       Information to lay out chain holes:
-        Circle for chain hole centers       = {circ_in} ({circ_mm})
-        Divider setting for hole layout     = {chord_in} ({chord_mm})
+        Circle for chain hole centers       = {circ_in} in ({circ_mm} mm)
+        Divider setting for hole layout     = {chord_in} in ({chord_mm} mm)
     '''))
 if __name__ == "__main__":
     d = {}
     ParseCommandLine(d)
-    if True:
-        GetInfo(d)
-    else:
+    dbg = 0
+    if dbg:
         # Debugging:  5 inch diameter, 1/4 inch diameter drill, 0.1 inches
         # between hole edges, and 0.05 from final diameter.
-        if 0:
-            factor = u("inches")
-            d["hole_diameter"] = factor*5
-            d["dist_betw_holes"] = factor*0.1
-            d["desired"] = "hole"
-            d["chain_drill"] = factor*0.25
-            d["allowance"] = factor*0.05
-        else:
-            d["hole_diameter"] = flt(5, units="inches")
-            d["dist_betw_holes"] = flt(0.1, units="inches")
-            d["desired"] = "hole"
-            d["chain_drill"] = flt(0.25, units="inches")
-            d["allowance"] = flt(0.05, units="inches")
+        U = u("inches")
+        d["hole_diameter"] = 5*U
+        d["dist_betw_holes"] = 0.1*U
+        d["desired"] = "hole"
+        d["chain_drill"] = 0.25*U
+        d["allowance"] = 0.05*U
+    else:
+        GetInfo(d)
     PrintResults(d)
