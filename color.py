f0573067174d325b3878b60ab3f26da313a25538
Don Peterson <someonesdad1@gmail.com>
2024-03-20 15:14:27 -0600

    Updated

--- color.py
+++ color.py
@@ -3124,6 +3124,7 @@ if __name__ == "__main__":
         Usage:  {sys.argv[0]} [options] [cmd]
           cmd
            d    Show demo
+           l    List short names with various Color constructor arguments
            s    Show short names, attributes [default action for empty cmd]
            t4   Show 4-bit color table
            t8   Show 8-bit color table
@@ -3149,7 +3150,7 @@ if __name__ == "__main__":
         ShortNames()
         print()
         ShowAttributes()
-        print("\nUse d for examples, 't[4|8|24]' for color table")
+        print("\nUse d for examples, 't[4|8|24]' for color table, l for short name properties,")
         print("otherwise interpret the color specifier")
     elif first_char == "t":     # Show 4, 8, or 24 bit color table
         ColorTable(int(cmds[0][1:]))
