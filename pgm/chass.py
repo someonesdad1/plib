2dc32b8ead235de6987bfaf18dcc4609aa9a280b
Don Peterson <someonesdad1@gmail.com>
2024-03-25 13:41:49 -0600

    Updated

--- pgm/chass.py
+++ pgm/chass.py
@@ -1,5 +1,11 @@
 '''
 
+ToDo
+    - Turn this into a design tool given wire insulation temperature rating, number of conductors
+      bundled, and ambient temperature.  In the report, note that it's an estimate and that
+      construction of a real prototype with temperature measurement at the rated power for each
+      wire is needed to check the design.
+
 MIL5088L has tables for maximum currents in copper conductors for aeronautical vehicles.  This
 standard was based on a lot of work in the 1960's and still appears to be an important design
 document (see URLs below).
@@ -7,13 +13,16 @@ document (see URLs below).
 I suspect the chassis currents found elsewhere are derived from these milspec numbers.  The key
 document is a log-log graph of allowed current versus temperature difference between ambient and
 the wire; the lines are parameterized by the AWG wire size.  The graph is Figure 3 on page 46 and
-47 of the PDF.
+47 of the PDF.  These currents are for a single copper wire in free air.
 
-I picked two points off each line, one at 30°C and one at 250°C.  The PDF of the page was maximized
-and screen captured; I plotted this to a piece of letter paper scaled to take up the whole page in
-landscape mode.  This let me read off the numbers with fairly good accuracy.  Compare to the second
-table following; the 30°C numbers are close to each other.  The table values are currents in A.
+In Table 1 below, I picked two points off each line, one at 30°C (bottom end of line) and one at
+250°C (top end of line).  The PDF of the page was maximized and screen captured; I plotted this to
+a piece of letter paper scaled to take up the whole page in landscape mode.  This let me read off
+the numbers with fairly good accuracy.  This table was used to get the empirical line slopes and
+intercepts.
 
+              Table 1
+              -------
     AWG     ΔT = 30°C     ΔT = 250°C 
     ---     ---------     ----------
     10         38             104
@@ -26,19 +35,6 @@ table following; the 30°C numbers are close to each other.  The table values ar
     24         6.2            16
     26         4.58           12.1
 
-The following were the first set of points I picked off the graph.
-
-    AWG     ΔT = 30°C     ΔT = 250°C 
-    ---     ---------     ----------
-    10         38             69   
-    12         29             51   
-    14         21.7           38.8 
-    16         16.2           28.7 
-    18         13.9           24.7 
-    20         10.6           18.6 
-    22         8              14
-    24         6.2            10.7
-
 https://www.lectromec.com/maximum-harness-ampacity/
 https://www.lectromec.com/ampacity-improvements/ 
 
@@ -50,8 +46,11 @@ https://www.lectromec.com/ampacity-improvements/
     by the need to worry about the long-term degradation of insulation at elevated temperatures.  
 
 The HP 6033A power supply manual on page 37 of the Agilent PDF gives a table of stranded copper
-wire ampacity in A derived from MIL-W-5088B:
+wire ampacity in A derived from MIL-W-5088B (is probably for 50 °C ambient):
 
+                  Table 2
+                  -------
+              Agilent
     AWG     Ampacity, A     My Chass, A
     ---     -----------     -----------
     0          245             240
@@ -69,6 +68,12 @@ wire ampacity in A derived from MIL-W-5088B:
 
     Bundled derating for each wire:  2: 0.94, 3: 0.89, 4: 0.83, 5: 0.76
     Maximum ambient 50°C, conductor 105°C
+
+The My Chass values in Table 2 come from the chassis values I use in /plib/pgm/cu_wire.py; these in
+turn came from SAMS, Handbook of Electronics Tables and Formulas, various editions.  Years ago I
+read comments that this SAMS information probably was based on a military standard and, through the
+Agilent 6033 manual, I found the number MIL-W-5088L (the L revision is dated in the 1990's and appears
+to be the latest).
 '''
 
 from math import log10
@@ -78,6 +83,8 @@ if 1:
     import debug
     debug.SetDebugger()
 
+# Columns are
+#  AWG        
 data = '''
     10         38             104
     12         29             79
@@ -91,6 +98,7 @@ data = '''
 '''
 # The array Ig contains the values I read from the MIL5088L graph for 60°C
 Ig = [51.3, 40.4, 29.7, 22.3, 19, 14.4, 11, 8.6, 6.4]
+# AWG  10    12    14    16   18   20   22  24   26
 
 def GetLine(Δt30, Δt250):
     'Return the slope m and intercept 10**b'
---
+++ pgm/pdoc.py
@@ -0,0 +1,112 @@
+'''
+Display python files without header information
+    
+    The default behavior is to search for lines with "#∞x∞#" were x is one of the keywords
+    copyright, contact, category, license, what, and test.  
+
+'''
+if 1:   # Header
+    if 1:   # Copyright, license
+        # These "trigger strings" can be managed with trigger.py
+        #∞copyright∞# Copyright (C) 2024 Don Peterson #∞copyright∞#
+        #∞contact∞# gmail.com@someonesdad1 #∞contact∞#
+        #∞license∞#
+        #   Licensed under the Open Software License version 3.0.
+        #   See http://opensource.org/licenses/OSL-3.0.
+        #∞license∞#
+        #∞what∞#
+        # Display python files without header information
+        #∞what∞#
+        #∞test∞# #∞test∞#
+        pass
+    if 1:   # Standard imports
+        from collections import deque
+        from pathlib import Path as P
+        import getopt
+        import os
+        import re
+        import subprocess
+        import sys
+    if 1:   # Custom imports
+        from color import t
+        from dpprint import PP
+        pp = PP()   # Screen width aware form of pprint.pprint
+        from get import GetLines
+        from wrap import dedent
+        from wsl import wsl     # wsl is True when running under WSL Linux
+        #from columnize import Columnize
+    if 1:   # Global variables
+        class G:    # Storage for global variables as attributes
+            pass
+        g = G()
+        g.dbg = False
+        ii = isinstance
+if 1:   # Utility
+    def GetScreen():
+        'Return (LINES, COLUMNS)'
+        return (
+            int(os.environ.get("LINES", "50")),
+            int(os.environ.get("COLUMNS", "80")) - 1
+        )
+    def GetColors():
+        t.dbg = t("cyn") if g.dbg else ""
+        t.N = t.n if g.dbg else ""
+        t.err = t("redl")
+    def Dbg(*p, **kw):
+        if g.dbg:
+            print(f"{t.dbg}", end="", file=Dbg.file)
+            k = kw.copy()
+            k["file"] = Dbg.file
+            print(*p, **k)
+            print(f"{t.N}", end="", file=Dbg.file)
+    Dbg.file = sys.stdout
+    def Error(*msg, status=1):
+        print(*msg, file=sys.stderr)
+        exit(status)
+    def Usage(status=0):
+        print(dedent(f'''
+        Usage:  {sys.argv[0]} [options] [arg1 [arg2...]]
+          Examine the arguments (assumed to be python files) and print out those that need 
+          work.  The goal is to 
+        Options:
+            -h      Print a manpage
+        '''))
+        exit(status)
+    def ParseCommandLine(d):
+        d["-r"] = False     # Recursively search for python scripts
+        if 0 and len(sys.argv) < 2:
+            Usage()
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], "hr") 
+        except getopt.GetoptError as e:
+            print(str(e))
+            exit(1)
+        for o, a in opts:
+            if o[1] in list("r"):
+                d[o] = not d[o]
+            elif o == "-h":
+                Usage(status=0)
+        GetColors()
+        g.W, g.L = GetScreen()
+        return args
+if 1:   # Core functionality
+    def Process(i):
+        p = P(i)
+        if p.is_dir():
+            if d["-r"]:
+                files = p.glob("**/*.py")
+            else:
+                files = p.glob("*.py")
+        elif p.is_file():
+            files = [p]
+        else:
+            Error(f"{i!r} is not a file or directory")
+        # Process each file
+
+if __name__ == "__main__":
+    d = {}      # Options dictionary
+    args = ParseCommandLine(d)
+    if not args:
+    else:
+        for i in args:
+            Process(i)
