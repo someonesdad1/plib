cf562ba3d607f77754f2a944194c4a15e5ba854b
Don Peterson <someonesdad1@gmail.com>
2024-07-26 15:48:43 -0600

    Updated

--- pgm/cpi.py
+++ pgm/cpi.py
@@ -27,15 +27,14 @@ if 1:  # Header
         import getopt
         import os
         import sys
-        from pdb import set_trace as xx
     if 1:   # Custom imports
         from wrap import dedent
         from f import flt
         from columnize import Columnize
         from color import t
     if 1:   # Global variables
-        # The following CPI data came from the cpi_data.py script.
-        CPI = {
+        # Construct CPI dictionary:  key is year, value is CPI index
+        CPI0 = { # The following CPI data came from the cpi_data.py script.
             2023: flt(304.7),   2000: flt(172.2),   1978: flt(65.2),    1956: flt(27.2),    1934: flt(13.4),
             2022: flt(292.7),   1999: flt(166.6),   1977: flt(60.6),    1955: flt(26.8),    1933: flt(13),
             2021: flt(271),     1998: flt(163),     1976: flt(56.9),    1954: flt(26.9),    1932: flt(13.7),
@@ -60,10 +59,150 @@ if 1:  # Header
             2002: flt(179.9),   1979: flt(72.6),    1957: flt(28.1),    1935: flt(13.7),    1913: flt(9.9),
             2001: flt(177.1),
         }
+        # The following data came from the Bureau of Labor Statistics page
+        # https://data.bls.gov/pdq/SurveyOutputServlet.  Select
+        #   "Table Format"
+        #   "Original Data Value"
+        #   "All years"
+        #   "Annual Data"
+        #   Output type of HTML table
+        # then click on the "Retrieve Data" button.
+        # Copy and paste the table from 1913 to 2024.
+        # Downloaded Fri 26 Jul 2024 03:07:26 PM
+        # Note that 6 digits are given for 2007 and later.
+        # Year	Annual
+        data = '''
+            1913	9.9
+            1914	10.0
+            1915	10.1
+            1916	10.9
+            1917	12.8
+            1918	15.1
+            1919	17.3
+            1920	20.0
+            1921	17.9
+            1922	16.8
+            1923	17.1
+            1924	17.1
+            1925	17.5
+            1926	17.7
+            1927	17.4
+            1928	17.1
+            1929	17.1
+            1930	16.7
+            1931	15.2
+            1932	13.7
+            1933	13.0
+            1934	13.4
+            1935	13.7
+            1936	13.9
+            1937	14.4
+            1938	14.1
+            1939	13.9
+            1940	14.0
+            1941	14.7
+            1942	16.3
+            1943	17.3
+            1944	17.6
+            1945	18.0
+            1946	19.5
+            1947	22.3
+            1948	24.1
+            1949	23.8
+            1950	24.1
+            1951	26.0
+            1952	26.5
+            1953	26.7
+            1954	26.9
+            1955	26.8
+            1956	27.2
+            1957	28.1
+            1958	28.9
+            1959	29.1
+            1960	29.6
+            1961	29.9
+            1962	30.2
+            1963	30.6
+            1964	31.0
+            1965	31.5
+            1966	32.4
+            1967	33.4
+            1968	34.8
+            1969	36.7
+            1970	38.8
+            1971	40.5
+            1972	41.8
+            1973	44.4
+            1974	49.3
+            1975	53.8
+            1976	56.9
+            1977	60.6
+            1978	65.2
+            1979	72.6
+            1980	82.4
+            1981	90.9
+            1982	96.5
+            1983	99.6
+            1984	103.9
+            1985	107.6
+            1986	109.6
+            1987	113.6
+            1988	118.3
+            1989	124.0
+            1990	130.7
+            1991	136.2
+            1992	140.3
+            1993	144.5
+            1994	148.2
+            1995	152.4
+            1996	156.9
+            1997	160.5
+            1998	163.0
+            1999	166.6
+            2000	172.2
+            2001	177.1
+            2002	179.9
+            2003	184.0
+            2004	188.9
+            2005	195.3
+            2006	201.6
+            2007	207.342
+            2008	215.303
+            2009	214.537
+            2010	218.056
+            2011	224.939
+            2012	229.594
+            2013	232.957
+            2014	236.736
+            2015	237.017
+            2016	240.007
+            2017	245.120
+            2018	251.107
+            2019	255.657
+            2020	258.811
+            2021	270.970
+            2022	292.655
+            2023	304.702
+        '''
+        CPI = {}
+        for line in data.split("\n"):
+            if not line.strip():
+                continue
+            yr, cpi = line.split("\t")
+            CPI[int(yr)] = flt(cpi)
+        #
         ref_year = max(CPI)
         min_year = min(CPI)
         min_digits, max_digits = 1, 8
         t.ref = t("ornl")
+        if 0:   # Compare the CPI data
+            x = flt(0)
+            x.N = 6
+            print("year, new, old")
+            for i in CPI:
+                print(f"{i} {CPI[i]!r} {CPI0[i]!r}")
+            exit(0)
+
 if 1:  # Utility
     def Error(msg, status=1):
         print(msg, file=sys.stderr)
