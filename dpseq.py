f89062ee732705295db1d0c30913664b584a5d72
Don Peterson <someonesdad1@gmail.com>
2024-07-28 15:13:27 -0600

    Updated

--- dpseq.py
+++ dpseq.py
@@ -27,7 +27,7 @@ if 1:   # Core functionality
         if i != len(seq):
             return seq[i]
         raise ValueError
-    def GetClosest(x, seq, is_sorted=False, key=None, distance=operator.sub):
+    def GetClosest(x, seq, is_sorted=False, key=None, distance=operator.sub, unresolved=0):
         '''Return the value in sequence seq that is closest to x.
  
         The intended common use case is where x is a number and seq is a sequence of numbers, but
@@ -44,16 +44,22 @@ if 1:   # Core functionality
             key for the sorted() built-in if the elements don't have a relevant '<' operator.  If
             is_sorted is True, the original sequence is used as is.
  
-            You can also set is_sorted to None, meaning do not use a sort sequence and the bisect
-            module.  Instead, a sequence is constructed of the absolute value of the distance
-            between x and each element of seq.  The index of the smallest value of this sequence
-            is found and is used to return the corresponding element of seq.
+            You can also set is_sorted to None, meaning do not use bisection.  Instead, a sequence
+            is constructed of the absolute value of the distance between x and each element of
+            seq.  The index of the smallest value of this sequence is found and is used to return
+            the corresponding element of seq.
  
         distance
             This is a binary function that returns the distance between x and a sequence element.
             This distance must be an integer or a floating point number.  This function is only
             used if is_sorted is set to None.
  
+        unresolved
+            An example like seq = (0, 1, 2, 3) and x = 1e99 will return the list o with all
+            elements the same number, so the problem is unresolved.  If unresolved is set to an
+            integer, then that array index of seq is returned in this case.  Otherwise, a
+            ValueError exception is raised.
+
         Example:  let seq = (5, -8, 10, 1).  Then
             GetClosest(-1e99, seq) = -8
             GetClosest(-9, seq) = -8
@@ -71,9 +77,19 @@ if 1:   # Core functionality
             # Get o's index of the minimum
             index = o.index(minimum)
             # Check for the special case where the question is unresolvable, as all of these
-            # differences are the same number
-            if len(set(o)) == 1:    # Question is unresolvable
-                raise ValueError("Closest item is unresolvable")
+            # differences are the same number.  Example:  seq = (0, 1, 2, 3) and x = 1e99.  As
+            # all of the numbers in seq subtracted from 1e99 give the same value, the problem
+            # is not solvable with floating point arithmetic.  Thus, any entry from the array can
+            # be returned.
+            if len(set(o)) == 1:    # Problem can't be resolved
+                if unresolved is not None and isinstance(unresolved, int):
+                    index = unresolved
+                    try:
+                        seq[index]
+                    except Exception:
+                        raise ValueError(f"'resolved' is not an index for seq")
+                else:
+                    raise ValueError("Closest item is unresolvable")
             # Return the closest value
             return seq[index]
         else:
@@ -97,19 +113,24 @@ if __name__ == "__main__":
     from functools import partial
     def Test_GetClosest():
         low, high = -3, 6
-        seq = (4, low, high, 1)
-        sseq = (low, 1, 4, high)
+        seq = (4, low, high, 1)     # Unsorted sequence
+        sseq = (low, 1, 4, high)    # Sorted sequence
         if 1:   # Test for each type of is_sorted
             for k in (None, False, True):
                 f = partial(GetClosest, is_sorted=k)
                 seq = sseq if k else seq
                 if k is None:
-                    raises(ValueError, f, -1e99, seq)
-                    raises(ValueError, f, 1e99, seq)
+                    raises(ValueError, f, -1e99, seq, unresolved=None)
+                    raises(ValueError, f, 1e99, seq, unresolved=None)
+                    Assert(f(-1e99, seq) == seq[0])
+                    Assert(f(1e99, seq) == seq[0])
                 else:
                     Assert(f(-1e99, seq) == low)
                     Assert(f(1e99, seq) == high)
+                Assert(f(-40, seq) == low)
                 Assert(f(-4, seq) == low)
+                # Note x can be a float also
+                Assert(f(-4., seq) == low)
                 Assert(f(-3, seq) == low)
                 Assert(f(-2, seq) == low)
                 Assert(f(-1, seq) == low)
