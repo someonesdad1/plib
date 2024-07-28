f1d3b02ad440397d778b7b339c1990b17d1b51d2
Don Peterson <someonesdad1@gmail.com>
2024-07-28 11:23:49 -0600

    Updated

--- dpseq.py
+++ dpseq.py
@@ -5,23 +5,31 @@ if 1:   # Header
     if 1:   # Standard includes
         import operator
     if 1:   # Custom includes
-        from lwtest import Assert
+        from lwtest import Assert, raises
     if 1:   # Global variables
         pass
 if 1:   # Core functionality
     def GetClosest(x, seq, is_sorted=False, key=None, distance=operator.sub):
         '''Return the value in sequence seq that is closest to x.
 
+        The intended common use case is where x is a number and seq is a sequence of numbers, but
+        the pattern should apply to any objects that can be ordered with "<" or have the notion of
+        distance between x and any of seq's elements.
+
+        For best efficiency, have seq be in sorted order or allow it to be sorted, as this uses
+        the bisect module to find the relevant item in O(log(n)).  Otherwise, the algorithm is
+        O(n).
+
         is_sorted
             If is_sorted is False, a sorted copy of the sequence is made and bisect.bisect_left is
             used to find the relevant insertion point.  If is_sorted is False, you must provide a
             key for the sorted() built-in if the elements don't have a relevant '<' operator.  If
-            is_sorted is True, the original sequence is used.
+            is_sorted is True, the original sequence is used as is.
 
             You can also set is_sorted to None, meaning do not use a sort sequence and the bisect
             module.  Instead, a sequence is constructed of the absolute value of the distance
             between x and each element of seq.  The index of the smallest value of this sequence
-            is found and is used to return that particular sequence of seq.
+            is found and is used to return the corresponding element of seq.
 
         distance
             This is a binary function that returns the distance between x and a sequence element.
@@ -29,6 +37,13 @@ if 1:   # Core functionality
             used if is_sorted is set to None.
 
 
+        Example:  let seq = (5, -8, 10, 1).  Then
+            GetClosest(-1e99, seq) = -8
+            GetClosest(-9, seq) = -8
+            GetClosest(-7, seq) = -8
+            GetClosest(0, seq) = 1
+            GetClosest(-7, seq) = 5
+            GetClosest(1e99, seq) = 10
         '''
         if not seq:
             raise ValueError("Sequence seq cannot be empty")
@@ -36,7 +51,13 @@ if 1:   # Core functionality
             # Get list of differences from x
             o = [abs(distance(i, x)) for i in seq]
             minimum = min(o)                # Minimum difference
-            index = seq.index(minimum)      # Get index of minimum
+            try:
+                index = seq.index(minimum)      # Get index of minimum
+            except ValueError:  # minimum isn't in sequence
+                if len(set(o)) == 1:    # Question is unresolvable
+                    raise ValueError("Closest item is unresolvable")
+                # Get the index of minimum in o, as this is the index of the closest value in seq
+                index = o.index(minimum)
             return seq[index]               # This is the closest value
         else:
             lseq = seq if is_sorted else sorted(seq, key=key)
@@ -54,13 +75,15 @@ if 1:   # Core functionality
                     diff_low, diff_high = abs(x - lseq[l]), abs(x - lseq[r])
                     return lseq[l] if diff_low <= diff_high else lseq[r]
 
-
 if __name__ == "__main__":  
+    from functools import partial
     def Test_GetClosest():
         seq = (0, 11, 28, 31, 40)
         if 1:   # Test with is_sorted == None
-        Assert(GetClosest(-1e99, seq) == 0)
-        Assert(GetClosest(-1, seq) == 0)
-        Assert(GetClosest(0, seq) == 0)
+            f = partial(GetClosest, is_sorted=None)
+            raises(ValueError, f, -1e99, seq)
+            raises(ValueError, f, 1e99, seq)
+            Assert(f(-1, seq) == 0)
+            Assert(f(0, seq) == 0)
 
     Test_GetClosest()
