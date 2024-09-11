f58df4d2a44b616b8d9a0fca8aec96119255c6e7
Don Peterson <someonesdad1@gmail.com>
2024-09-11 14:01:10 -0600

    Updated

---
+++ pgm/exp.py
@@ -0,0 +1,116 @@
+'''
+Keep track of experiments
+
+    - Assign serial numbers to experiments such as EX1, EX2, ...  This allows e.g. identification
+      of parts in bags as they collect for an experiment (similar to the proj.py script).  Parts
+      can be identified as EX1.1, etc.
+    - Data kept in a text file in ~/.0rc
+    - 
+
+'''
+ 
+if 1:  # Header
+    if 1:  # Copyright, license
+        # These "trigger strings" can be managed with trigger.py
+        #∞copyright∞# Copyright (C) 2024 Don Peterson #∞copyright∞#
+        #∞contact∞# gmail.com@someonesdad1 #∞contact∞#
+        #∞license∞#
+        #   Licensed under the Open Software License version 3.0.
+        #   See http://opensource.org/licenses/OSL-3.0.
+        #∞license∞#
+        #∞what∞#
+        # Experiment tracking script
+        #∞what∞#
+        #∞test∞# #∞test∞#
+        pass
+    if 1:   # Standard imports
+        from collections import deque
+        from pathlib import Path as P
+        import getopt
+        import os
+        import re
+        import sys
+    if 1:   # Custom imports
+        from f import flt
+        from wrap import dedent
+        from color import t
+        from lwtest import Assert
+        if 0:
+            import debug
+            debug.SetDebugger()
+    if 1:   # Global variables
+        class G:
+            pass
+        g = G()
+        g.dbg = False
+        ii = isinstance
+if 1:   # Utility
+    def GetColors():
+        t.err = t("redl")
+        t.dbg = t("lill") if g.dbg else ""
+        t.N = t.n if g.dbg else ""
+    def GetScreen():
+        'Return (LINES, COLUMNS)'
+        return (
+            int(os.environ.get("LINES", "50")),
+            int(os.environ.get("COLUMNS", "80")) - 1
+        )
+    def Dbg(*p, **kw):
+        if g.dbg:
+            print(f"{t.dbg}", end="")
+            print(*p, **kw)
+            print(f"{t.N}", end="")
+    def Error(*msg, status=1):
+        print(*msg, file=sys.stderr)
+        exit(status)
+    def Usage(status=0):
+        print(dedent(f'''
+        Usage:  {sys.argv[0]} [options] [cmd]
+          Script to track experiments I've done.  You can search for keywords (-k) or use 
+          a regex to search titles (use -b to search body of the description).  The primary
+          purpose of the script is to provide a link to the document file that describes the
+          experiment and its results.
+
+          No cmd causes the data file to be edited.  cmd options are:
+            l   List the experiments, one experiment per line
+            s   Search for a regex
+            k   Search for a keyword
+        Options:
+            -h      Print a manpage
+        '''))
+        exit(status)
+    def ParseCommandLine(d):
+        d["-a"] = False     # Need description
+        d["-d"] = 3         # Number of significant digits
+        if len(sys.argv) < 2:
+            Usage()
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], "ad:h", "--debug") 
+        except getopt.GetoptError as e:
+            print(str(e))
+            exit(1)
+        for o, a in opts:
+            if o[1] in list("a"):
+                d[o] = not d[o]
+            elif o in ("-d",):
+                try:
+                    d["-d"] = int(a)
+                    if not (1 <= d["-d"] <= 15):
+                        raise ValueError()
+                except ValueError:
+                    msg = ("-d option's argument must be an integer between "
+                        "1 and 15")
+                    Error(msg)
+            elif o == "-h":
+                Usage()
+            elif o in ("--debug",):
+                # Set up a handler to drop us into the debugger on an unhandled exception
+                import debug
+                debug.SetDebugger()
+        return args
+if 1:   # Core functionality
+    pass
+
+if __name__ == "__main__":
+    d = {}      # Options dictionary
+    args = ParseCommandLine(d)
