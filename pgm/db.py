cf3e76070b61debd1c1929ee9d0c4deeac9cb7fd
Don Peterson <someonesdad1@gmail.com>
2024-10-24 08:53:38 -0600

    Updated

--- pgm/db.py
+++ pgm/db.py
@@ -1,19 +1,5 @@
 '''
-Prints out tables of dB stuff.
-
-dBm definitions
-    dBm(R Ω) = 20*log(V/sqrt(R/1000))
-    V = sqrt(R/1000)*10**(dBm/20)
-    R = V**2*10**(-dBm/10 + 3)
-
-Converting between different dBm voltage measures:
-    dBm(R Ω) = dBm(S Ω) + C where C = 10*log(S/R)
-        dBm(50) = dBm(600) + 10.8
-        dBm(75) = dBm(600) + 9.0
-        dBm(50) = dBm(75) + 1.8
-    0 dBm(600) is 0.7746 V = sqrt(600/1000)
-    0 dBm(50) is 0.2236 V = sqrt(50/1000)
-    0 dBm(75) is 0.2739 V = sqrt(75/1000)
+Prints out tables of dB stuff
 
 '''
 if 1:  # Copyright, license
@@ -38,10 +24,13 @@ if 1:   # Imports
 if 1:   # Custom imports
     from wrap import dedent
     from columnize import Columnize
-    from sig import sig
+    from f import flt, sqrt
+    from color import t
 if 1:   # Global variables
-    sig.low = 1e-4
     W = int(os.environ["COLUMNS"]) - 1
+    x = flt(0)
+    x.N = 3
+    x.rtz = True
 def Error(msg, status=1):
     print(msg, file=sys.stderr)
     exit(status)
@@ -70,27 +59,77 @@ def ParseCommandLine(d):
             Usage(d, status=0)
     return args
 def Distortion():
-    print(f"{'Distortion in dBc converted to %':^{W}s}")
+    print(f"{'Distortion in dBc (dB below carrier) converted to %':^{W}s}")
+    print(f"{'percent = 100*10**(dBc/20)':^{W}s}")
     out = ["dBc   %"]
     for dBc in range(101):
-        pct = 100*10**(-dBc/20)
-        out.append(f"{dBc:3d}  {sig(pct)}")
+        pct = flt(100*10**(-dBc/20))
+        out.append(f"{-dBc:4d}  {pct}")
     for i in Columnize(out):
         print(i)
 def dBV_dBm600():
-    print(f"{'dBV to dBm(600)':^{W}s}")
-    out = ["dBV  dBm(600)"]
+    print(f"{'dBV to dBm(600 Ω)':^{W}s}")
+    out = ["dBV  dBm"]
     for dBV in range(101):
-        dBm = 20*log(10**(dBV/20)/sqrt(600/1000))
-        out.append(f"{dBV:3d}  {dBm:4.1f}")
+        dBm = 20*log(10**(-dBV/20)/sqrt(600/1000))
+        out.append(f"{-dBV:3d}  {dBm:4.1f}")
     for i in Columnize(out):
         print(i)
+def Header():
+    print(dedent(f'''
+    dBm definitions (R in Ω, V in volts)
+        dBm(R Ω) = 20*log(V/sqrt(R/1000))
+        V = sqrt(R/1000)*10**(dBm/20)
+        R = V**2*10**(-dBm/10 + 3)
+
+    Converting between different dBm voltage measures:
+        dBm(R Ω) = dBm(S Ω) + C where C = 10*log(S/R)
+            dBm(50 Ω) = dBm(600 Ω) + 10.8
+            dBm(75 Ω) = dBm(600 Ω) + 9.0
+            dBm(50 Ω) = dBm(75 Ω) + 1.8
+        0 dBm(600 Ω) is 0.7746 V = sqrt(600/1000)
+        0 dBm(50 Ω) is 0.2236 V = sqrt(50/1000)
+        0 dBm(75 Ω) is 0.2739 V = sqrt(75/1000)
+    '''))
+def Table():
+    w = 12  # Column width
+    hdr = (
+        f"{'Value':>{w}s}",
+        f"{'dBm(50 Ω)':>{w}s}",
+        f"{'dBm(75 Ω)':>{w}s}",
+        f"{'dBm(600 Ω)':>{w}s}",
+        f"{'dBV':>{w}s}",
+        #f"{'dBmV':>{w}s}",
+        #f"{'dBuV':>{w}s}",
+    )
+    print("dB values in voltage for various references\n")
+    t.print(f"{t('pnkl')}{' '.join(hdr)}")
+    for db in range(60, -120 - 1, -2):
+        res = []
+        res.append(sqrt(50/1000)*10**(db/20))       # dBm(50)
+        res.append(sqrt(75/1000)*10**(db/20))       # dBm(75)
+        res.append(sqrt(600/1000)*10**(db/20))      # dBm(600)
+        res.append(10**(db/20))                     # dBV
+        #res.append(1e-3*10**(db/20))                # dBmV
+        #res.append(1e-6*10**(db/20))                # dBμV
+        res = [f"{flt(i).engsi + 'V'!s:>{w}s}" for i in res]
+        res.insert(0, f"{'%3d' % db!s:>{w}s}")
+        if db % 10 == 0:
+            t.print(f"{t('grnl')}{' '.join(res)}")
+        else:
+            print(' '.join(res))
+    t.print(f"{t('pnkl')}{' '.join(hdr)}")
+
 if __name__ == "__main__":
     d = {}      # Options dictionary
     args = ParseCommandLine(d)
     if d["-d"]:
         Distortion()
         exit()
+    Header()
+    print()
     Distortion()
     print()
     dBV_dBm600()
+    print()
+    Table()
