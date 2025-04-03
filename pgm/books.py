754323f3f42b447923be3af3e357aa6dab635193
Don Peterson <someonesdad1@gmail.com>
2025-04-03 12:29:09 -0600

    Updated

--- pgm/books.py
+++ pgm/books.py
@@ -1,516 +1,110 @@
 '''
 PBS list of 100 great read books in the US.  
-https://www.pbs.org/the-great-american-read/books/#/
-(Information below was screen-scraped)
+    https://www.pbs.org/the-great-american-read/books/#/
 '''
-data = '''
-1
-To Kill a Mockingbird
-Harper Lee
-Outlander (Series) coverFinalist
-2
-Outlander (Series)
-Diana Gabaldon
-Harry Potter (Series) coverFinalist
-3
-Harry Potter (Series)
-J.K. Rowling
-Pride and Prejudice coverFinalist
-4
-Pride and Prejudice
-Jane Austen
-The Lord of the Rings (Series) coverFinalist
-5
-The Lord of the Rings (Series)
-J.R.R. Tolkien
-Gone with the Wind cover
-6
-Gone with the Wind
-Margaret Mitchell
-Charlotte's Web cover
-7
-Charlotte's Web
-E. B. White
-Little Women cover
-8
-Little Women
-Louisa May Alcott
-The Chronicles of Narnia (Series) cover
-9
-The Chronicles of Narnia (Series)
-C.S. Lewis
-Jane Eyre cover
-10
-Jane Eyre
-Charlotte Brontë
-Anne of Green Gables cover
-11
-Anne of Green Gables
-Lucy Maud Montgomery
-The Grapes of Wrath cover
-12
-The Grapes of Wrath
-John Steinbeck
-A Tree Grows in Brooklyn cover
-13
-A Tree Grows in Brooklyn
-Betty Smith
-The Book Thief cover
-14
-The Book Thief
-Markus Zusak
-The Great Gatsby cover
-15
-The Great Gatsby
-F. Scott Fitzgerald
-The Help cover
-16
-The Help
-Kathryn Stockett
-The Adventures of Tom Sawyer cover
-17
-The Adventures of Tom Sawyer
-Mark Twain
-1984 cover
-18
-1984
-George Orwell
-And Then There Were None cover
-19
-And Then There Were None
-Agatha Christie
-Atlas Shrugged cover
-20
-Atlas Shrugged
-Ayn Rand
-Wuthering Heights cover
-21
-Wuthering Heights
-Emily Brontë
-Lonesome Dove cover
-22
-Lonesome Dove
-Larry McMurtry
-The Pillars of the Earth cover
-23
-The Pillars of the Earth
-Ken Follett
-The Stand cover
-24
-The Stand
-Stephen King
-Rebecca cover
-25
-Rebecca
-Daphne du Maurier
-A Prayer for Owen Meany cover
-26
-A Prayer for Owen Meany
-John Irving
-The Color Purple cover
-27
-The Color Purple
-Alice Walker
-Alice's Adventures in Wonderland cover
-28
-Alice's Adventures in Wonderland
-Lewis Carroll
-Great Expectations cover
-29
-Great Expectations
-Charles Dickens
-The Catcher in the Rye cover
-30
-The Catcher in the Rye
-J.D. Salinger
-Where the Red Fern Grows cover
-31
-Where the Red Fern Grows
-Wilson Rawls
-The Outsiders cover
-32
-The Outsiders
-S. E. Hinton
-The Da Vinci Code cover
-33
-The Da Vinci Code
-Dan Brown
-The Handmaid's Tale cover
-34
-The Handmaid's Tale
-Margaret Atwood
-Dune cover
-35
-Dune
-Frank Herbert
-The Little Prince cover
-36
-The Little Prince
-Antoine de Saint-Exupéry
-The Call of the Wild cover
-37
-The Call of the Wild
-Jack London
-The Clan of the Cave Bear cover
-38
-The Clan of the Cave Bear
-Jean M. Auel
-The Hitchhiker's Guide to The Galaxy cover
-39
-The Hitchhiker's Guide to The Galaxy
-Douglas Adams
-The Hunger Games (Series) cover
-40
-The Hunger Games (Series)
-Suzanne Collins
-The Count of Monte Cristo cover
-41
-The Count of Monte Cristo
-Alexandre Dumas
-The Joy Luck Club cover
-42
-The Joy Luck Club
-Amy Tan
-Frankenstein cover
-43
-Frankenstein
-Mary Shelley
-The Giver cover
-44
-The Giver
-Lois Lowry
-Memoirs of a Geisha cover
-45
-Memoirs of a Geisha
-Arthur Golden
-Moby Dick cover
-46
-Moby Dick
-Herman Melville
-Catch-22 cover
-47
-Catch-22
-Joseph Heller
-Game of Thrones (Series) cover
-48
-Game of Thrones (Series)
-George R. R. Martin
-Foundation (Series) cover
-49
-Foundation (Series)
-Isaac Asimov
-War and Peace cover
-50
-War and Peace
-Leo Tolstoy
-Their Eyes Were Watching God cover
-51
-Their Eyes Were Watching God
-Zora Neale Hurston
-Jurassic Park cover
-52
-Jurassic Park
-Michael Crichton
-The Godfather cover
-53
-The Godfather
-Mario Puzo
-One Hundred Years of Solitude cover
-54
-One Hundred Years of Solitude
-Gabriel García Márquez
-The Picture of Dorian Gray cover
-55
-The Picture of Dorian Gray
-Oscar Wilde
-The Notebook cover
-56
-The Notebook
-Nicholas Sparks
-The Shack cover
-57
-The Shack
-William P. Young
-A Confederacy of Dunces cover
-58
-A Confederacy of Dunces
-John Kennedy Toole
-The Hunt for Red October cover
-59
-The Hunt for Red October
-Tom Clancy
-Beloved cover
-60
-Beloved
-Toni Morrison
-The Martian cover
-61
-The Martian
-Andy Weir
-The Wheel of Time (Series) cover
-62
-The Wheel of Time (Series)
-Robert Jordan / Brandon Sanderson
-Siddhartha cover
-63
-Siddhartha
-Hermann Hesse
-Crime and Punishment cover
-64
-Crime and Punishment
-Fyodor Dostoyevsky
-The Sun Also Rises cover
-65
-The Sun Also Rises
-Ernest Hemingway
-The Curious Incident of the Dog in the Night-Time cover
-66
-The Curious Incident of the Dog in the Night-Time
-Mark Haddon
-A Separate Peace cover
-67
-A Separate Peace
-John Knowles
-Don Quixote cover
-68
-Don Quixote
-Miguel de Cervantes
-The Lovely Bones cover
-69
-The Lovely Bones
-Alice Sebold
-The Alchemist cover
-70
-The Alchemist
-Paulo Coelho
-Hatchet (Series) cover
-71
-Hatchet (Series)
-Gary Paulsen
-Invisible Man cover
-72
-Invisible Man
-Ralph Ellison
-The Twilight Saga (Series) cover
-73
-The Twilight Saga (Series)
-Stephenie Meyer
-Tales of the City (Series) cover
-74
-Tales of the City (Series)
-Armistead Maupin
-Gulliver's Travels cover
-75
-Gulliver's Travels
-Jonathan Swift
-Ready Player One cover
-76
-Ready Player One
-Ernest Cline
-Left Behind (Series) cover
-77
-Left Behind (Series)
-Tim LaHaye / Jerry B. Jenkins
-Gone Girl cover
-78
-Gone Girl
-Gillian Flynn
-Watchers cover
-79
-Watchers
-Dean Koontz
-The Pilgrim's Progress cover
-80
-The Pilgrim's Progress
-John Bunyan
-Alex Cross Mysteries (Series) cover
-81
-Alex Cross Mysteries (Series)
-James Patterson
-Things Fall Apart cover
-82
-Things Fall Apart
-Chinua Achebe
-Heart of Darkness cover
-83
-Heart of Darkness
-Joseph Conrad
-Gilead cover
-84
-Gilead
-Marilynne Robinson
-Flowers in the Attic cover
-85
-Flowers in the Attic
-V.C. Andrews
-Fifty Shades of Grey (Series) cover
-86
-Fifty Shades of Grey (Series)
-E.L. James
-The Sirens of Titan cover
-87
-The Sirens of Titan
-Kurt Vonnegut
-This Present Darkness cover
-88
-This Present Darkness
-Frank E. Peretti
-Americanah cover
-89
-Americanah
-Chimamanda Ngozi Adichie
-Another Country cover
-90
-Another Country
-James Baldwin
-Bless Me, Ultima cover
-91
-Bless Me, Ultima
-Rudolfo Anaya
-Looking for Alaska cover
-92
-Looking for Alaska
-John Green
-The Brief Wondrous Life of Oscar Wao cover
-93
-The Brief Wondrous Life of Oscar Wao
-Junot Díaz
-Swan Song cover
-94
-Swan Song
-Robert R. McCammon
-Mind Invaders cover
-95
-Mind Invaders
-Dave Hunt
-White Teeth cover
-96
-White Teeth
-Zadie Smith
-Ghost cover
-97
-Ghost
-Jason Reynolds
-The Coldest Winter Ever cover
-98
-The Coldest Winter Ever
-Sister Souljah
-The Intuitionist cover
-99
-The Intuitionist
-Colson Whitehead
-Doña Bárbára cover
-100
-Doña Bárbára
-Rómulo Gallegos
-'''[1:-1]
-
-data = '''
-1 | To Kill a Mockingbird | Harper Lee 
-2 | Outlander (Series) | Diana Gabaldon 
-3 | Harry Potter (Series) | J.K. Rowling 
-4 | Pride and Prejudice | Jane Austen 
-5 | The Lord of the Rings (Series) | J.R.R. Tolkien 
-6 | Gone with the Wind | Margaret Mitchell 
-7 | Charlotte's Web | E. B. White 
-8 | Little Women | Louisa May Alcott 
-9 | The Chronicles of Narnia (Series) | C.S. Lewis 
-10 | Jane Eyre | Charlotte Brontë 
-11 | Anne of Green Gables | Lucy Maud Montgomery 
-12 | The Grapes of Wrath | John Steinbeck 
-13 | A Tree Grows in Brooklyn | Betty Smith 
-14 | The Book Thief | Markus Zusak 
-15 | The Great Gatsby | F. Scott Fitzgerald 
-16 | The Help | Kathryn Stockett 
-17 | The Adventures of Tom Sawyer | Mark Twain 
-18 | 1984 | George Orwell 
-19 | And Then There Were None | Agatha Christie 
-20 | Atlas Shrugged | Ayn Rand 
-21 | Wuthering Heights | Emily Brontë 
-22 | Lonesome Dove | Larry McMurtry 
-23 | The Pillars of the Earth | Ken Follett 
-24 | The Stand | Stephen King 
-25 | Rebecca | Daphne du Maurier 
-26 | A Prayer for Owen Meany | John Irving 
-27 | The Color Purple | Alice Walker 
-28 | Alice's Adventures in Wonderland | Lewis Carroll 
-29 | Great Expectations | Charles Dickens 
-30 | The Catcher in the Rye | J.D. Salinger 
-31 | Where the Red Fern Grows | Wilson Rawls 
-32 | The Outsiders | S. E. Hinton 
-33 | The Da Vinci Code | Dan Brown 
-34 | The Handmaid's Tale | Margaret Atwood 
-35 | Dune | Frank Herbert 
-36 | The Little Prince | Antoine de Saint-Exupéry 
-37 | The Call of the Wild | Jack London 
-38 | The Clan of the Cave Bear | Jean M. Auel 
-39 | The Hitchhiker's Guide to The Galaxy | Douglas Adams 
-40 | The Hunger Games (Series) | Suzanne Collins 
-41 | The Count of Monte Cristo | Alexandre Dumas 
-42 | The Joy Luck Club | Amy Tan 
-43 | Frankenstein | Mary Shelley 
-44 | The Giver | Lois Lowry 
-45 | Memoirs of a Geisha | Arthur Golden 
-46 | Moby Dick | Herman Melville 
-47 | Catch-22 | Joseph Heller 
-48 | Game of Thrones (Series) | George R. R. Martin 
-49 | Foundation (Series) | Isaac Asimov 
-50 | War and Peace | Leo Tolstoy 
-51 | Their Eyes Were Watching God | Zora Neale Hurston 
-52 | Jurassic Park | Michael Crichton 
-53 | The Godfather | Mario Puzo 
-54 | One Hundred Years of Solitude | Gabriel García Márquez 
-55 | The Picture of Dorian Gray | Oscar Wilde 
-56 | The Notebook | Nicholas Sparks 
-57 | The Shack | William P. Young 
-58 | A Confederacy of Dunces | John Kennedy Toole 
-59 | The Hunt for Red October | Tom Clancy 
-60 | Beloved | Toni Morrison 
-61 | The Martian | Andy Weir 
-62 | The Wheel of Time (Series) | Robert Jordan, Brandon Sanderson 
-63 | Siddhartha | Hermann Hesse 
-64 | Crime and Punishment | Fyodor Dostoyevsky 
-65 | The Sun Also Rises | Ernest Hemingway 
-66 | The Curious Incident of the Dog in the Night-Time | Mark Haddon 
-67 | A Separate Peace | John Knowles 
-68 | Don Quixote | Miguel de Cervantes 
-69 | The Lovely Bones | Alice Sebold 
-70 | The Alchemist | Paulo Coelho 
-71 | Hatchet (Series) | Gary Paulsen 
-72 | Invisible Man | Ralph Ellison 
-73 | The Twilight Saga (Series) | Stephenie Meyer 
-74 | Tales of the City (Series) | Armistead Maupin 
-75 | Gulliver's Travels | Jonathan Swift 
-76 | Ready Player One | Ernest Cline 
-77 | Left Behind (Series) | Tim LaHaye, Jerry B. Jenkins 
-78 | Gone Girl | Gillian Flynn 
-79 | Watchers | Dean Koontz 
-80 | The Pilgrim's Progress | John Bunyan 
-81 | Alex Cross Mysteries (Series) | James Patterson 
-82 | Things Fall Apart | Chinua Achebe 
-83 | Heart of Darkness | Joseph Conrad 
-84 | Gilead | Marilynne Robinson 
-85 | Flowers in the Attic | V.C. Andrews 
-86 | Fifty Shades of Grey (Series) | E.L. James 
-87 | The Sirens of Titan | Kurt Vonnegut 
-88 | This Present Darkness | Frank E. Peretti 
-89 | Americanah | Chimamanda Ngozi Adichie 
-90 | Another Country | James Baldwin 
-91 | Bless Me, Ultima | Rudolfo Anaya 
-92 | Looking for Alaska | John Green 
-93 | The Brief Wondrous Life of Oscar Wao | Junot Díaz 
-94 | Swan Song | Robert R. McCammon 
-95 | Mind Invaders | Dave Hunt 
-96 | White Teeth | Zadie Smith 
-97 | Ghost | Jason Reynolds 
-98 | The Coldest Winter Ever | Sister Souljah 
-99 | The Intuitionist | Colson Whitehead 
-100 | Doña Bárbára | Rómulo Gallegos 
-'''[1:-1]
-
-from collections import deque
 from color import t
-
+data = '''
+    1 | To Kill a Mockingbird | Harper Lee 
+    2 | Outlander (Series) | Diana Gabaldon 
+    3 | Harry Potter (Series) | J.K. Rowling 
+    4 | Pride and Prejudice | Jane Austen 
+    5 | The Lord of the Rings (Series) | J.R.R. Tolkien 
+    6 | Gone with the Wind | Margaret Mitchell 
+    7 | Charlotte's Web | E. B. White 
+    8 | Little Women | Louisa May Alcott 
+    9 | The Chronicles of Narnia (Series) | C.S. Lewis 
+    10 | Jane Eyre | Charlotte Brontë 
+    11 | Anne of Green Gables | Lucy Maud Montgomery 
+    12 | The Grapes of Wrath | John Steinbeck 
+    13 | A Tree Grows in Brooklyn | Betty Smith 
+    14 | The Book Thief | Markus Zusak 
+    15 | The Great Gatsby | F. Scott Fitzgerald 
+    16 | The Help | Kathryn Stockett 
+    17 | The Adventures of Tom Sawyer | Mark Twain 
+    18 | 1984 | George Orwell 
+    19 | And Then There Were None | Agatha Christie 
+    20 | Atlas Shrugged | Ayn Rand 
+    21 | Wuthering Heights | Emily Brontë 
+    22 | Lonesome Dove | Larry McMurtry 
+    23 | The Pillars of the Earth | Ken Follett 
+    24 | The Stand | Stephen King 
+    25 | Rebecca | Daphne du Maurier 
+    26 | A Prayer for Owen Meany | John Irving 
+    27 | The Color Purple | Alice Walker 
+    28 | Alice's Adventures in Wonderland | Lewis Carroll 
+    29 | Great Expectations | Charles Dickens 
+    30 | The Catcher in the Rye | J.D. Salinger 
+    31 | Where the Red Fern Grows | Wilson Rawls 
+    32 | The Outsiders | S. E. Hinton 
+    33 | The Da Vinci Code | Dan Brown 
+    34 | The Handmaid's Tale | Margaret Atwood 
+    35 | Dune | Frank Herbert 
+    36 | The Little Prince | Antoine de Saint-Exupéry 
+    37 | The Call of the Wild | Jack London 
+    38 | The Clan of the Cave Bear | Jean M. Auel 
+    39 | The Hitchhiker's Guide to The Galaxy | Douglas Adams 
+    40 | The Hunger Games (Series) | Suzanne Collins 
+    41 | The Count of Monte Cristo | Alexandre Dumas 
+    42 | The Joy Luck Club | Amy Tan 
+    43 | Frankenstein | Mary Shelley 
+    44 | The Giver | Lois Lowry 
+    45 | Memoirs of a Geisha | Arthur Golden 
+    46 | Moby Dick | Herman Melville 
+    47 | Catch-22 | Joseph Heller 
+    48 | Game of Thrones (Series) | George R. R. Martin 
+    49 | Foundation (Series) | Isaac Asimov 
+    50 | War and Peace | Leo Tolstoy 
+    51 | Their Eyes Were Watching God | Zora Neale Hurston 
+    52 | Jurassic Park | Michael Crichton 
+    53 | The Godfather | Mario Puzo 
+    54 | One Hundred Years of Solitude | Gabriel García Márquez 
+    55 | The Picture of Dorian Gray | Oscar Wilde 
+    56 | The Notebook | Nicholas Sparks 
+    57 | The Shack | William P. Young 
+    58 | A Confederacy of Dunces | John Kennedy Toole 
+    59 | The Hunt for Red October | Tom Clancy 
+    60 | Beloved | Toni Morrison 
+    61 | The Martian | Andy Weir 
+    62 | The Wheel of Time (Series) | Robert Jordan, Brandon Sanderson 
+    63 | Siddhartha | Hermann Hesse 
+    64 | Crime and Punishment | Fyodor Dostoyevsky 
+    65 | The Sun Also Rises | Ernest Hemingway 
+    66 | The Curious Incident of the Dog in the Night-Time | Mark Haddon 
+    67 | A Separate Peace | John Knowles 
+    68 | Don Quixote | Miguel de Cervantes 
+    69 | The Lovely Bones | Alice Sebold 
+    70 | The Alchemist | Paulo Coelho 
+    71 | Hatchet (Series) | Gary Paulsen 
+    72 | Invisible Man | Ralph Ellison 
+    73 | The Twilight Saga (Series) | Stephenie Meyer 
+    74 | Tales of the City (Series) | Armistead Maupin 
+    75 | Gulliver's Travels | Jonathan Swift 
+    76 | Ready Player One | Ernest Cline 
+    77 | Left Behind (Series) | Tim LaHaye, Jerry B. Jenkins 
+    78 | Gone Girl | Gillian Flynn 
+    79 | Watchers | Dean Koontz 
+    80 | The Pilgrim's Progress | John Bunyan 
+    81 | Alex Cross Mysteries (Series) | James Patterson 
+    82 | Things Fall Apart | Chinua Achebe 
+    83 | Heart of Darkness | Joseph Conrad 
+    84 | Gilead | Marilynne Robinson 
+    85 | Flowers in the Attic | V.C. Andrews 
+    86 | Fifty Shades of Grey (Series) | E.L. James 
+    87 | The Sirens of Titan | Kurt Vonnegut 
+    88 | This Present Darkness | Frank E. Peretti 
+    89 | Americanah | Chimamanda Ngozi Adichie 
+    90 | Another Country | James Baldwin 
+    91 | Bless Me, Ultima | Rudolfo Anaya 
+    92 | Looking for Alaska | John Green 
+    93 | The Brief Wondrous Life of Oscar Wao | Junot Díaz 
+    94 | Swan Song | Robert R. McCammon 
+    95 | Mind Invaders | Dave Hunt 
+    96 | White Teeth | Zadie Smith 
+    97 | Ghost | Jason Reynolds 
+    98 | The Coldest Winter Ever | Sister Souljah 
+    99 | The Intuitionist | Colson Whitehead 
+    100 | Doña Bárbára | Rómulo Gallegos 
+'''[1:-1]
 def GetData():
     'Return list of (num, title, author)'
     o = []
@@ -530,31 +124,5 @@ def PrintData():
     # Print in color
     for n, ti, au in o:
         t.print(f"{t.num}{str(n):{wn}s} {t.title}{ti:{wti}s}{t.n} {t.author}{au:{wa}s}")
-PrintData()
-exit()
-
-
-o = []
-# Get array elements (num, title, author)
-while lines:
-    line = lines.popleft()
-    try:
-        n = int(line)
-        title = lines.popleft()
-        author = lines.popleft()
-        o.append((n, title, author))
-    except Exception:
-        pass
-if 1:
-    # Print neatly
-    wn = max(len(str(i[0])) for i in o) + 2
-    wti = max(len(str(i[1])) for i in o) + 2
-    wa = max(len(str(i[2])) for i in o) + 2
-    for n, ti, au in o:
-        t.print(f"{t.num}{str(n):{wn}s} {t.title}{ti:{wti}s}{t.n} {t.author}{au:{wa}s}")
-else:
-    # Print as python tuple
-    print("(")
-    for n, ti, au in o:
-        print(f"    ({n}, {ti!r}, {au!r}),")
-    print(")")
+if __name__ == "__main__":  
+    PrintData()
