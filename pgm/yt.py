da4558ca9c1745450ff74ba3174ccfcee8a09cd2
Don Peterson <someonesdad1@gmail.com>
2025-04-21 09:28:19 -0600

    Updated

--- pgm/yt.py
+++ pgm/yt.py
@@ -2,16 +2,15 @@
 Make an HTML file of URLs to videos
 '''
 if 1:   # Header
-    from collections import namedtuple
     from color import t
-    from dpprint import PP
-    from get import GetTextLines
+    from wrap import dedent
     import iso
     import requests
     import sys
     if 0:
         import debug
         debug.SetDebugger()
+    from dpprint import PP
     pp = PP()
 if 1:   # Classes
     class G:
@@ -32,12 +31,12 @@ if 1:   # Classes
             '''
             if not g.validate:
                 return
-            r = requests.get(entry.url)
+            r = requests.get(self.url)
             s = "This video isn't available anymore"
             if s in r.text:
                 t.print(f"{t.ornl}{self.url!r} not available")
         def __str__(self):
-            return f"{self.title} {self.url}"
+            return f'    {self.title} <a href="{self.url}">click</a><br>'
 if 1:   # Data
     data = {
         "Construction": [
@@ -50,17 +49,17 @@ if 1:   # Data
             Item("Land clearing with two bulldozers and an anchor chain", "2TL7fuVYyRg"),
         ],
 }
-if 1:   # Functions
-    def OutputHTML():
-        pass
-
 if __name__ == "__main__":
     now = iso.ISO()
-    print("Items:</p>")
+    print(dedent('''
+        <!DOCTYPE html>
+        <body>
+    '''))
     for category in data:
-        t.print(f"{t.ornl}{category}")
+        print(f"<h2>{category}</h2>")
         for item in data[category]:
             print(item)
-    if 0:
-        print(f"<p>Updated {now.date}<p>")
+    if 1:
+        print(f"<br><p>Updated {now.date}</p>")
+    print("</body>")
 # vim: tw=0 wm=0
