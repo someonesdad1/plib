51fb7a9cd5c8b3c8449d84842e2578734dbcc4db
Don Peterson <someonesdad1@gmail.com>
2025-04-21 18:25:59 -0600

    Updated

--- pgm/braces.py
+++ pgm/braces.py
@@ -1,38 +1,70 @@
 '''
     
+Definitions
+    - A brace pair is a pair of characters (a, b) defined where a is the opening
+      character and b is the closing character.
+    - Parity is a number associated with each defined character.  a's parity is +1 and
+      b's parity is -1.
+    - Parity sum is the sum of each brace pair's parity values over the whole file.
+      For a brace pair to be declared OK, the parity sum must be zero at the end of the
+      file.
+    - The parity sum at any point in the file must be >= 0.  If the sum is negative, it
+      is considered an error condition.
+
 Processing algorithm
     - The character pairs are kept track of by the dictionary g.char_pairs; the
       character is the key and the character's associated Char instance is the value.
     - Each character of the input file is examined to see if it is in g.char_pairs.  If
       it is, the character, offset, and line number are passed to the Char instance for
       logging.
+
+Test cases to consider/address:
+    - OK cases
+        - '{}', '{{}}', etc.
+    - Mismatched
+        - '{'       offset 0
+        - '}'       offset 0
+        - '{{}'     offset 0
+        - '{}}'     offset 2
+        - '}{}'     offset 0
+        - '}}{{'    offset 0
+
      
 '''
 _pgminfo = '''
 <oo desc
-    Script to locate unmatched braces 
+    Script to locate unmatched brace characters (defaults to '{}', '()', and '[]'
+    pairs).  Correct forms are brace occurrences like '{}', '{{}}', etc., with other
+    intervening characters.  Incorrect forms are things like '{{}', '}{', etc.
+
 oo>
 <oo cr Copyright © 2025 Don Peterson oo>
-<oo cat oo>
+<oo cat utility oo>
 <oo test none oo>
-<oo todo oo>
+<oo todo 
+
+    - Define parity.  Use it to detect a syntax error quickly and exit at that point.
+    - Tokenize python code and set all string content to space characters to maintain
+      character offsets.  Then process for braces.  The tokenizing might have to be done
+      as bytes, not strings.
+
+        - 'python -m tokenize -e a.py' does the needed job, as it identifies { as
+          LBRACE, ( as LPAR, [ as LSQB.
+
+oo>
 '''
 if 1:  # Header
     if 1:   # Standard imports
-        from collections import deque
-        from pathlib import Path as P
         import getopt
         import os
-        import re
         import sys
     if 1:   # Custom imports
-        from f import flt
         from wrap import dedent
         from color import t
         from lwtest import Assert
         from dpprint import PP
         pp = PP()   # Get pprint with current screen width
-        if 0:
+        if 1:
             import debug
             debug.SetDebugger()
     if 1:   # Global variables
@@ -40,40 +72,61 @@ if 1:  # Header
             pass
         g = G()
         g.dbg = True
+        g.test = False
         # The following dict keeps track of character pairs and their Char instance
         g.char_pairs = {}
         ii = isinstance
 if 1:   # Classes
+    class ParityError(Exception):
+        pass
     class Char:
-        def __init__(self, start, end):
-            self.start = start
-            self.end = end
-            Assert(len(start) == 1 and len(end) == 1)
+        def __init__(self, open, close):
+            self.open = open
+            self.close = close
+            Assert(len(open) == 1 and len(close) == 1)
             # Keep track of positions where characters were found
-            self.start_locations = []
-            self.end_locations = []
+            self.open_locations = []
+            self.close_locations = []
+            # Parity is sum of open (+1) and close (-1) characters' parities.  If parity
+            # sum is ever < 0, it's considered an error.
+            self.parity = 0
+            # Character counters
+            self.n_open = 0
+            self.n_close = 0
             # 'Register' with the character pair dictionary
-            if self.start in g.char_pairs:
-                Error(f"{self.start!r} already in g.char_pairs")
-            if self.end in g.char_pairs:
-                Error(f"{self.end!r} already in g.char_pairs")
-            g.char_pairs[self.start] = self
-            g.char_pairs[self.end] = self
+            if self.open in g.char_pairs:
+                Error(f"{self.open!r} already in g.char_pairs")
+            if self.close in g.char_pairs:
+                Error(f"{self.close!r} already in g.char_pairs")
+            g.char_pairs[self.open] = self
+            g.char_pairs[self.close] = self
         def __str__(self):
-            return f"Char({self.start}{self.end})"
+            return f"Char({self.open}{self.close})"
         def __repr__(self):
-            return f"Char(\n  {self.start_locations}\n  {self.end_locations}\n)"
+            return f"Char(\n  {self.open_locations}\n  {self.close_locations}\n)"
         def __bool__(self):
             "Return True if character pairs don't match, False otherwise"
-            return len(self.start_locations) != len(self.end_locations)
-        def found(self, character, offset, linenum, column):
-            'If character is found, log its offset, line number and column number'
-            if character == self.start:
-                self.start_locations.append((offset, linenum, column))
-            elif character == self.end:
-                self.end_locations.append((offset, linenum, column))
+            if not self.n_open and not self.n_close:
+                return False
+            return bool(self.parity)
+        def found(self, character, offset0b, linenum0b, column1b):
+            '''Log the occurrence of an open or close character.  offset0b is the
+            character offset into the file, linenum0b is the 0-based line number the
+            character is on, and column1b is the 0-based column number in this line.
+            '''
+            if character == self.open:
+                self.open_locations.append((offset0b, linenum0b, column1b))
+                self.parity += 1
+                self.n_open += 1
+            elif character == self.close:
+                self.close_locations.append((offset0b, linenum0b, column1b))
+                self.parity -= 1
+                self.n_close += 1
             else:
                 raise ValueError(f"{character!r} not tracked by this class")
+            if self.parity < 0:
+                raise ParityError(f"Parity < 0 for {character!r}:  offset={offset0b}, "
+                                  f"line={linenum0b + 1}, column={column1b}")
 if 1:   # Utility
     def GetColors():
         t.err = t.redl
@@ -99,28 +152,28 @@ if 1:   # Utility
     def Usage(status=0):
         print(dedent(f'''
         Usage:  {sys.argv[0]} [options] [file1 [file2...]]
-          Examine files for unmatched pairs of characters.  If no files are given, use
-          -- for stdin.
+          Examine files for unmatched pairs of characters.  If no files are given, the
+          script will use stdin for input.
         Options:
             -C s    Add set of characters s to character pairs
             -c s    Define the character pairs: '()[]{{}}' is the default
             -p      Print the line(s) with mismatches; color highlight the problem
+            -t      Run self tests
             -h      Print a manpage
         '''))
         exit(status)
     def ParseCommandLine(d):
         d["-c"] = "()[]{}"     # Character pairs
         d["-p"] = False        # Print the offending character in its line
-        if len(sys.argv) < 2:
-            Usage()
+        d["-t"] = False        # Run self tests
         GetColors()
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "C:c:hp") 
+            opts, args = getopt.getopt(sys.argv[1:], "C:c:hpt") 
         except getopt.GetoptError as e:
             print(str(e))
             exit(1)
         for o, a in opts:
-            if o[1] in list("p"):
+            if o[1] in list("pt"):
                 d[o] = not d[o]
                 Dbg(f"{o!r} option used")
             elif o == "-C":
@@ -136,6 +189,9 @@ if 1:   # Utility
             elif o == "-h":
                 Usage()
         RegisterCharacterPairs(d["-c"])
+        if d["-t"]:
+            g.test = True
+            SelfTests()
         return args
 if 1:   # Core functionality
     def RegisterCharacterPairs(pairs):
@@ -146,62 +202,83 @@ if 1:   # Core functionality
             Char(start, end)
     def Process(s, filename):
         'Process the string s contents of the indicated file'
-        linenum = 0         # Line number counter (0-based)
-        last_newline = 0    # Offset of last newline
-        # offset is character count into text file
-        for offset, char in enumerate(s):
+        linenum0b = 0       # Line number counter (0-based)
+        last_newline0b = 0    # Offset of last newline
+        parity_errors = []
+        for offset0b, char in enumerate(s):
             if char == "\n":
-                linenum += 1
-                last_newline = offset
+                linenum0b += 1
+                last_newline0b = offset0b
             if char in g.char_pairs:
                 Char_instance = g.char_pairs[char]
-                column = offset - last_newline
-                Char_instance.found(char, offset, linenum, column)
-        if 0:
-            for i in g.char_pairs:
-                item = g.char_pairs[i]
-                print(item)
-                print(item.start_locations)
-                print(item.end_locations)
-                print()
-            exit()
+                column = offset0b - last_newline0b
+                try:
+                    Char_instance.found(char, offset0b, linenum0b, column)
+                except ParityError as e:
+                    if 0:
+                        t.print(f"{t.ornl}Parity error for file {filename!r}")
+                        print(f"  {e}")
+                    else:
+                        s = f"{filename}[{linenum0b + 1}:{column}]:  Too many closing {char!r} characters"
+                        parity_errors.append(s)
         # Identify pair mismatches 
-        bad = set()
+        pair_mismatches = set()
         for item in g.char_pairs:
             char = g.char_pairs[item]
             if char:
-                bad.add(char)
-        if bad:
-            Report(s, filename, bad)
-    def Report(s, filename, bad):
-        '''Print report.  s is file's string and bad is the Char instances that don't
-        match.
+                pair_mismatches.add(char)
+        if not g.test and (pair_mismatches or parity_errors):
+            Report(s, filename, pair_mismatches, parity_errors)
+        else:
+            return pair_mismatches, parity_errors
+    def Report(s, filename, pair_mismatches, parity_errors):
+        '''Print report.  s is file's string, pair_mismatches is a set of mismatched
+        pairs, and parity_errors is a list of parity errors.
         '''
-        for item in bad:
-            start_locations = item.start_locations
-            end_locations = item.end_locations
-            if len(start_locations) > len(end_locations):
-                offset, linenum, column = start_locations[0]
-                char = item.start
+        for item in pair_mismatches:
+            open_locations = item.open_locations
+            close_locations = item.close_locations
+            if len(open_locations) > len(close_locations):
+                offset, linenum, column = open_locations[0]
+                char = item.open
             else:
-                offset, linenum, column = end_locations[-1]
-                char = item.end
-            print(f"{filename}[{linenum + 1}:{column}]:  {char!r} "
-                   "is missing matching character", end="")
-            if d["-p"]:
-                print(":")
-                # Get the string for the whole line
-                u = s.split("\n")[linenum]
-                # Decorate the mismatched character in color
-                print(f"  {u[:column - 1]}", end="")
-                print(f"{t.nomatch}{u[column - 1:column]}{t.N}", end="")
-                print(f"{u[column:]}")
-            else:
-                print()
-
+                offset, linenum, column = close_locations[-1]
+                char = item.close
+            print(f"{filename}[{linenum + 1}:{column + 1}]:  {char!r} "
+                   "is missing matching character")
+        for item in parity_errors:
+            print(item)
+if 1:   # Self tests
+    def EmptyFile():
+         # Empty file should also be successful
+         pair_mismatches, parity_errors = Process("", "")
+         Assert(not pair_mismatches and not parity_errors)
+         # So should be a file with no braces in it
+         pair_mismatches, parity_errors = Process("abc\ndef", "")
+         Assert(not pair_mismatches and not parity_errors)
+    def PairMismatch():
+         for s in ("((\n)", "[[\n]", "{{\n}"):
+            p, _ = Process(s, "")
+            c = p.pop()
+            Assert(c.open_locations == [(0, 0, 0), (1, 0, 1)])
+            Assert(c.close_locations == [(3, 1, 1)])
+    def ParityErrors():
+         for s in ("())", "[]]", "{}}"):
+            breakpoint() #xx 
+            _, p = Process(s, "")
+    def SelfTests():
+        EmptyFile()
+        PairMismatch()
+        ParityErrors()
+        exit(0)
+    ParityErrors()
+    exit()
 if __name__ == "__main__":
     d = {}      # Options dictionary
     files = ParseCommandLine(d)
-    if files:
+    if not files:
+        s = sys.stdin.read()
+        Process(s, "<stdin>")
+    else:
         for file in files:
             Process(open(file).read(), file)
