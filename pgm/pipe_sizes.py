53d95aa0c9b1722f2ea69840489134d4e6494517
Don Peterson <someonesdad1@gmail.com>
2025-04-26 09:04:54 -0600

    Updated

--- pgm/pipe_sizes.py
+++ pgm/pipe_sizes.py
@@ -21,12 +21,13 @@ if 1:  # Header
         import sys
     if 1:  # Custom imports
         from f import flt
+        from color import t
         from wrap import dedent
         import termtables as tt
-        if 0:
+        if len(sys.argv) > 1:
             import debug       
             debug.SetDebugger()
-if len(sys.argv) > 1:   # Original implementation
+if 0:   # Original implementation
     print(
         dedent(f'''
               Schedule 40 PVC pipe                    Schedule 80 PVC pipe
@@ -67,124 +68,122 @@ if len(sys.argv) > 1:   # Original implementation
         ''')
     )
 if 1:   # Using dimensions
-    o8, oq, oh, tq, t8 = "⅛ ¼ ½ ¾ ⅜".split()
-    sch40 = {
-        # Key is fractional size
-        # Value fields in mils are
-        #   OD
-        #   Wall
-        #   tpi
-        #   PD
-        #   Tap drill
-        #   Sch80 PVC wall thickness
-        Fraction(1, 8): (405,    70, 27,    364, 339),
-        Fraction(1, 4): (540,    90, 18,    477, 438),
-        Fraction(3, 8): (675,    90, 18,    612, 1000*(37/64)),
-        Fraction(1, 2): (840 ,  109, 14,    758, 1000*(23/32),   147/1000),
-        Fraction(3, 4): (1050,  113, 14,    968, 1000*(59/64),   154/1000),
-        Fraction(1, 1): (1315,  133, 11.5, 1214, 1000*(1+5/32),  179/1000),
-        Fraction(5, 4): (1660,  140, 11.5, 1557, 1000*(1+1/2),   191/1000),
-        Fraction(3, 2): (1900,  145, 11.5, 1796, 1000*(1+47/64), 200/1000),
-        Fraction(2, 1): (2375,  154, 11.5, 2269, 1000*(2+7/32),  218/1000),
-        Fraction(5, 2): (2875,  203, 8,    2720, 1000*(2+5/8)),
-        Fraction(3, 1): (3500,  216, 8,    3341, 1000*(3+1/4)),
-        Fraction(7, 2): (3000,  226, 8,    3838, 1000*(3+3/4)),
-        Fraction(4, 1): (4500,  237, 8,    4334, 1000*(4+1/4)),
-        Fraction(5, 1): (5563,  258, 8,    5391, 1000*(1.22)),
-        Fraction(6, 1): (6625,  280, 8,    6446, 1000*(1.58)),
-    }
-    def FF(fraction, use_unicode=False):
-        'Return a formatted fraction string'
-        i, r = divmod(fraction.numerator, fraction.denominator)
-        if use_unicode:
-            if fraction.denominator == 8:
-                if r == 1:
-                    return o8
-                elif r == 3:
-                    return t8
-                else:
-                    raise ValueError
-            elif fraction.denominator == 4:
-                if r == 1:
-                    if i == 0:
-                        return oq
-                    else:
-                        return f"1{oq}"
-                elif i == 0 and r == 3:
-                    return tq
-                elif r == 3:
-                    return tq
-                else:
-                    raise ValueError
-            elif fraction.denominator == 2:
-                if r == 1:
-                    if i == 1:
-                        return f"1{oh}"
-                    elif i == 2:
-                        return f"2{oh}"
-                    elif i == 3:
-                        return f"3{oh}"
-                    else:
-                        return oh
-                else:
-                    raise ValueError
-            else:
-                return str(int(fraction))
-        else:
+    def PipeSizes(metric=False):
+        o8, oq, oh, tq, t8 = "⅛ ¼ ½ ¾ ⅜".split()
+        sch40 = {
+            # Key is fractional size
+            # Value fields in mils are
+            #   OD
+            #   Wall
+            #   tpi
+            #   PD
+            #   Tap drill
+            #   Sch80 PVC wall thickness
+            Fraction(1, 8): (405,    70, 27,    364, 339),
+            Fraction(1, 4): (540,    90, 18,    477, 438),
+            Fraction(3, 8): (675,    90, 18,    612, 1000*(37/64)),
+            Fraction(1, 2): (840 ,  109, 14,    758, 1000*(23/32),   147/1000),
+            Fraction(3, 4): (1050,  113, 14,    968, 1000*(59/64),   154/1000),
+            Fraction(1, 1): (1315,  133, 11.5, 1214, 1000*(1+5/32),  179/1000),
+            Fraction(5, 4): (1660,  140, 11.5, 1557, 1000*(1+1/2),   191/1000),
+            Fraction(3, 2): (1900,  145, 11.5, 1796, 1000*(1+47/64), 200/1000),
+            Fraction(2, 1): (2375,  154, 11.5, 2269, 1000*(2+7/32),  218/1000),
+            Fraction(5, 2): (2875,  203, 8,    2720, 1000*(2+5/8)),
+            Fraction(3, 1): (3500,  216, 8,    3341, 1000*(3+1/4)),
+            Fraction(7, 2): (3000,  226, 8,    3838, 1000*(3+3/4)),
+            Fraction(4, 1): (4500,  237, 8,    4334, 1000*(4+1/4)),
+            Fraction(5, 1): (5563,  258, 8,    5391, 1000*(1.22)),
+            Fraction(6, 1): (6625,  280, 8,    6446, 1000*(1.58)),
+        }
+        def FF(fraction):
+            'Return a formatted fraction string'
+            i, r = divmod(fraction.numerator, fraction.denominator)
             if not r:
                 return f"{i}"
             elif i:
                 return f"{i}-{r}/{fraction.denominator}"
             else:
                 return f"{r}/{fraction.denominator}"
-    # Set up flt formatting
-    x = flt(0)
-    x.rtz = x.rdp = False
-    # Build table data
-    p = " "*1
-    header = [
-        f"{p}Size{p}",
-        f"{p}OD{p}",
-        f"{p}Wall{p}",
-        f"{p}ID{p}",
-        f"{p}tpi{p}",
-        f"{p}pitch{p}",
-        f"{p}PD{p}",
-        f"{p}Tap drill{p}",
-        f"{p}Wall Sch 80{p}"
-    ]
-    data = [[
-        f"{p}{'-'*6}{p}",   # Size
-        f"{p}{'-'*6}{p}",   # OD
-        f"{p}{'-'*6}{p}",   # Wall
-        f"{p}{'-'*5}{p}",   # ID
-        f"{p}{'-'*4}{p}",   # tpi
-        f"{p}{'-'*6}{p}",   # pitch
-        f"{p}{'-'*5}{p}",   # PD
-        f"{p}{'-'*9}{p}",   # TD
-        f"{p}{'-'*11}{p}"   # Wall80
-    ]]
-    for k in sch40:
-        entry = sch40[k]
-        #print(f"{i!s:5s} {FF(k)}")
-        if len(entry) == 5:
-            OD, wall, tpi, PD, TD = [flt(j) for j in entry]
-            wall80 = 0
+        # Set up flt formatting
+        x = flt(0)
+        x.rtz = x.rdp = True
+        # Table title
+        t.title = t.purl if metric else t.ornl 
+        t.body = t.sky if metric else t.whtl
+        if metric:
+            print(f"{' '*28}{t.title}", "US Pipe sizes in mm", t.body)
         else:
-            OD, wall, tpi, PD, TD, wall80 = [flt(j) for j in entry]
-        # Get columns
-        size = FF(k)
-        OD, wall, PD, TD = [flt(j)/1000 for j in (OD, wall, PD, TD)]
-        ID = OD - 2*wall
-        pitch = 1/tpi
-        elem = [size, 
-                f"{p}{OD}{p}", 
-                f"{p}{wall}{p}", 
-                f"{p}{ID}{p}", 
-                f"{p}{tpi}{p}", 
-                f"{p}{pitch}{p}", 
-                f"{p}{PD}{p}", 
-                f"{p}{TD}{p}", 
-                f"{p}{str(wall80) if wall80 else ''}{p}"] 
-        data.append(elem)
-    tt.print(data, header=header, padding=(0, 0), style=None, alignment="c"*len(header))
+            print(f"{' '*25}{t.title}", "US Pipe sizes in inches", t.body)
+        # Build table data
+        p = " "*1
+        header = [
+            f"{p}Size{p}",
+            f"{p}OD  {p}",
+            f"{p}Wall{p}",
+            f"{p}ID {p}",
+            f"{p}tpi{p}" if not metric else f"{p}   {p}",
+            f"{p}pitch{p}" ,
+            f"{p}PD  {p}",
+            f"{p}Tap drill{p}",
+            f"{p}Wall80{p}",
+            f"{p}ID80{p}"
+        ]
+        data = [[
+            f"{p}{'-'*5}{p}",   # Size
+            f"{p}{'-'*6}{p}",   # OD
+            f"{p}{'-'*5}{p}",   # Wall
+            f"{p}{'-'*5}{p}",   # ID
+            f"{p}{'-'*4}{p}" if not metric else f"{p}   {p}",   # tpi
+            f"{p}{'-'*6}{p}",   # pitch
+            f"{p}{'-'*6}{p}",   # PD
+            f"{p}{'-'*9}{p}",   # TD
+            f"{p}{'-'*6}{p}",   # Wall80
+            f"{p}{'-'*6}{p}"    # ID80
+        ]]
+        assert(len(header) == len(data[0]))
+        for k in sch40:
+            entry = sch40[k]
+            #print(f"{i!s:5s} {FF(k)}")
+            if len(entry) == 5:
+                OD, wall, tpi, PD, TD = [flt(j) for j in entry]
+                wall80 = 0
+            else:
+                OD, wall, tpi, PD, TD, wall80 = [flt(j) for j in entry]
+            # Get columns
+            size = FF(k)
+            OD, wall, PD, TD = [flt(j)/1000 for j in (OD, wall, PD, TD)]
+            ID = OD - 2*wall
+            pitch = 1/tpi
+            ID80 = OD - 2*wall80 if wall80 else 0
+            if metric:
+                n, m = 1, 25.4
+                elem = [f"{p}{size}{p}", 
+                        f"{p}{m*OD:>.{n}f}{p}", 
+                        f"{p}{m*wall:>.{n}f}{p}", 
+                        f"{p}{m*ID:>.{n}f}{p}", 
+                        f"{p}{''}{p}", 
+                        f"{p}{m*pitch:>.{n}f}{p}", 
+                        f"{p}{m*PD:>.{n}f}{p}", 
+                        f"{p}{m*TD:>.{n}f}{p}", 
+                        f"{p}{m*wall80:>.{n}f}{p}" if wall80 else f"{p}{p}", 
+                        f"{p}{m*ID80:>.{n}f}{p}" if wall80 else f"{p}{p}", 
+                ]
+            else:
+                n = 3
+                elem = [f"{p}{size}{p}", 
+                        f"{p}{OD:>.{n}f}{p}", 
+                        f"{p}{wall:>.{n}f}{p}", 
+                        f"{p}{ID:>.{n}f}{p}", 
+                        f"{p}{tpi}{p}", 
+                        f"{p}{pitch:>.{n + 1}f}{p}", 
+                        f"{p}{PD:>.{n}f}{p}", 
+                        f"{p}{TD:>.{n}f}{p}", 
+                        f"{p}{wall80:>.{n}f}{p}" if wall80 else f"{p}{p}",
+                        f"{p}{ID80:>.{n}f}{p}" if wall80 else f"{p}{p}"
+                ]
+            assert(len(header) == len(elem))
+            data.append(elem)
+        tt.print(data, header=header, padding=(0, 0), style=None, alignment="r"*len(header))
+    PipeSizes(metric=True)
+    t.print()
+    PipeSizes(metric=False)
