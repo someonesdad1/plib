168983b50f6bde21d73950baf2e752643a8f8ebe
Don Peterson <someonesdad1@gmail.com>
2025-04-25 18:05:43 -0600

    Updated

--- util.py
+++ util.py
@@ -114,6 +114,7 @@ if 1:  # Header
         import subprocess
         import sys
         import tempfile
+        import threading
         import time
         if platform.system() == "Windows":
             import msvcrt
@@ -132,6 +133,7 @@ if 1:  # Header
     if 1:  # Global variables
         ii = isinstance
         nl = "\n"
+        fsig_lock = threading.Lock()
 def US_states():
     "Return dictionary of US state abbreviations"
     a = '''AK AL AR AZ CA CO CT DE FL GA HI IA ID IL IN KS KY LA MA MD ME MI MN MO MS MT NC ND NE
@@ -1938,77 +1940,74 @@ def fsig(x, digits=None):
     
     This function is not thread-safe.
     '''
-    fsig.low = fsig.__dict__.get("low", 1e-5)
-    fsig.high = fsig.__dict__.get("high", 1e6)
-    fsig.digits = fsig.__dict__.get("digits", 3)
-    fsig.dp = fsig.__dict__.get("dp", ".")
-    fsig.rdp = fsig.__dict__.get("rdp", False)
-    fsig.rtz = fsig.__dict__.get("rtz", False)
-    fsig.rlz = fsig.__dict__.get("rlz", False)
-    def rtz(s):
-        if not fsig.rtz:
-            return s
-        t = list(s)
-        while t and t[-1] == "0":
-            del t[-1]
-        return "".join(t)
-    if fsig.low > fsig.high:
-        raise ValueError("fsig.low > fsig.high")
-    msg = "{}digits = {} is out of range"
-    if not (1 <= fsig.digits <= 15):
-        raise ValueError(msg.format("fsig.", fsig.digits))
-    if digits is not None and not (1 <= digits <= 15):
-        raise ValueError(msg.format("", digits))
-    if not isinstance(x, (float, int)):
-        raise TypeError("x must be a float or integer")
-    if isinstance(x, int):
-        x = float(x)
-    ndig = fsig.digits - 1 if digits is None else digits - 1
-    if x and (abs(x) < fsig.low or abs(x) > fsig.high):
-        xs = "{:.{}e}".format(x, ndig)  # Use scientific notation
-        st, e = xs.split("e")
-        t = "{}e{}".format(rtz(st), int(e))
-        return t.replace(".", fsig.dp)
-    # xs = list of significant digits with decimal point removed
-    # e = integer exponent
-    xs, e = "{:.{}e}".format(abs(x), ndig).replace(".", "").split("e")
-    xs, e = list(xs), int(e)
-    sgn = "-" if x < 0 else ""
-    if not e:
-        t = "{:.{}e}".format(abs(x), ndig).split("e")[0]
-        u = t.replace(".", fsig.dp)
-        v = rtz(u)
-        if fsig.rdp and v[-1] == fsig.dp:
-            v = v[:-1]
-        return sgn + v
-    elif e < 0:
-        e = abs(e) - 1
-        xs.reverse()
-        while e:
-            xs.append("0")
-            e -= 1
-        xs.append(fsig.dp)
-        if not fsig.rlz:
-            xs.append("0")
-        xs.reverse()
-    else:
-        n = len(xs)
-        if e >= n:
-            e -= n - 1
+    with fsig_lock:
+        fsig.low = fsig.__dict__.get("low", 1e-5)
+        fsig.high = fsig.__dict__.get("high", 1e6)
+        fsig.digits = fsig.__dict__.get("digits", 3)
+        fsig.dp = fsig.__dict__.get("dp", ".")
+        fsig.rdp = fsig.__dict__.get("rdp", False)
+        fsig.rtz = fsig.__dict__.get("rtz", False)
+        fsig.rlz = fsig.__dict__.get("rlz", False)
+        def rtz(s):
+            if not fsig.rtz:
+                return s
+            t = list(s)
+            while t and t[-1] == "0":
+                del t[-1]
+            return "".join(t)
+        if fsig.low > fsig.high:
+            raise ValueError("fsig.low > fsig.high")
+        msg = "{}digits = {} is out of range"
+        if not (1 <= fsig.digits <= 15):
+            raise ValueError(msg.format("fsig.", fsig.digits))
+        if digits is not None and not (1 <= digits <= 15):
+            raise ValueError(msg.format("", digits))
+        if not isinstance(x, (float, int)):
+            raise TypeError("x must be a float or integer")
+        if isinstance(x, int):
+            x = float(x)
+        ndig = fsig.digits - 1 if digits is None else digits - 1
+        if x and (abs(x) < fsig.low or abs(x) > fsig.high):
+            xs = "{:.{}e}".format(x, ndig)  # Use scientific notation
+            st, e = xs.split("e")
+            t = "{}e{}".format(rtz(st), int(e))
+            return t.replace(".", fsig.dp)
+        # xs = list of significant digits with decimal point removed
+        # e = integer exponent
+        xs, e = "{:.{}e}".format(abs(x), ndig).replace(".", "").split("e")
+        xs, e = list(xs), int(e)
+        sgn = "-" if x < 0 else ""
+        if not e:
+            t = "{:.{}e}".format(abs(x), ndig).split("e")[0]
+            u = t.replace(".", fsig.dp)
+            v = rtz(u)
+            if fsig.rdp and v[-1] == fsig.dp:
+                v = v[:-1]
+            return sgn + v
+        elif e < 0:
+            e = abs(e) - 1
+            xs.reverse()
             while e:
                 xs.append("0")
                 e -= 1
             xs.append(fsig.dp)
+            if not fsig.rlz:
+                xs.append("0")
+            xs.reverse()
         else:
-            xs.insert(e + 1, fsig.dp)
-    t = rtz("".join(xs))
-    if fsig.rdp and t[-1] == fsig.dp:
-        t = t[:-1]
-    return sgn + t
-
-if 0:
-    print(hyphen_range("1-3 6"))
-    exit()
+            n = len(xs)
+            if e >= n:
+                e -= n - 1
+                while e:
+                    xs.append("0")
+                    e -= 1
+                xs.append(fsig.dp)
+            else:
+                xs.insert(e + 1, fsig.dp)
+        t = rtz("".join(xs))
+        if fsig.rdp and t[-1] == fsig.dp:
+            t = t[:-1]
+        return sgn + t
 
 if __name__ == "__main__":
     # Missing tests for: Ignore Debug, GetString
@@ -2846,6 +2845,7 @@ if __name__ == "__main__":
             flt
             Fraction
             frange
+            fsig_lock
             glob
             groupby
             hashlib
@@ -2881,6 +2881,7 @@ if __name__ == "__main__":
             sys
             tempfile
             Test
+            threading
             time
             ToDo
             translated_util_simlink
