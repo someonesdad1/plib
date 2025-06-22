60f75c2ffdd50892bfe08aac5a9edb4e19441747
Don Peterson <someonesdad1@gmail.com>
2025-06-22 09:39:26 -0600

    Updated

---
+++ pgm/6033.py
@@ -0,0 +1,107 @@
+'''
+'''
+_pgminfo = '''
+<oo 
+    Script to calculate resistance values for a remote control box for the HP 6033A DC
+    power supply (and similar power supplies).
+oo>
+<oo cr Copyright © 2025 Don Peterson oo>
+<oo cat oo>
+<oo test none oo>
+<oo todo oo>
+'''
+ 
+if 1:  # Header
+    if 1:   # Standard imports
+        from collections import deque
+        from pathlib import Path as P
+        import getopt
+        import os
+        import re
+        import sys
+    if 1:   # Custom imports
+        from f import flt
+        from wrap import dedent
+        from color import t
+        from lwtest import Assert
+        from dpprint import PP
+        pp = PP()   # Get pprint with current screen width
+        if 0:
+            import debug
+            debug.SetDebugger()
+    if 1:   # Global variables
+        class G:
+            pass
+        g = G()
+        g.dbg = False
+        ii = isinstance
+if 1:   # Utility
+    def GetColors():
+        t.stuff = t.lill
+        t.err = t.redl
+        t.dbg = t.lill if g.dbg else ""
+        t.N = t.n if g.dbg else ""
+    def GetScreen():
+        'Return (LINES, COLUMNS)'
+        return (
+            int(os.environ.get("LINES", "50")),
+            int(os.environ.get("COLUMNS", "80")) - 1
+        )
+    def Dbg(*p, **kw):
+        if g.dbg:
+            print(f"{t.dbg}", end="")
+            print(*p, **kw)
+            print(f"{t.N}", end="")
+    def Warn(*msg, status=1):
+        print(*msg, file=sys.stderr)
+    def Error(*msg, status=1):
+        Warn(*msg)
+        exit(status)
+    def Usage(status=0):
+        print(dedent(f'''
+        Usage:  {sys.argv[0]} [options] etc.
+          Interactive script to calculate the needed resistances for the remote
+          programming box for the HP 603X power supplies.  You will input the desired
+          current and voltage levels.
+
+          These calculations are specific to the custom box I plan to make for these
+          power supplies.  This box has one coarse-adjust pot and one fine-adjust pot.
+          This fits the following use case:  a typical experiment needs to have either
+          voltage or current set to some maximum value, then the other parameter is
+          finely adjusted for the experiment's output.  These pots have auxiliary pots
+          or resistors that can be used to limit the low and high values reachable.
+        Options:
+            -3      Use the 6033A power supply [default]
+            -8      Use the 6038A power supply
+        '''))
+        exit(status)
+    def ParseCommandLine(d):
+        d["-a"] = False     # Need description
+        d["-d"] = 3         # Number of significant digits
+        if len(sys.argv) < 2:
+            Usage()
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], "ad:h") 
+        except getopt.GetoptError as e:
+            print(str(e))
+            exit(1)
+        for o, a in opts:
+            if o[1] in list("a"):
+                d[o] = not d[o]
+            elif o == "-d":
+                try:
+                    d[o] = int(a)
+                    if not (1 <= d[o] <= 15):
+                        raise ValueError()
+                except ValueError:
+                    Error(f"-d option's argument must be an integer between 1 and 15")
+            elif o == "-h":
+                Usage()
+        GetColors()
+        return args
+if 1:   # Core functionality
+    pass
+
+if __name__ == "__main__":
+    d = {}      # Options dictionary
+    args = ParseCommandLine(d)
