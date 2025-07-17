91b17ac3deb6b6d1ed7746ec293bc1606c51ef3c
Don Peterson <someonesdad1@gmail.com>
2025-07-17 11:16:47 -0600

    Updated

--- pgm/draintank.py
+++ pgm/draintank.py
@@ -36,13 +36,24 @@ if 1:  # Header
     from pdb import set_trace as xx
     # Custom imports
     from wrap import wrap, dedent
-    from color import Color, TRM as t
+    from color import t
     from f import flt, pi, sqrt
     import u
     # Global variables
-    ii = isinstance
-    W = int(os.environ.get("COLUMNS", "80")) - 1
-    L = int(os.environ.get("LINES", "50"))
+    class G:
+        pass
+    g = G()
+    g.g = 9.81  # Acceleration of gravity
+    g.Vs = 0    # Volume input string
+    g.V = 0     # Volume in m³
+    g.hs = 0    # Height input string
+    g.h = 0     # Height of fluid level in m
+    g.Ds = 0    # Hole diameter input string
+    g.D = 0     # Hole diameter in m
+    g.Cs = 0    # Coefficient of discharge input string
+    g.μ = 0     # Coefficient of discharge 
+    g.msg = ""  # Test case message
+
 if 1:  # Utility
     def Error(*msg, status=1):
         print(*msg, file=sys.stderr)
@@ -81,6 +92,25 @@ if 1:  # Utility
         x.n = d["-d"]
         return args
 if 1:  # Core functionality
+    def PrintMessage():
+        print(dedent('''
+        Calculate the time to drain a cylindrical tank of water (or fluids with viscosities
+        near water) through a hole.  You'll be asked for a tank volume in m³, a height in m,
+        and a drain hole diameter in m.  If you want to use other units, append them to the
+        number you type in.
+        
+        The discharge coefficient is the fraction of the ideal mass flow capable through the
+        hole; it's a fraction of ρ*dV/dt with typical values for sharp holes around 0.6 to
+        0.65.  Some other values are:
+            Smoothly rounded    0.98
+            Short tube          0.8
+            Sharp               0.6 to 0.65
+            Borda               0.51 (short tube protruding inside tank)
+        
+        Example:  a 50 gallon drum of water 1 m high with a 1 inch hole hole in the bottom
+        will take 1.83 minutes.
+        '''))
+        print()
     def GetNumber(prompt, units, default=None):
         "Return (input, value_in_SI)"
         while True:
@@ -101,58 +131,74 @@ if 1:  # Core functionality
                 except Exception as e:
                     print(e)
                     print("Try again")
+    def TimeToDrain(hole_diameter, coeff_of_discharge, volume, height):
+        'Return the time to drain the tank in seconds'
+        d = hole_diameter
+        μ = coeff_of_discharge
+        V = volume
+        h = height
+        g = 9.81  # Acceleration of gravity in m/s²
+        A = pi*d**2/4  # Hole area in m²
+        t = μ*V/A*sqrt(2/(h*g))  # Time to drain tank in s
+        return t
+    def GetProblem():
+        # All units are SI
+        #   V = Volume of tank
+        #   h = Height of fluid at start of discharge
+        #   D = Drain hole diameter
+        #   μ = Coefficient of discharge
+        problem = 1
+        if problem == 0:
+            g.Vs, g.V = GetNumber("What is volume? ", "m³")
+            g.hs, g.h = GetNumber("What is height? ", "m")
+            g.Ds, g.D = GetNumber("What is hole diameter? ", "m")
+            g.Cs, g.μ = GetNumber("What is coefficient of discharge [0.65]?  ", "", 0.65)
+        elif problem == 1:
+            # 50 gal drum 1 m high with 1 inch hole
+            g.Vs, g.V = "50 gal", flt(50) * u.u("gal")
+            g.hs, g.h = "1 m", flt(1) * u.u("m")
+            g.Ds, g.D = "1 in", flt(1) * u.u("in")
+            g.Cs, g.μ = "0.65", flt(0.65)
+            g.msg = "50 gallon drum test case"
+            # Gives 1.83 minutes
+        elif problem == 2:
+            # This estimate was made for our sprayer draining through a chunk
+            # of 1-1/4 inch pipe and it feels about right.
+            g.Vs, g.V = "15 gal", flt(15) * u.u("gal")
+            g.hs, g.h = "10 in", flt(10) * u.u("in")
+            g.Ds, g.D = "1.38 in", flt(1.38) * u.u("in")
+            g.Cs, g.μ = "0.65", flt(0.65)
+            g.msg = "15 gallon sprayer tank test case"
+            # Gives 34.3 s
+        else:
+            print("Unsupported problem number")
+            exit(1)
+    def PrintResults():
+        A = pi*g.D**2/4  # Area in m²
+        T = TimeToDrain(g.D, g.μ, g.V, g.h)
+        # Colors
+        t.title = t.ornl
+        t.result = t.yel
+        t.testcase = t.magl
+        # Print report
+        t.print(f"{t.title}Input data            User                 SI")
+        print(f"  Volume              {g.Vs:20s} {g.V} m³")
+        print(f"  Height              {g.hs:20s} {g.h} m")
+        print(f"  Hole dia            {g.Ds:20s} {g.D} m")
+        print(f"  Coeff of discharge  {g.Cs:20s} {g.μ}")
+        print()
+        if g.msg:
+            t.print(f"{t.testcase}{g.msg}")
+        if T >= 3600:
+            t.print(f"{t.result}Drain time  {T/3600} hours")
+        elif T >= 60:
+            t.print(f"{t.result}Drain time  {T/60} minutes")
+        else:
+            t.print(f"{t.result}Drain time  {T} s")
 
 if __name__ == "__main__":
     d = {}  # Options dictionary
     args = ParseCommandLine(d)
-    print(dedent('''
-    Calculate the time to drain a tank of water (or fluids with viscosities near water) through a
-    hole.  You'll be asked for a tank volume in m³, a height in m, and a drain hole diameter in m.
-    If you want to use other units, append them to the number you type in.
-    
-    The discharge coefficient is the fraction of the ideal mass flow capable through the hole; it's
-    a fraction of ρ*dV/dt with typical values for sharp holes around 0.6 to 0.65.
-    
-    Example:  a 50 gallon drum of water 1 m high with a 1 inch hole hole in the bottom will take
-    1.8 minutes.
-    ''')
-    )
-    # All units are SI
-    #   V = Volume of tank
-    #   h = Height of fluid at start of discharge
-    #   d = Drain hole diameter
-    #   μ = Coefficient of discharge
-    if 1:
-        Vs, V = GetNumber("What is volume? ", "m3")
-        Hs, h = GetNumber("What is height? ", "m")
-        Ds, d = GetNumber("What is hole diameter? ", "m")
-        Cs, μ = GetNumber("What is coefficient of discharge [0.65]?  ", "", 0.65)
-    elif 0:
-        # 50 gal drum 1 m high with 1 inch hole
-        Vs, V = "50 gal", flt(50) * u.u("gal")
-        Hs, h = "1 m", flt(1) * u.u("m")
-        Ds, d = "1 in", flt(1) * u.u("in")
-        Cs, μ = "0.65", flt(0.65)
-        # Gives 1.83 minutes
-    else:
-        # This estimate was made for our sprayer draining through a chunk
-        # of 1-1/4 inch pipe and it feels about right.
-        Vs, V = "15 gal", flt(15) * u.u("gal")
-        Hs, h = "10 in", flt(10) * u.u("in")
-        Ds, d = "1.38 in", flt(1.38) * u.u("in")
-        Cs, μ = "0.65", flt(0.65)
-        # Gives 34.3 s
-    g = 9.8  # Acceleration of gravity in m/s²
-    A = pi * d**2 / 4  # Area in m²
-    t = μ * V / A * sqrt(2 / (h * g))  # Time to drain tank in s
-    # Print report
-    print(f"Volume              {Vs:20s} = {V} m³")
-    print(f"Height              {Hs:20s} = {h} m")
-    print(f"Hole dia            {Ds:20s} = {d} m")
-    print(f"Coeff of discharge  {Cs:20s} = {μ}")
-    if t >= 3600:
-        print(f"\nDrain time  {t / 3600} = hours")
-    elif t >= 60:
-        print(f"\nDrain time  {t / 60} = minutes")
-    else:
-        print(f"\nDrain time  {t} = s")
+    PrintMessage()
+    GetProblem()
+    PrintResults()
