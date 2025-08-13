60c431710d38ac27c79aac33fbd8282177bd4658
Don Peterson <someonesdad1@gmail.com>
2025-08-12 20:04:29 -0600

    Updated

--- pgm/hex.py
+++ pgm/hex.py
@@ -12,75 +12,83 @@ if 1:  # Header
         import getopt
         import sys
     if 1:   # Custom imports
+        import termtables as tt
         from color import t
         from wrap import dedent
+        from get import GetInt
     if 1:   # Global variables
         class G:
             pass
         g = G()
         g.results = []
+        # Colors for output
+        t.input, t.hex, t.oct, t.dec = t.brnl, t.ornl, t.sky, t.lavl
 if 1:   # Utility
     def Usage(status=0):
         print(dedent(f'''
         Usage:  {sys.argv[0]} [options] arg1 [arg2...]
           Convert arguments to base 8, 10, 16 forms.  For negative integers, use '--' to
-          indicate there are no more options.  A leading "0x", "h", or "H" indicate a
-          hex number.  A leading "0" or "0o" indicate an octal integer.  An appended
-          ":n" indicates a base n number.
+          indicate there are no more options (or put argument in quotes).  A leading 0x,
+          h, or H indicate a hex number.  A leading 0 or 0o indicate an octal integer.
+          An appended ":n" indicates a base n number.
         Examples:  
           1.  '10 10.0 10.273 012 0o12 0xa ha Ha' are all forms of the number 10
-          2.  '10 10.0 10.273 012 0o12 0xa ha Ha' are all forms of the number 10
+          2.  '1000:32' is 32768 in base 32
         Options
           -S    Sort the output by largest to smallest
           -s    Sort the output by smallest to largest
-          -x    Don't include "0x" in hex form
+          -x    Don't include "0x" or "0o" in output
         '''))
         exit(status)
     def ParseCommandLine(d):
         d["-S"] = False     # Sort the output by largest to smallest
-        d["-S"] = False     # Sort the output by smallest to largest
+        d["-s"] = False     # Sort the output by smallest to largest
         d["-x"] = False
         if len(sys.argv) < 2:
             Usage()
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "hx") 
+            opts, args = getopt.getopt(sys.argv[1:], "hSsx") 
         except getopt.GetoptError as e:
             print(str(e))
             exit(1)
         for o, a in opts:
-            if o[1] in list("x"):
+            if o[1] in list("Ssx"):
                 d[o] = not d[o]
             elif o == "-h":
                 Usage()
         return args
 if 1:   # Core functionality
-    def GetInteger(arg):
-        'Return arg as an integer'
     def Convert(arg):
-        s = arg.strip()
-        sign = "-" if s[0] == "-" else ""
-        if s[0] == "-":
-            s = s[1:]
-        # Get hex form
-        a = f"{arg.strip()}"
         try:
-            if "e" in s or "E" in s:
-                h = f"{sign + hex(int(float(s)))}"
-            else:
-                h = f"{sign + hex(int(s))}"
+            n = GetInt(arg)
         except Exception:
-            t.print(f"{t.ornl}Couldn't convert {arg.strip()!r}")
+            t.print(f"{t.ornl}Couldn't convert {arg.strip()!r}", file=sys.stderr)
+            return
+        if isinstance(arg, str):
+            input = arg.strip()
         else:
-            g.results.append((a, h))
+            input = str(arg)
+        hex = f"{n:x}" if d["-x"] else f"0x{n:x}"
+        oct = f"{n:o}" if d["-x"] else f"0o{n:o}"
+        dec = f"{n:d}"
+        g.results.append((input, dec, hex, oct))
+    def Report():
+        header, o = (f"{t.input}Input", f"{t.dec}Decimal", f"{t.hex}Hex", f"{t.oct}Octal"), []
+        # Sort g.results if necessary
+        if d["-s"]:     # Sort smallest to largest
+            g.results = sorted(g.results, key=lambda x: int(x[1]))
+        elif d["-S"]:   # Sort largest to smallest
+            g.results = sorted(g.results, key=lambda x: int(x[1]), reverse=True)
+        # Colorize and append to o
+        for input, dec, hex, oct in g.results:
+            o.append((f"{t.input}{input}", f"{t.dec}{dec}",
+                      f"{t.hex}{hex}", f"{t.oct}{oct}"))
+        tt.print(o, header, style=" "*15, alignment="cccc")
 
 if __name__ == "__main__":
     d = {}      # Options dictionary
     args = ParseCommandLine(d)
     for arg in args:
         Convert(arg)
-    # Report
     if g.results:
-        # Get width of largest input string
-        w = max(len(s) for s, _ in g.results)
-        for i, o in g.results:
-            t.print(f"{t.yel}{i:{w}s} {t.sky}{o}")
+        Report()
