c950fdd44b3f8a1d333c557c1e3d7d15fb33a4de
Don Peterson <someonesdad1@gmail.com>
2025-11-10 09:32:47 -0700

    Updated

--- pgm/sha.py
+++ pgm/sha.py
@@ -1,7 +1,6 @@
-"""
+'''
 Print out the SHA hashes for files on the command line.
-"""
-
+'''
 if 1:  # Header
     if 1:  # Copyright, license
         # These "trigger strings" can be managed with trigger.py
@@ -26,11 +25,13 @@ if 1:  # Header
         import zlib
     if 1:  # Custom imports
         from wrap import dedent
+        from color import t
+    if 1:  # Global variables
+        t.trunc = t.brnl
 if 1:  # Utility
-
     def Usage(status=1):
         print(
-            dedent(f"""
+            dedent(f'''
         Usage:  {sys.argv[0]} [options] [file1 [file2...]]
           Calculate the hash of the given files.  Use '-' for stdin.  If a
           command line argument is a directory, it's silently ignored.
@@ -45,15 +46,17 @@ if 1:  # Utility
             -8      ADLER32                     32
             -a      Show hash for each of the different methods
             -c n    Truncate hash to n characters
-            -w      Remove whitespace from each file
-            """)
+            -t      Truncate all hashes to 16 characters
+            -w      Open as UTF8 text file, read, remove all whitespace,
+                    encode to bytes, then calculate hash
+            ''')
         )
         exit(status)
-
     def ParseCommandLine():
         d["-a"] = False  # Use all hash methods
         d["-h"] = False  # Help
-        d["-n"] = None  # Truncate hash to n characters
+        d["-n"] = None   # Truncate hash to n characters
+        d["-t"] = False  # Truncate hash to 16 characters
         d["-w"] = False  # Remove whitespace from each file
         d["-1"] = False  # SHA1
         d["-2"] = False  # SHA224
@@ -74,19 +77,21 @@ if 1:  # Utility
             "-8": (zlib.adler32, "ADLER32"),
         }
         try:
-            opts, files = getopt.getopt(sys.argv[1:], "12345678ahn:w")
+            opts, files = getopt.getopt(sys.argv[1:], "12345678ahn:tw")
         except getopt.GetoptError as e:
             msg, option = e
             print(msg)
             exit(1)
         L = d["L"] = "-1 -2 -3 -4 -5 -6 -7 -8".split()
         for o, a in opts:
-            if o in L + ["-a", "-w"]:
+            if o in L + ["-a", "-t", "-w"]:
                 d[o] = not d[o]
             elif o == "-n":
                 d[o] = int(a)
                 if d[o] <= 0:
                     Error("-n option must be > 0")
+        if d["-t"]:
+            d["-n"] = 32
         if d["-a"]:
             for i in L:
                 d[i] = True
@@ -97,11 +102,10 @@ if 1:  # Utility
             d["-3"] = True  # Use SHA256 by default
         d["more_than_one"] = num > 1
         return files
-
-
 if 1:  # Core functionality
-
     def GetHash(Bytes, method):
+        'Return (h, t) where h is hash and t is True if truncated'
+        truncated = False
         if method == zlib.crc32:
             i = zlib.crc32(Bytes)
             h = f"{i:08x}"
@@ -113,9 +117,11 @@ if 1:  # Core functionality
             h.update(Bytes)
             h = h.hexdigest()
         if d["-n"] is not None:
-            h = h[: d["-n"]]
-        return h
-
+            old = h
+            new = h[:d["-n"]]
+            truncated = True if old != new else False
+            h = new
+        return (h, truncated)
     def GetBytes(file):
         "Read file in binary as a bytes object"
         if file == "-":
@@ -131,16 +137,35 @@ if 1:  # Core functionality
                 print(f"Couldn't read {file!r}", file=sys.stderr)
                 return None
         assert type(b) == bytes
-        if d["-w"]:
-            b = re.sub(rb"\s+", b"", b)  # Remove whitespace
         return b
-
+    def GetTextFile(file):
+        'Read as UTF8 text, remove whitespace, return bytes'
+        if file == "-":
+            b = sys.stdin.read()
+        else:
+            p = P(file)
+            if p.is_dir():
+                return None
+            try:
+                b = p.open("r").read()
+            except Exception:
+                print(f"Couldn't read {file!r}", file=sys.stderr)
+                return None
+        # Remove whitespace
+        b = re.sub(r"\s+", "", b)
+        # Convert to binary
+        b = b.encode()
+        return b
 
 if __name__ == "__main__":
     d = {}  # Options dictionary
     files = ParseCommandLine()
+    show_color_message = False
     for file in files:
-        b = GetBytes(file)
+        if d["-w"]:
+            b = GetTextFile(file)
+        else:
+            b = GetBytes(file)
         if b is None:
             continue
         if d["more_than_one"]:
@@ -148,11 +173,21 @@ if __name__ == "__main__":
             for i in d["L"]:
                 if d[i]:
                     method, name = d["method"][i]
-                    h = GetHash(b, method)
-                    print(f"  {name:7s} {h}")
+                    h, truncated = GetHash(b, method)
+                    if truncated:
+                        t.print(f"{t.trunc}  {name:7s} {h}")
+                        show_color_message = True
+                    else:
+                        print(f"  {name:7s} {h}")
         else:
             for i in d["L"]:
                 if d[i]:
                     method, name = d["method"][i]
-                    h = GetHash(b, method)
-                    print(f"{h} {'<stdin>' if file == '-' else file}")
+                    h, truncated = GetHash(b, method)
+                    if truncated:
+                        t.print(f"{t.trunc}{h} {'<stdin>' if file == '-' else file}")
+                        show_color_message = True
+                    else:
+                        print(f"{h} {'<stdin>' if file == '-' else file}")
+    if show_color_message:
+        print(f"A hash in {t.trunc}this color{t.n} means it was truncated")
