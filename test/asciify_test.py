82567321f0535867947ef1e03a0d0caa9b3a55e5
Don Peterson <someonesdad1@gmail.com>
2026-01-18 20:10:54 -0700

    Updated

--- test/asciify_test.py
+++ test/asciify_test.py
@@ -4,17 +4,14 @@ from lwtest import run, raises
 from asciify import Asciify
 from pdb import set_trace as xx
 from color import TRM as t
-
 t.e = t("grnl")
 t.g = t("redl")
-
-
 def TestAsciify1():
-    """Unfortunately, I didn't document where this set of test cases
+    '''Unfortunately, I didn't document where this set of test cases
     came from.  In the "A" line, the last letter fails, but it's a
     Roman wide A, so it should pass.  Thus, the asciify.py script needs
     updating.
-    """
+    '''
     return
     d = {
         "A": "ÀÁÂÃÄÅĀĂǍǞǠǺȀȂȦȺⒶḀẠẢẤẦẨẪẬẮẰẲẴẶＡ",
@@ -91,13 +88,11 @@ def TestAsciify1():
             for i in ltrs:
                 if Asciify(i) != asc:
                     print(asc, i, hex(ord(i)))
-
-
 def TestAsciify():
-    """This test uses the /pylib/asciify.test file, which contains lines
+    '''This test uses the /pylib/asciify.test file, which contains lines
     that have a character X, tab, and a set of Unicode codepoints that
     should Asciify to X.
-    """
+    '''
     p = pathlib.Path("/pylib/asciify.test")
     s = p.read_text()
     print("asciify_test.py fails on these characters:")
@@ -110,13 +105,7 @@ def TestAsciify():
         for i in chars:
             got = Asciify(i)
             if got != expected:
-                print(
-                    f"{i:6s}:  {t.e}{expected:^{w}s} {t.g}{got:^{w}s}{t.n} {hex(ord(i))}"
-                )
-
-
+                print(f"{i:6s}:  {t.e}{expected:^{w}s} {t.g}{got:^{w}s}{t.n} {hex(ord(i))}")
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        exit(run(globals(), broken=True)[0])
-    else:
-        exit(run(globals())[0])
+    #exit(run(globals(), broken=True)[0])
+    exit(run(globals())[0])
--- test/stack_test.py
+++ test/stack_test.py
@@ -1,48 +1,39 @@
 import sys
 from stack import Stack
 from lwtest import run, raises
-from pdb import set_trace as xx
-
 n = 5
-
-
 def init():
     "Make a stack with n integers"
     st = Stack()
     for i in range(n):
         st.push(i)
     return st
-
-
 def TestLen():
     st = init()
     assert len(st) == n
-
-
 def TestPop():
     st = init()
     R = list(st)
-    for i in R:
-        assert st.pop() == i
+    n = len(R)
+    for i in range(n):
+        item = st.pop()
+        ritem = R.pop()
+        assert item == ritem
     assert len(st) == 0
     assert not st
     with raises(IndexError):
         st.pop()
-
-
 def TestPush():
     st = Stack()
     r = list(range(n))
     R = list(reversed(r))
     for i in r:
         st.push(i)
-    assert list(st) == R
+    assert list(st) == r
     for i in R:
         assert st.pop() == i
     assert len(st) == 0
     assert not st
-
-
 def TestCopy():
     "The copy() method appeared in python 3.5."
     v = sys.version_info
@@ -52,31 +43,27 @@ def TestCopy():
     s = st.copy()
     assert st == s
     assert id(st) != id(s)
-
-
 def TestClear():
     st = init()
     assert len(st) == n
     st.clear()
     assert len(st) == 0
-
-
 def TestHomogeneity():
-    st = Stack(["a", "b"], homogeneous="a")
-    assert st.homogeneous and st.homogeneous == type("a")
+    # Homogeneous means here st can only contain strings
+    st = Stack(["a", "b"], homogeneous=str)
+    assert st.homogeneous is str
     raises(TypeError, st.push, 1)
     st.push("b")
-    # Non-homogeneous allows homogeneity
-    st = init()
+    assert str(st) == "Stack(['a', 'b', 'b'], homogeneous=<class 'str'>)"
+    # Non-homogeneous
+    st = Stack()
     st.push("a")
-
-
+    st.push(1)
+    assert str(st) == "Stack(['a', 1])"
 def TestMaxlen():
     st = Stack([1, 2], maxlen=2)
     st.push(3)
-    assert list(st) == [3, 1]
+    assert list(st) == [2, 3]
     assert st.maxlen == 2
-
-
 if __name__ == "__main__":
     exit(run(globals())[0])
