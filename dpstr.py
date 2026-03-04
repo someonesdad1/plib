7d162485858253c2cce2ad5bc2ddfff56d6d8ed8
Don Peterson <someonesdad1@gmail.com>
2026-03-04 08:01:50 -0700

    Updated

--- dpstr.py
+++ dpstr.py
@@ -101,6 +101,7 @@ if 1:   # Header
         import struct
         import subprocess
         import sys
+        import textwrap
         import time
     if 1:   # Custom imports
         import asciify
@@ -115,6 +116,7 @@ if 1:   # Header
     if 1:   # Import symbols
         Fraction = fractions.Fraction
         Path = pathlib.Path
+        TextWrapperOrig = textwrap.TextWrapper
         defaultdict = collections.defaultdict
         deque = collections.deque
         filterfalse = itertools.filterfalse
@@ -1619,6 +1621,105 @@ if 1:   # Core functionality
             if "8" in keys or "0" in keys:
                 pass
             return b
+    class TextWrapper(TextWrapperOrig):
+        '''This is the same as the textwrap.TextWrapper class except the
+        method with calls to len had each occurrence replaced with Len.
+        '''
+        def _wrap_chunks(self, chunks):
+            '''_wrap_chunks(chunks : [string]) -> [string]
+            
+            Wrap a sequence of text chunks and return a list of lines of
+            length 'self.width' or less.  (If 'break_long_words' is false,
+            some lines may be longer than this.)  Chunks correspond roughly
+            to words and the whitespace between them: each chunk is
+            indivisible (modulo 'break_long_words'), but a line break can
+            come between any two chunks.  Chunks should not have internal
+            whitespace; ie. a chunk is either all whitespace or a "word".
+            Whitespace chunks will be removed from the beginning and end of
+            lines, but apart from that whitespace is preserved.
+            '''
+            lines = []
+            if self.width <= 0:
+                raise ValueError("invalid width %r (must be > 0)" % self.width)
+            if self.max_lines is not None:
+                if self.max_lines > 1:
+                    indent = self.subsequent_indent
+                else:
+                    indent = self.initial_indent
+                if Len(indent) + Len(self.placeholder.lstrip()) > self.width:
+                    raise ValueError("placeholder too large for max width")
+            # Arrange in reverse order so items can be efficiently popped
+            # from a stack of chucks.
+            chunks.reverse()
+            while chunks:
+                # Start the list of chunks that will make up the current line.
+                # cur_len is just the length of all the chunks in cur_line.
+                cur_line = []
+                cur_len = 0
+                # Figure out which static string will prefix this line.
+                if lines:
+                    indent = self.subsequent_indent
+                else:
+                    indent = self.initial_indent
+                # Maximum width for this line.
+                width = self.width - Len(indent)
+                # First chunk on line is whitespace -- drop it, unless this
+                # is the very beginning of the text (ie. no lines started yet).
+                if self.drop_whitespace and chunks[-1].strip() == "" and lines:
+                    del chunks[-1]
+                while chunks:
+                    L = Len(chunks[-1])
+                    # Can at least squeeze this chunk onto the current line.
+                    if cur_len + L <= width:
+                        cur_line.append(chunks.pop())
+                        cur_len += L
+                    # Nope, this line is full.
+                    else:
+                        break
+                # The current line is full, and the next chunk is too big to
+                # fit on *any* line (not just this one).
+                if chunks and Len(chunks[-1]) > width:
+                    self._handle_long_word(chunks, cur_line, cur_len, width)
+                    cur_len = sum(map(Len, cur_line))
+                # If the last chunk on this line is all whitespace, drop it.
+                if self.drop_whitespace and cur_line and cur_line[-1].strip() == "":
+                    cur_len -= Len(cur_line[-1])
+                    del cur_line[-1]
+                if cur_line:
+                    if (
+                        self.max_lines is None
+                        or Len(lines) + 1 < self.max_lines
+                        or (
+                            not chunks
+                            or self.drop_whitespace
+                            and Len(chunks) == 1
+                            and not chunks[0].strip()
+                        )
+                        and cur_len <= width
+                    ):
+                        # Convert current line back to a string and store it in
+                        # list of all lines (return value).
+                        lines.append(indent + "".join(cur_line))
+                    else:
+                        while cur_line:
+                            if (
+                                cur_line[-1].strip()
+                                and cur_len + Len(self.placeholder) <= width
+                            ):
+                                cur_line.append(self.placeholder)
+                                lines.append(indent + "".join(cur_line))
+                                break
+                            cur_len -= Len(cur_line[-1])
+                            del cur_line[-1]
+                        else:
+                            if lines:
+                                prev_line = lines[-1].rstrip()
+                                if Len(prev_line) + Len(self.placeholder) <= self.width:
+                                    lines[-1] = prev_line + self.placeholder
+                                    break
+                            lines.append(indent + self.placeholder.lstrip())
+                        break
+            return lines
 if 1:   # Old util stuff
     def StringToNumbers(s, sep=" ", handle_i=True):
         '''s is a string; return the sequence (tuple) of numbers it represents; number
@@ -1808,15 +1909,16 @@ if 1:   # Old util stuff
         os.chdir(dir)
         for file in files:
             p = Path(file) if isinstance(file, str) else file
-            for line in p.open().readlines():
-                line = line.rstrip()
-                mo = r.search(line)
-                if mo:
-                    for tag in mo.groups():
-                        t = f"{tag}\t{file}\t/*{tag}*"
-                        tags.append(t)
-                    if dbg:
-                        print(f"tag(s) found in [{file}]:  {line!r}")
+            with p.open() as f:
+                for line in f.readlines():
+                    line = line.rstrip()
+                    mo = r.search(line)
+                    if mo:
+                        for tag in mo.groups():
+                            t = f"{tag}\t{file}\t/*{tag}*"
+                            tags.append(t)
+                        if dbg:
+                            print(f"tag(s) found in [{file}]:  {line!r}")
         # Get rid of duplicates
         tags = list(sorted(list(set(tags))))
         n = len(tags) - 1
